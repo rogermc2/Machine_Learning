f33e2a365d5ccc39d15d3c4ada70f15205240c76
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-08-11 16:33:10 +1000

    New project class_weight_tests

--- decision_tree_classifier/test/class_weight_tests/src/test_weight_functions.adb
+++ decision_tree_classifier/test/class_weight_tests/src/test_weight_functions.adb
@@ -1,10 +1,9 @@
 
-with Ada.Text_IO; use Ada.Text_IO;
-
+with Classifier_Types; use Classifier_Types;
 with Classifier_Utilities; use Classifier_Utilities;
 with Models; use Models;
 
-procedure Test_Functions is
+procedure Test_Weight_Functions is
    X      : constant Float_Array (1 .. 6) := (0.0, 1.0, 2.0, 3.0, 4.0, 5.0);
    B      : constant Float_Array (1 .. 4) := (13.7, 1.0, 1.0, 9.7);
    B_List : constant Float_List := To_Float_List (B);
@@ -12,4 +11,4 @@ procedure Test_Functions is
         Y : constant Float_List := Linear_Function (B_List, X_List);
 begin
    Print_Float_List ("Y", Y);
-end Test_Functions;
+end Test_Weight_Functions;
