a833eddab1a978a9a3b680935d71ac718a4247a8
rogermc2 <rogermc@iinet.net.au>
2021-09-13 14:59:33 +1000

    Coding Build_Tree

--- decision_tree_classifier/src/decision_tree_classifer.adb
+++ decision_tree_classifier/src/decision_tree_classifer.adb
@@ -11,212 +11,218 @@ with Tree_Build;
 
 package body Decision_Tree_Classifer is
 
-   procedure Build_Best_First
-     (X, Y          : ML_Types.List_Of_Value_Data_Lists;
-      Sample_Weight : Classifier_Types.Weight_List;
-      aTree         : in out Tree.Tree_Class);
-   procedure Build_Depth_First
-     (X, Y          : ML_Types.List_Of_Value_Data_Lists;
-      Sample_Weight : Classifier_Types.Weight_List;
-      aTree         : in out Tree.Tree_Class);
+    procedure Build_Best_First
+      (X, Y          : ML_Types.List_Of_Value_Data_Lists;
+       Sample_Weight : Classifier_Types.Weight_List;
+       aTree         : in out Tree.Tree_Class);
+    procedure Build_Depth_First
+      (X, Y          : ML_Types.List_Of_Value_Data_Lists;
+       Sample_Weight : Classifier_Types.Weight_List;
+       aTree         : in out Tree.Tree_Class);
 
-   --  -------------------------------------------------------------------------
+    --  -------------------------------------------------------------------------
 
-   procedure Build_Tree (Self          : in out Classifier;
-                         X, Y          : ML_Types.List_Of_Value_Data_Lists;
-                         Sample_Weight : Classifier_Types.Weight_List;
-                        Max_Leaves : Natural) is
-      use ML_Types;
-      --        Criterion : Classifier_Criteria_Type := Self.Parameters.Criterion;
-      --        Splitter  : Splitter_Type := Self.Parameters.Splitter;
-      theTree   : Tree.Tree_Class
-        (Self.Attributes.Num_Features, Self.Attributes.Num_Outputs,
-         Tree.Index_Range (Self.Attributes.Classes.Length));
-      Feature_Values : Value_Data_List;
-      aRow           : Row_Data (Class_Range (X.Element (1).Length));
-      Rows           : Rows_Vector;
-      Row_Tree       : ML_Types.Tree_Type;
-   begin
-      --  L346
-      --  if is_classifier(self):
-      Self.Attributes.Decision_Tree := theTree;
+    procedure Build_Tree (Self          : in out Classifier;
+                          X, Y          : ML_Types.List_Of_Value_Data_Lists;
+                          Sample_Weight : Classifier_Types.Weight_List;
+                          Max_Leaves : Natural := 0) is
+        use ML_Types;
+        --        Criterion : Classifier_Criteria_Type := Self.Parameters.Criterion;
+        --        Splitter  : Splitter_Type := Self.Parameters.Splitter;
+        theTree   : Tree.Tree_Class
+          (Self.Attributes.Num_Features, Self.Attributes.Num_Outputs,
+           Tree.Index_Range (Self.Attributes.Classes.Length));
+        Feature_Values : Value_Data_List;
+        aRow           : Row_Data (Class_Range (X.Element (1).Length));
+        Rows           : Rows_Vector;
+        Row_Tree       : ML_Types.Tree_Type;
+    begin
+        --  L346
+        --  if is_classifier(self):
+        Self.Attributes.Decision_Tree := theTree;
 
-      --  L390
-      if Self.Parameters.Max_Leaf_Nodes < 0 then
-         Build_Depth_First (X, Y, Sample_Weight, theTree);
-      else
-         Build_Best_First (X, Y, Sample_Weight, theTree);
-      end if;
+        --  L390
+        if Self.Parameters.Max_Leaf_Nodes < 0 then
+            Build_Depth_First (X, Y, Sample_Weight, theTree);
+        else
+            Build_Best_First (X, Y, Sample_Weight, theTree);
+        end if;
 
-      --  L408
-      --        Builder.Build_Tree (Self, X, y , Sample_Weight)
-      for index in 1 .. Positive (X.Length) loop
-         Feature_Values := X.Element (index);
-         aRow.Features (Class_Range (index)) := Feature_Values;
-      end loop;
-      Row_Tree := Builder.Build_Tree (Rows, Max_Leaves);
-
-   end Build_Tree;
-
-   --  -------------------------------------------------------------------------
-
-   procedure Build_Best_First
-     (X, Y          : ML_Types.List_Of_Value_Data_Lists;
-      Sample_Weight : Classifier_Types.Weight_List;
-      aTree         : in out Tree.Tree_Class) is
-      Builder       : Tree_Build.Tree_Builder (Tree_Build.Best_First_Tree);
-   begin
-      Tree_Build.Build_Best_First_Tree (Builder, aTree, X, Y, Sample_Weight);
-
-   end Build_Best_First;
-
-   --  -------------------------------------------------------------------------
-
-   procedure Build_Depth_First
-     (X, Y          : ML_Types.List_Of_Value_Data_Lists;
-      Sample_Weight : Classifier_Types.Weight_List;
-      aTree         : in out Tree.Tree_Class) is
-      Builder       : Tree_Build.Tree_Builder (Tree_Build.Depth_First_Tree);
-   begin
-      Tree_Build.Build_Depth_First_Tree (Builder, aTree, X, Y, Sample_Weight);
-
-   end Build_Depth_First;
-
-   --  -------------------------------------------------------------------------
-
-   procedure Check_Parameters is
-   begin
-      null;
-   end Check_Parameters;
-
-   --  -------------------------------------------------------------------------
-   --  if is_classification: part of Python BasesDecisionTree.Fit
-   procedure Classification_Fit
-     (aClassifier           : in out Classifier;
-      Y                     : in out ML_Types.List_Of_Value_Data_Lists; Num_Outputs : Positive;
-      Y_Encoded             : out ML_Types.List_Of_Value_Data_Lists;
-      Expanded_Class_Weight : out Classifier_Types.Float_List) is
-      use ML_Types;
-      use Weights;
-      Y_Original : ML_Types.List_Of_Value_Data_Lists;
-      Y_K        : Value_Data_List;
-      Classes_K  : Value_Data_List;
-      Inverse    : Natural_List;
-   begin
-      --  L206
-      Y_Encoded.Clear;
-      Y_Encoded.Set_Length (Ada.Containers.Count_Type (Num_Outputs));
-      for k in 1 .. Num_Outputs loop
-         Y_K := Y.Element (k);
-         Classes_K := Encode_Utils.Unique (Y_K, Inverse);
-         Y_Encoded.Replace_Element (k, Classes_K);
-         aClassifier.Attributes.Classes.Append (Classes_K);
-      end loop;
-      Y := Y_Encoded;
-
-      if aClassifier.Parameters.Class_Weight /= Weights.No_Weight then
-         --  y_original = np.copy(y)
-         Expanded_Class_Weight :=
-           Weights.Compute_Sample_Weight (Weights.No_Weight, Y_Original);
-      end if;
-
-      --  L215
-      if aClassifier.Parameters.Class_Weight /= No_Weight then
-         Expanded_Class_Weight := Weights.Compute_Sample_Weight
-           (aClassifier.Parameters.Class_Weight, Y_Original);
-      end if;
-
-   end Classification_Fit;
-
-   --  -------------------------------------------------------------------------
-   --  The Fit function adjusts weights according to data values so that
-   --  better accuracy can be achieved
-   --  Based on tree/_classes.py BaseDecisionTree.Fit
-   --  X :  a (n_samples, n_features) matrix of training samples
-   --  Y :  a (n_samples, n_outputs) array of integer valued class labels
-   --       for the training samples.
-   --  Sample_Weight : array-like of shape (n_samples,), default=None
-   procedure Fit (aClassifier   : in out Classifier;
-                 X             : ML_Types.List_Of_Value_Data_Lists;
-                 Y             : in out ML_Types.List_Of_Value_Data_Lists;
-                 Sample_Weight : in out Classifier_Types.Weight_List;
-                 Check_Input   : Boolean := False) is
-      use Ada.Containers;
-      use ML_Types;
-      use Classifier_Types;
-      use Classifier_Types.Integer_Package;
-      Num_Outputs           : constant Positive := Positive (Y.Length);
-      Num_Samples           : constant Positive :=
-                                Positive (Y.Length);
-      --        Random_State          : Integer := aClassifier.Parameters.Random_State;
-      Expanded_Class_Weight : Classifier_Types.Float_List;
-      Y_Encoded             : List_Of_Value_Data_Lists;
-      Max_Leaf_Nodes        : Integer := -1;
-   begin
-      --  L154
-      if aClassifier.Parameters.CCP_Alpha < 0.0 then
-         raise Value_Error with
-           "Decision_Tree_Classifer.Fit CCP_Alpha must be greater than or equal to 0";
-      end if;
-
-      --  L156
-      if Check_Input then
-         null;
-      end if;
-
-      --  L184
-      aClassifier.Attributes.Num_Features := Tree.Index_Range (Num_Samples);
-      aClassifier.Attributes.Num_Outputs := Tree.Index_Range (Num_Outputs);
-      aClassifier.Parameters.Max_Leaf_Nodes := Max_Leaf_Nodes;
-     --  L201
-      aClassifier.Attributes.Classes.Clear;
-      aClassifier.Attributes.Num_Classes.Clear;
-
-       Classifier_Utilities.Print_Value_List ("Decision_Tree_Classifer.Fit X (1)", X.Element (1));
-      Classifier_Utilities.Print_Value_List ("Decision_Tree_Classifer.Fit X (2)", X.Element (2));
-
-      --  L293
-      if Positive (Y.Length) /= Num_Samples then
-         raise Value_Error with
-           "Decision_Tree_Classifer.Fit Number of labels =" &
-           Count_Type'Image (Y.Length) & " does not match number of samples ="
-           & Integer'Image (Num_Samples);
-      end if;
-
-      Classifier_Utilities.Print_Value_List ("Decision_Tree_Classifer.Fit Y (1)", Y.Element (1));
-
-      --  L206
-      Classification_Fit (aClassifier, Y, Num_Outputs, Y_Encoded,
-                          Expanded_Class_Weight);
-      --  L218
-
-      --  L226
-      if aClassifier.Parameters.Max_Leaf_Nodes > 0 then
-         Max_Leaf_Nodes := aClassifier.Parameters.Max_Leaf_Nodes;
-      end if;
-
-      Check_Parameters;
-
-      --  L318
-      if not Expanded_Class_Weight.Is_Empty then
-         if Sample_Weight.Is_Empty then
-            Sample_Weight := Expanded_Class_Weight;
-         else
-            for index in Sample_Weight.First_Index ..
-              Sample_Weight.Last_Index loop
-               Sample_Weight.Replace_Element
-                 (index, Sample_Weight.Element (index) *
-                      Expanded_Class_Weight.Element (index)) ;
+        --  L408
+        --        Builder.Build_Tree (Self, X, y , Sample_Weight)
+        for index in 1 .. Positive (X.Length) loop
+            Feature_Values := X.Element (index);
+            for index2 in Feature_Values.First_Index ..
+              Feature_Values.Last_Index loop
+                aRow.Features (Class_Range (index)) :=
+                  Feature_Values.Element (index2).UB_String_Value;
+                Rows.Append (aRow);
             end loop;
-         end if;
-      end if;
+        end loop;
 
-      --  L343
-      Build_Tree (aClassifier, X, Y, Sample_Weight);
-      --  L410
+        Row_Tree := Builder.Build_Tree (Rows, Max_Leaves);
 
-   end Fit;
+    end Build_Tree;
 
-   --  -------------------------------------------------------------------------
+    --  -------------------------------------------------------------------------
+
+    procedure Build_Best_First
+      (X, Y          : ML_Types.List_Of_Value_Data_Lists;
+       Sample_Weight : Classifier_Types.Weight_List;
+       aTree         : in out Tree.Tree_Class) is
+        Builder       : Tree_Build.Tree_Builder (Tree_Build.Best_First_Tree);
+    begin
+        Tree_Build.Build_Best_First_Tree (Builder, aTree, X, Y, Sample_Weight);
+
+    end Build_Best_First;
+
+    --  -------------------------------------------------------------------------
+
+    procedure Build_Depth_First
+      (X, Y          : ML_Types.List_Of_Value_Data_Lists;
+       Sample_Weight : Classifier_Types.Weight_List;
+       aTree         : in out Tree.Tree_Class) is
+        Builder       : Tree_Build.Tree_Builder (Tree_Build.Depth_First_Tree);
+    begin
+        Tree_Build.Build_Depth_First_Tree (Builder, aTree, X, Y, Sample_Weight);
+
+    end Build_Depth_First;
+
+    --  -------------------------------------------------------------------------
+
+    procedure Check_Parameters is
+    begin
+        null;
+    end Check_Parameters;
+
+    --  -------------------------------------------------------------------------
+    --  if is_classification: part of Python BasesDecisionTree.Fit
+    procedure Classification_Fit
+      (aClassifier           : in out Classifier;
+       Y                     : in out ML_Types.List_Of_Value_Data_Lists; Num_Outputs : Positive;
+       Y_Encoded             : out ML_Types.List_Of_Value_Data_Lists;
+       Expanded_Class_Weight : out Classifier_Types.Float_List) is
+        use ML_Types;
+        use Weights;
+        Y_Original : ML_Types.List_Of_Value_Data_Lists;
+        Y_K        : Value_Data_List;
+        Classes_K  : Value_Data_List;
+        Inverse    : Natural_List;
+    begin
+        --  L206
+        Y_Encoded.Clear;
+        Y_Encoded.Set_Length (Ada.Containers.Count_Type (Num_Outputs));
+        for k in 1 .. Num_Outputs loop
+            Y_K := Y.Element (k);
+            Classes_K := Encode_Utils.Unique (Y_K, Inverse);
+            Y_Encoded.Replace_Element (k, Classes_K);
+            aClassifier.Attributes.Classes.Append (Classes_K);
+        end loop;
+        Y := Y_Encoded;
+
+        if aClassifier.Parameters.Class_Weight /= Weights.No_Weight then
+            --  y_original = np.copy(y)
+            Expanded_Class_Weight :=
+              Weights.Compute_Sample_Weight (Weights.No_Weight, Y_Original);
+        end if;
+
+        --  L215
+        if aClassifier.Parameters.Class_Weight /= No_Weight then
+            Expanded_Class_Weight := Weights.Compute_Sample_Weight
+              (aClassifier.Parameters.Class_Weight, Y_Original);
+        end if;
+
+    end Classification_Fit;
+
+    --  -------------------------------------------------------------------------
+    --  The Fit function adjusts weights according to data values so that
+    --  better accuracy can be achieved
+    --  Based on tree/_classes.py BaseDecisionTree.Fit
+    --  X :  a (n_samples, n_features) matrix of training samples
+    --  Y :  a (n_samples, n_outputs) array of integer valued class labels
+    --       for the training samples.
+    --  Sample_Weight : array-like of shape (n_samples,), default=None
+    procedure Fit (aClassifier   : in out Classifier;
+                   X             : ML_Types.List_Of_Value_Data_Lists;
+                   Y             : in out ML_Types.List_Of_Value_Data_Lists;
+                   Sample_Weight : in out Classifier_Types.Weight_List;
+                   Check_Input   : Boolean := False) is
+        use Ada.Containers;
+        use ML_Types;
+        use Classifier_Types;
+        use Classifier_Types.Integer_Package;
+        Num_Outputs           : constant Positive := Positive (Y.Length);
+        Num_Samples           : constant Positive :=
+                                  Positive (Y.Length);
+        --        Random_State          : Integer := aClassifier.Parameters.Random_State;
+        Expanded_Class_Weight : Classifier_Types.Float_List;
+        Y_Encoded             : List_Of_Value_Data_Lists;
+        Max_Leaf_Nodes        : Integer := -1;
+    begin
+        --  L154
+        if aClassifier.Parameters.CCP_Alpha < 0.0 then
+            raise Value_Error with
+              "Decision_Tree_Classifer.Fit CCP_Alpha must be greater than or equal to 0";
+        end if;
+
+        --  L156
+        if Check_Input then
+            null;
+        end if;
+
+        --  L184
+        aClassifier.Attributes.Num_Features := Tree.Index_Range (Num_Samples);
+        aClassifier.Attributes.Num_Outputs := Tree.Index_Range (Num_Outputs);
+        aClassifier.Parameters.Max_Leaf_Nodes := Max_Leaf_Nodes;
+        --  L201
+        aClassifier.Attributes.Classes.Clear;
+        aClassifier.Attributes.Num_Classes.Clear;
+
+        Classifier_Utilities.Print_Value_List ("Decision_Tree_Classifer.Fit X (1)", X.Element (1));
+        Classifier_Utilities.Print_Value_List ("Decision_Tree_Classifer.Fit X (2)", X.Element (2));
+
+        --  L293
+        if Positive (Y.Length) /= Num_Samples then
+            raise Value_Error with
+              "Decision_Tree_Classifer.Fit Number of labels =" &
+              Count_Type'Image (Y.Length) & " does not match number of samples ="
+              & Integer'Image (Num_Samples);
+        end if;
+
+        Classifier_Utilities.Print_Value_List ("Decision_Tree_Classifer.Fit Y (1)", Y.Element (1));
+
+        --  L206
+        Classification_Fit (aClassifier, Y, Num_Outputs, Y_Encoded,
+                            Expanded_Class_Weight);
+        --  L218
+
+        --  L226
+        if aClassifier.Parameters.Max_Leaf_Nodes > 0 then
+            Max_Leaf_Nodes := aClassifier.Parameters.Max_Leaf_Nodes;
+        end if;
+
+        Check_Parameters;
+
+        --  L318
+        if not Expanded_Class_Weight.Is_Empty then
+            if Sample_Weight.Is_Empty then
+                Sample_Weight := Expanded_Class_Weight;
+            else
+                for index in Sample_Weight.First_Index ..
+                  Sample_Weight.Last_Index loop
+                    Sample_Weight.Replace_Element
+                      (index, Sample_Weight.Element (index) *
+                           Expanded_Class_Weight.Element (index)) ;
+                end loop;
+            end if;
+        end if;
+
+        --  L343
+        Build_Tree (aClassifier, X, Y, Sample_Weight);
+        --  L410
+
+    end Fit;
+
+    --  -------------------------------------------------------------------------
 
 end Decision_Tree_Classifer;
