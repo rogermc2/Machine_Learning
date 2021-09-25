697b88e9c71c2bc6d59da0b95ff95ab73ea744d0
rogermc2 <rogermc@iinet.net.au>
2021-09-25 11:12:04 +1000

    New package ada_tree_build

---
+++ decision_tree_classifier/src/ada_tree_build.ads
@@ -0,0 +1,36 @@
+
+with Classifier_Types;
+with ML_Types;
+with Node_Splitter;
+with Tree;
+
+package Ada_Tree_Build is
+
+    type Tree_Builder is record
+        Splitter              : Node_Splitter.Splitter_Class;
+        --  Minimum samples in an internal node
+        Min_Samples_Split     : Natural := 0;
+        Min_Samples_Leaf      : Natural := 0;
+        Min_Weight_Leaf       : Float := 0.0;
+        Max_Depth             : Natural := 0;
+        Min_Impurity_Decrease : Float := 0.0;
+    end record;
+
+    Ada_Tree_Build_Error : Exception;
+
+    procedure Build_Tree
+      (Builder       : in out Tree_Builder;
+       theTree       : in out Tree.Tree_Class;
+       X, Y          : ML_Types.List_Of_Value_Data_Lists;
+       Sample_Weight : Classifier_Types.Weight_List);
+    procedure Init_Tree
+      (Builder               : in out Tree_Builder;
+       Splitter              : Node_Splitter.Splitter_Class;
+       Min_Samples_Split     : Natural := 0;
+       Min_Samples_Leaf      : Natural := 0;
+       Min_Weight_Leaf       : Float := 0.0;
+       Max_Depth             : Natural := 0;
+       Max_Leaf_Nodes        : Integer := -1;  --  -1 means undefined
+       Min_Impurity_Decrease : Float := 0.0);
+
+end Ada_Tree_Build;
