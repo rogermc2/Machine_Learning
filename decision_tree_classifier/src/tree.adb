c31731f4c5ef88fc0d1dff1f43a21fac4cce498c
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-10-22 14:32:58 +1100

    Update Apply_Dense

--- decision_tree_classifier/src/tree.adb
+++ decision_tree_classifier/src/tree.adb
@@ -30,6 +30,7 @@ package body Tree is
       use ML_Types;
       use Nodes_Package;
       Num_Samples    : constant Positive := Positive (X.Length);
+      Top_Cursor     : constant Tree_Cursor := First_Child (Self.Nodes.Root);
       Node_Cursor    : Tree_Cursor;
       Left_Cursor    : Tree_Cursor;
       Node           : Tree_Node;
@@ -40,24 +41,31 @@ package body Tree is
       Split          : Boolean;
       Output_Cursors : Leaf_Cursor_Array (1 .. Num_Samples);
    begin
-      Assert (Integer (Child_Count (Self.Nodes.Root)) > 0,
+      Assert (Integer (Child_Count (Top_Cursor)) > 0,
               "Tree.Apply_Dense Self.Nodes tree is empty");
 
       --  for each sample of features
       for index in X.First_Index .. X.Last_Index loop
+         Not_Leaf := True;
          Sample := X.Element (index);
-         Node_Cursor := First_Child (Self.Nodes.Root);
---           while Not_Leaf and then Has_Element (Node_Cursor) loop
+         Node_Cursor := Top_Cursor;
+         Put_Line ("Tree.Apply_Dense Node_Cursor index: " &
+                     Integer'Image (index));
+         --           while Not_Leaf and then Has_Element (Node_Cursor) loop
          while Not_Leaf loop
             Node := Element (Node_Cursor);
             Left_Cursor := First_Child (Node_Cursor);
             Not_Leaf := not Element (Left_Cursor).Leaf_Node;
+            Put_Line ("Tree.Apply_Dense Left_Cursor Not_Leaf: " &
+                     Boolean'Image (Not_Leaf));
             if Not_Leaf then
                Left_Node := Element (Left_Cursor);
                if not Left_Node.Leaf_Node then
                   Assert (Feature.Value_Kind = Float_Type or
                             Feature.Value_Kind = Integer_Type,
                           "Tree.Apply_Dense Self.Nodes invalid feature data type");
+                  Put_Line ("Tree.Apply_Dense Feature_Index: " &
+                              Integer'Image (Node.Feature_Index));
                   Feature := Sample.Element (Node.Feature_Index);
                   case Feature.Value_Kind is
                   when Float_Type =>
@@ -66,16 +74,21 @@ package body Tree is
                      Split := Float (Feature.Integer_Value) <= Node.Threshold;
                   when others => null;
                   end case;
+
+                  Put_Line ("Tree.Apply_Dense Node_Cursor Split: " &
+                              Boolean'Image (Split));
                   if Split then
                      Node_Cursor := First_Child (Node_Cursor);
                   else
                      Node_Cursor := Last_Child (Node_Cursor);
                   end if;
                end if;
+
             end if;
          end loop;
 
          Output_Cursors (index) := Node_Cursor;
+         New_Line;
       end loop;
 
       return Output_Cursors;
@@ -100,7 +113,7 @@ package body Tree is
    --  _tree L763
    function Predict (Self : Tree_Class;
                      X    : ML_Types.List_Of_Value_Data_Lists)
-                  return ML_Types.Value_Data_List is
+                     return ML_Types.Value_Data_List is
       --  X is a list of samples
       --  Each sample is a list of feature values, one value per feature
       use ML_Types;
@@ -121,11 +134,11 @@ package body Tree is
         ("Tree.Predict Self.Nodes X", X);
 
       Leaf_Cursors := Apply (Self, X);
-      Classifier_Utilities.Print_Node_Cursor_Array
-          ("Tree.Predict Self.Nodes Leaf_Cursors", Leaf_Cursors);
+--        Classifier_Utilities.Print_Node_Cursor_Array
+--          ("Tree.Predict Self.Nodes Leaf_Cursors", Leaf_Cursors);
       --  L801
       for index in 1 .. N_Samples loop
-         Put_Line ("Tree.Predict Self.Nodes index" & Integer'Image (index));
+--           Put_Line ("Tree.Predict Self.Nodes index" & Integer'Image (index));
          Leaf := Element (Leaf_Cursors (index));
          if Leaf.Leaf_Node then
             Put_Line ("Tree.Predict Self.Nodes Leaf.Feature_Index" &
