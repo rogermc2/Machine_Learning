560dcfb62b9e17a3535db5424cb6e0e6073b20b3
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-10-23 23:42:51 +1100

    Update

--- decision_tree_classifier/src/tree.adb
+++ decision_tree_classifier/src/tree.adb
@@ -9,87 +9,87 @@ package body Tree is
 
    --  -------------------------------------------------------------------------
    --  L770 Apply finds the terminal region (=leaf node) for each sample in X.
---     function Apply (Self : Tree_Class;
---                     X    : ML_Types.List_Of_Value_Data_Lists)
---                     return Leaf_Cursor_Array is
---     begin
---        return Apply_Dense (Self, X);
---     end Apply;
+   --     function Apply (Self : Tree_Class;
+   --                     X    : ML_Types.List_Of_Value_Data_Lists)
+   --                     return Leaf_Cursor_Array is
+   --     begin
+   --        return Apply_Dense (Self, X);
+   --     end Apply;
 
    --  -------------------------------------------------------------------------
    --  L777
---     function Apply_Dense (Self : Tree_Class;
---                           X    : ML_Types.List_Of_Value_Data_Lists)
---                           return Leaf_Cursor_Array is
---        --  X is a list of samples of features
---        use Ada.Containers;
---        use ML_Types;
---        use Nodes_Package;
---        Num_Samples    : constant Positive := Positive (X.Length);
---        Top_Cursor     : constant Tree_Cursor := First_Child (Self.Nodes.Root);
---        Node_Cursor    : Tree_Cursor;
---        Left_Cursor    : Tree_Cursor;
---        Node           : Tree_Node;
---        Left_Node      : Tree_Node;
---        Sample         : Value_Data_List;
---        Feature_Value  : Value_Record;
---        Not_Leaf       : Boolean := True;
---        Split          : Boolean;
---        Output_Cursors : Leaf_Cursor_Array (1 .. Num_Samples);
---     begin
---        Assert (Integer (Child_Count (Top_Cursor)) > 0,
---                "Tree.Apply_Dense Self.Nodes tree is empty");
---
---        --  for each sample of features
---        for index in X.First_Index .. X.Last_Index loop
---           Sample := X.Element (index);
---           Node_Cursor := Top_Cursor;
---           Not_Leaf := True;
---           Put_Line ("Tree.Apply_Dense sample index: " &
---                       Integer'Image (index));
---           while Not_Leaf loop
---              Node := Element (Node_Cursor);
---              Left_Cursor := First_Child (Node_Cursor);
---              Not_Leaf := not Element (Left_Cursor).Leaf_Node;
---              Put_Line ("Tree.Apply_Dense Left_Cursor Not_Leaf: " &
---                          Boolean'Image (Not_Leaf));
---              if Not_Leaf then
---                 Left_Node := Element (Left_Cursor);
---                 if not Left_Node.Leaf_Node then
---                    Assert (Feature_Value.Value_Kind = Float_Type or
---                              Feature_Value.Value_Kind = Integer_Type,
---                            "Tree.Apply_Dense Self.Nodes invalid feature data type");
---                    Put_Line ("Tree.Apply_Dense Feature_Index: " &
---                                Integer'Image (Node.Feature_Index));
---                    Feature_Value := Sample.Element (Node.Feature_Index);
---                    case Feature_Value.Value_Kind is
---                    when Float_Type =>
---                       Split := Feature_Value.Float_Value <= Node.Threshold;
---                    when Integer_Type =>
---                       Split := Float (Feature_Value.Integer_Value) <=
---                         Node.Threshold;
---                    when others => null;
---                    end case;
---
---                    Put_Line ("Tree.Apply_Dense Node_Cursor Split: " &
---                                Boolean'Image (Split));
---                    if Split then
---                       Node_Cursor := First_Child (Node_Cursor);
---                    else
---                       Node_Cursor := Last_Child (Node_Cursor);
---                    end if;
---                 end if;
---
---              end if;  --  Not_Leaf
---           end loop;  --  Not_Leaf
---
---           Output_Cursors (index) := Node_Cursor;
---           New_Line;
---        end loop;
---
---        return Output_Cursors;
---
---     end Apply_Dense;
+   --     function Apply_Dense (Self : Tree_Class;
+   --                           X    : ML_Types.List_Of_Value_Data_Lists)
+   --                           return Leaf_Cursor_Array is
+   --        --  X is a list of samples of features
+   --        use Ada.Containers;
+   --        use ML_Types;
+   --        use Nodes_Package;
+   --        Num_Samples    : constant Positive := Positive (X.Length);
+   --        Top_Cursor     : constant Tree_Cursor := First_Child (Self.Nodes.Root);
+   --        Node_Cursor    : Tree_Cursor;
+   --        Left_Cursor    : Tree_Cursor;
+   --        Node           : Tree_Node;
+   --        Left_Node      : Tree_Node;
+   --        Sample         : Value_Data_List;
+   --        Feature_Value  : Value_Record;
+   --        Not_Leaf       : Boolean := True;
+   --        Split          : Boolean;
+   --        Output_Cursors : Leaf_Cursor_Array (1 .. Num_Samples);
+   --     begin
+   --        Assert (Integer (Child_Count (Top_Cursor)) > 0,
+   --                "Tree.Apply_Dense Self.Nodes tree is empty");
+   --
+   --        --  for each sample of features
+   --        for index in X.First_Index .. X.Last_Index loop
+   --           Sample := X.Element (index);
+   --           Node_Cursor := Top_Cursor;
+   --           Not_Leaf := True;
+   --           Put_Line ("Tree.Apply_Dense sample index: " &
+   --                       Integer'Image (index));
+   --           while Not_Leaf loop
+   --              Node := Element (Node_Cursor);
+   --              Left_Cursor := First_Child (Node_Cursor);
+   --              Not_Leaf := not Element (Left_Cursor).Leaf_Node;
+   --              Put_Line ("Tree.Apply_Dense Left_Cursor Not_Leaf: " &
+   --                          Boolean'Image (Not_Leaf));
+   --              if Not_Leaf then
+   --                 Left_Node := Element (Left_Cursor);
+   --                 if not Left_Node.Leaf_Node then
+   --                    Assert (Feature_Value.Value_Kind = Float_Type or
+   --                              Feature_Value.Value_Kind = Integer_Type,
+   --                            "Tree.Apply_Dense Self.Nodes invalid feature data type");
+   --                    Put_Line ("Tree.Apply_Dense Feature_Index: " &
+   --                                Integer'Image (Node.Feature_Index));
+   --                    Feature_Value := Sample.Element (Node.Feature_Index);
+   --                    case Feature_Value.Value_Kind is
+   --                    when Float_Type =>
+   --                       Split := Feature_Value.Float_Value <= Node.Threshold;
+   --                    when Integer_Type =>
+   --                       Split := Float (Feature_Value.Integer_Value) <=
+   --                         Node.Threshold;
+   --                    when others => null;
+   --                    end case;
+   --
+   --                    Put_Line ("Tree.Apply_Dense Node_Cursor Split: " &
+   --                                Boolean'Image (Split));
+   --                    if Split then
+   --                       Node_Cursor := First_Child (Node_Cursor);
+   --                    else
+   --                       Node_Cursor := Last_Child (Node_Cursor);
+   --                    end if;
+   --                 end if;
+   --
+   --              end if;  --  Not_Leaf
+   --           end loop;  --  Not_Leaf
+   --
+   --           Output_Cursors (index) := Node_Cursor;
+   --           New_Line;
+   --        end loop;
+   --
+   --        return Output_Cursors;
+   --
+   --     end Apply_Dense;
 
    --  -------------------------------------------------------------------------
 
@@ -115,11 +115,11 @@ package body Tree is
       use Ada.Containers;
       Num_Outputs     : constant Positive := Positive (X.Element (1).Length);
       Num_Nodes       : constant Positive :=
-                            Positive (Self.Nodes.Node_Count - 1);
+                          Positive (Self.Nodes.Node_Count - 1);
       Output_Values   : ML_Types.Value_Data_Lists_3D;
+      Node_ID         : Natural := 0;
       --  L1086 _get_value_ndarray
-      --  Output should be a 3D array (node count, num outputs, max classes)
-      --  self.value
+      --  Python output is a 3D array (node count, num outputs, max classes)
 
       procedure Build_Output (Curs : Cursor) is
          Node         : constant Tree_Node := Element (Curs);
@@ -128,6 +128,13 @@ package body Tree is
          Classes_Out  : ML_Types.Value_Data_Lists_2D;
          Class_Values : ML_Types.Value_Data_List;
       begin
+         Node_ID := Node_ID + 1;
+         Put_Line ("Tree.Predict.Build_Output Node ID" &
+                  Integer'Image (Node_ID));
+--           Classifier_Utilities.Print_Node
+--             ("Tree.Predict.Build_Output Node", Node);
+         Classifier_Utilities.Print_Value_Data_List_2D
+           ("Tree.Predict.Build_Output Values", Values);
          for output_index in Values.First_Index .. Values.Last_Index loop
             Class_Values := Values.Element (output_index);
             Classes_Out.Append (Class_Values);
@@ -141,16 +148,16 @@ package body Tree is
 
       Classifier_Utilities.Print_List_Of_Value_Data_Lists
         ("Tree.Predict Self.Nodes X", X);
-     Put_Line ("Tree.Predict Num_Nodes" & Integer'Image (Num_Nodes));
-     Put_Line ("Tree.Predict Num_Outputs" & Integer'Image (Num_Outputs));
-         Put_Line ("Tree.Predict.Build_Output Num_Classes" &
-                     Integer'Image (Integer (Self.Classes.Length)) &
-                  " x " & Integer'Image (Integer (Self.Classes.Element (1).Length)));
+      Put_Line ("Tree.Predict Num_Nodes" & Integer'Image (Num_Nodes));
+      Put_Line ("Tree.Predict Num_Outputs" & Integer'Image (Num_Outputs));
 
       --  L767
       Iterate_Subtree (Self.Nodes.Root, Build_Output'access);
 
---        Classifier_Utilities.Print_Value_Data_List ("Tree.Predict Output", Output);
+      Classifier_Utilities.Print_Value_Data_List_3D
+        ("Tree.Predict Output Values...", Output_Values);
+      Put_Line ("Tree.Predict done");
+
       return Output_Values;
 
    end Predict;
