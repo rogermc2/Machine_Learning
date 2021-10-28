864e297bc8d69fb0d1399896cc7e10033f31e4e9
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-10-28 16:54:33 +1100

    New package testing_classifer

---
+++ decision_tree_classifier/utils/testing_classifer.ads
@@ -0,0 +1,10 @@
+
+with ML_Types;
+with Tree;
+
+package Testing_Classifer is
+
+   procedure Predict (Self : Tree.Tree_Class;
+                      X    : ML_Types.Value_Data_Lists_2D);
+
+end Testing_Classifer;
