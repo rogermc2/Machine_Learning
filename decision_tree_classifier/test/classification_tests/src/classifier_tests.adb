b0db44cc8f134f0d9c262ab01b19bf472b10ae5f
rogermc2 <rogermc@iinet.net.au>
2021-10-31 20:44:30 +1100

    Update

--- decision_tree_classifier/test/classification_tests/src/classifier_tests.adb
+++ decision_tree_classifier/test/classification_tests/src/classifier_tests.adb
@@ -11,73 +11,75 @@ with Printing;
 with Tree;
 
 package body Classifier_Tests is
-   use Classifier_Types;
-   use ML_Types;
+    use Classifier_Types;
+    use ML_Types;
 
-   --     type Clf_Criterions is (Gini, Entropy);
-   --     type Reg_Criterions is (Squared_Error, Absolute_Error,
-   --                             Friedman_Mse, Poisson);
-   --     type Clf_Trees is (Decision_Tree_Classifier, Extra_Tree_Classifier);
-   --     type Reg_Trees is (Decision_Tree_Regressor, Extra_Tree_Regressor);
-   --  X_Array 6 rows (samples) x 2 columns (features)
-   X_Array     : constant Multi_Value_Array (1 .. 6, 1 .. 2) :=
-                   ((-2, -1), (-1, -1), (-1, -2), (1, 1), (1, 2), (2, 1));
-   Y_Array     : constant Integer_Array (1 .. 6) := (-1, -1, -1, 1, 1, 1);
-   T_Array     : constant Multi_Value_Array (1 .. 3, 1 .. 2) :=
-                      ((-1, -1), (2, 2), (3, 2));
---     True_Result : constant Integer_Array (1 .. 3) := (-1, 1, 1);
+    --     type Clf_Criterions is (Gini, Entropy);
+    --     type Reg_Criterions is (Squared_Error, Absolute_Error,
+    --                             Friedman_Mse, Poisson);
+    --     type Clf_Trees is (Decision_Tree_Classifier, Extra_Tree_Classifier);
+    --     type Reg_Trees is (Decision_Tree_Regressor, Extra_Tree_Regressor);
+    --  X_Array 6 rows (samples) x 2 columns (features)
+    X_Array     : constant Multi_Value_Array (1 .. 6, 1 .. 2) :=
+                    ((-2, -1), (-1, -1), (-1, -2), (1, 1), (1, 2), (2, 1));
+    Y_Array     : constant Integer_Array (1 .. 6) := (-1, -1, -1, 1, 1, 1);
+    T_Array     : constant Multi_Value_Array (1 .. 3, 1 .. 2) :=
+                    ((-1, -1), (2, 2), (3, 2));
+    True_Result : constant Integer_Array (1 .. 3) := (-1, 1, 1);
 
-   --  -------------------------------------------------------------------------
+    --  -------------------------------------------------------------------------
 
-   procedure Test_Classification_Toy  is
-      use Classifier_Utilities;
-      use Decision_Tree_Classifer;
-      use Printing;
+    procedure Test_Classification_Toy  is
+        use Classifier_Utilities;
+        use Decision_Tree_Classifer;
+        use Printing;
+        Expected        : Value_Data_Lists_2D;
+        Prediction      : ML_Types.Value_Data_Lists_2D;
+        theClassifier   : Base_Decision_Tree.Classifier
+          (Tree.Integer_Type, Tree.Integer_Type, Tree.Integer_Type);
+        --        Max_Depth       : constant Positive := 5;
+        X               : constant Value_Data_Lists_2D :=
+                            To_Multi_Value_List (X_Array);
+        Y               : Value_Data_Lists_2D;
+        T               : constant Value_Data_Lists_2D :=
+                            To_Multi_Value_List (T_Array);
+        Num_Samples     : constant Natural := Natural (X.Length);
+        --        Probabilities   : Weights.Weight_Lists_3D;
+    begin
+        Put_Line ("Classification_Tests.Test_Classification_Toy:");
+        Assert (Num_Samples > 0,
+                "Classification_Tests.Test_Classification_Toy called with empty X vector.");
 
---        Expected        : Value_Data_Lists_2D;
-      Prediction      : ML_Types.Value_Data_Lists_2D;
-      theClassifier   : Base_Decision_Tree.Classifier
-        (Tree.Integer_Type, Tree.Integer_Type, Tree.Integer_Type);
---        Max_Depth       : constant Positive := 5;
-      X               : constant Value_Data_Lists_2D :=
-                          To_Multi_Value_List (X_Array);
-      Y               : Value_Data_Lists_2D;
-      T               : constant Value_Data_Lists_2D :=
-                          To_Multi_Value_List (T_Array);
-      Num_Samples     : constant Natural := Natural (X.Length);
---        Probabilities   : Weights.Weight_Lists_3D;
-   begin
-      Put_Line ("Classification_Tests.Test_Classification_Toy:");
-      Assert (Num_Samples > 0,
-              "Classification_Tests.Test_Classification_Toy called with empty X vector.");
+        Y := To_Integer_Value_List (Y_Array);
+        --  L229
+        Expected := To_Integer_Value_List (True_Result);
+        --  L230
+        Classification_Fit (theClassifier, X, Y);
+        Put_Line ("Classification_Tests.Test_Classification_Toy Tree size: " &
+                    Integer'Image (Integer
+                    (theClassifier.Attributes.Decision_Tree.Nodes.Node_Count) - 1));
+        Print_Tree ("The Tree", theClassifier);
+        Put_Line ("----------------------------------------------");
+        New_Line;
+        Prediction := Base_Decision_Tree.Predict (theClassifier, T);
+        Print_Value_Data_Lists_2D
+          ("Classification_Tests.Test_Classification_Toy Predictions",
+           Prediction);
+        Print_Value_Data_Lists_2D
+          ("Classification_Tests.Test_Classification_Toy Expected", Expected);
+        --        Probabilities := Predict_Probability (theTree, X);
+        --        if Probabilities = Expected then
+        --           null;
+        --        end if;
+        --        Put_Line ("Classification_Tests.Test_Classification_Toy Print_Weight_Lists_3D");
+        --        Print_Weight_Lists_3D
+        --          ("Classification_Tests.Test_Classification_Toy Probabilities",
+        --           Probabilities);
+        --          Print_Value_List ("Classification_Tests.Test_Classification_Toy Classes",
+        --                            Classes);
 
-      Y := To_Integer_Value_List (Y_Array);
-      --  L229
---        Expected := To_Integer_Value_List (True_Result);
-      --  L230
-      Classification_Fit (theClassifier, X, Y);
-      Put_Line ("Classification_Tests.Test_Classification_Toy Tree size: " &
-                  Integer'Image (Integer
-                  (theClassifier.Attributes.Decision_Tree.Nodes.Node_Count) - 1));
-      Print_Tree ("The Tree", theClassifier);
-      Put_Line ("----------------------------------------------");
-      New_Line;
-      Prediction := Base_Decision_Tree.Predict (theClassifier, T);
-      Print_Value_Data_List ("Classification_Tests.Test_Classification_Toy Predictions",
-                              Prediction.Element (1));
---        Probabilities := Predict_Probability (theTree, X);
---        if Probabilities = Expected then
---           null;
---        end if;
---        Put_Line ("Classification_Tests.Test_Classification_Toy Print_Weight_Lists_3D");
---        Print_Weight_Lists_3D
---          ("Classification_Tests.Test_Classification_Toy Probabilities",
---           Probabilities);
-      --          Print_Value_List ("Classification_Tests.Test_Classification_Toy Classes",
-      --                            Classes);
+    end Test_Classification_Toy;
 
-   end Test_Classification_Toy;
-
-   --  -------------------------------------------------------------------------
+    --  -------------------------------------------------------------------------
 
 end Classifier_Tests;
