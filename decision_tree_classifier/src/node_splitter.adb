c3d21db2edee1b072e51b933d15c880d885e75d8
rogermc2 <rogermc@iinet.net.au>
2021-11-02 10:11:27 +1100

    Include Fisher-Yates pseuso code

--- decision_tree_classifier/src/node_splitter.adb
+++ decision_tree_classifier/src/node_splitter.adb
@@ -274,6 +274,10 @@ package body Node_Splitter is
    --  replacement using a Fisher-Yates-based algorithm.
    --  Variables F_I and F_J are used to compute a permutation of the Features
    --  being classified.
+   --  Pseudo code to shuffle an array a of n elements (indices 0..n-1):
+   --    for i from n - 1 downto 1 do
+   --         j = random integer with 0 <= j <= i
+   --         exchange a[j] and a[i]
    procedure Find_Best_Split (Self                  : in out Splitter_Class;
                               Num_Features          : Natural;
                               Num_Constant_Features : Natural;
