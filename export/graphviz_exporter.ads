e675c920c86d529e23dde54a9ac4368405a1a638
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-11-17 23:44:46 +1100

    Coding

--- export/graphviz_exporter.ads
+++ export/graphviz_exporter.ads
@@ -6,27 +6,57 @@ with Tree;
 
 package Graphviz_Exporter is
 
+   type DOT_Tree_Exporter is private;
+
    procedure Dot_To_Dot (Input_File_Name : String);
-   procedure Export_Graphviz (theTree            : Tree.Tree_Nodes;
-                              Output_File_Name   : Unbounded_String:=
-                                To_Unbounded_String ("tree.dot");
-                              Max_Depth          : Positive := Integer'Last;
-                              Feature_Names      : Feature_Names_List :=
-                                Unbounded_Package.Empty_Vector;
-                              Class_Names        : Class_Names_List
-                              := Unbounded_Package.Empty_Vector;
-                              Label              : Unbounded_String :=
-                                To_Unbounded_String ("all");
-                              Filled             : Boolean := False;
-                              Leaves_Parallel    : Boolean := False;
-                              Impurity           : Boolean := True;
-                              Node_Ids           : Boolean := False;
-                              Proportion         : Boolean := False;
-                              Rotate             : Boolean := False;
-                              Rounded            : Boolean := False;
-                              Special_Characters : Boolean := False;
-                              Precision          : Positive := 3;
-                              Font_Name          : Unbounded_String :=
-                                To_Unbounded_String ("helvetica"));
+   procedure Export_Graphviz (Exporter : DOT_Tree_Exporter);
+   procedure Init (Exporter           : in out DOT_Tree_Exporter;
+                   theTree            : Tree.Tree_Nodes;
+                   Output_File_Name   : Unbounded_String :=
+                     To_Unbounded_String ("tree.dot");
+                   Max_Depth          : Positive := Integer'Last;
+                   Feature_Names      : Feature_Names_List :=
+                     Unbounded_Package.Empty_Vector;
+                   Class_Names        : Class_Names_List :=
+                     Unbounded_Package.Empty_Vector;
+                   Label              : Unbounded_String :=
+                     To_Unbounded_String ("all");
+                   Filled             : Boolean := False;
+                   Leaves_Parallel    : Boolean := False;
+                   Impurity           : Boolean := True;
+                   Node_Ids           : Boolean := False;
+                   Proportion         : Boolean := False;
+                   Rotate             : Boolean := False;
+                   Rounded            : Boolean := False;
+                   Special_Characters : Boolean := False;
+                   Precision          : Positive := 3;
+                   Font_Name          : Unbounded_String :=
+                     To_Unbounded_String ("helvetica"));
+
+private
+
+   type DOT_Tree_Exporter is record
+      theTree            : Tree.Tree_Nodes;
+      Output_File_Name   : Unbounded_String := To_Unbounded_String ("tree.dot");
+      Max_Depth          : Positive := Integer'Last;
+      Feature_Names      : Feature_Names_List :=
+                             Unbounded_Package.Empty_Vector;
+      Class_Names        : Class_Names_List :=
+                             Unbounded_Package.Empty_Vector;
+      Label              : Unbounded_String :=
+                             To_Unbounded_String ("all");
+      Filled             : Boolean := False;
+      Leaves_Parallel    : Boolean := False;
+      Impurity           : Boolean := True;
+      Node_Ids           : Boolean := False;
+      Proportion         : Boolean := False;
+      Rotate             : Boolean := False;
+      Rounded            : Boolean := False;
+      Special_Characters : Boolean := False;
+      Precision          : Positive := 3;
+      Font_Name          : Unbounded_String :=
+                             To_Unbounded_String ("helvetica");
+      Initialized        : Boolean := False;
+   end record;
 
 end Graphviz_Exporter;
