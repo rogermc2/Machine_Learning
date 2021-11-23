b0b7a9ce757fdb8d760fd40779fcd989d0cefdfa
Roger Mc Murtrie <rogermc@grapevine.com.au>
2021-11-23 15:03:49 +1100

    Sub-routines moved out of Node_To_String

--- export/node_strings.adb
+++ export/node_strings.adb
@@ -9,7 +9,16 @@ with ML_Types;
 with Weights;
 
 package body Node_Strings is
-
+   procedure Write_Decision_Criteria
+     (Exporter    : Graphviz_Exporter.DOT_Tree_Exporter;
+      Node_ID     : Positive;
+      Node_Data   : Tree.Tree_Node;
+      Node_String : in out Unbounded_String);
+   procedure Write_Impurity (Exporter    : Graphviz_Exporter.DOT_Tree_Exporter;
+                             Show_Labels : Boolean;
+                             Node_Data   : Tree.Tree_Node;
+                             Criteria    : Criterion.Classifier_Criteria_Type;
+                             Node_String : in out Unbounded_String);
    procedure Write_Node_Class_Value
      (Exporter  : Graphviz_Exporter.DOT_Tree_Exporter;
       Node_Curs : Tree.Tree_Cursor;
@@ -18,6 +27,11 @@ package body Node_Strings is
      (Exporter    : Graphviz_Exporter.DOT_Tree_Exporter;
       Node_ID     : Positive; Show_Labels : Boolean;
       Node_String : in out Unbounded_String);
+   procedure Write_Node_Samples_Count
+     (Exporter            : Graphviz_Exporter.DOT_Tree_Exporter;
+      Node_Curs, Top_Node : Tree.Tree_Cursor;
+      Show_Labels         : Boolean;
+      Node_String         : in out Unbounded_String);
 
    --  -------------------------------------------------------------------------
    --  L269 Node_To_String generates the node content string
@@ -25,12 +39,11 @@ package body Node_Strings is
      (Exporter  : Graphviz_Exporter.DOT_Tree_Exporter;
       Node_Curs : Tree.Tree_Cursor;
       Criteria  : Criterion.Classifier_Criteria_Type) return String is
-      use Criterion;
       use ML_Types;
       use Tree;
       use Tree.Nodes_Package;
-      Routine_Name    : constant String :=
-                          "Node_Strings.Node_To_String ";
+      --        Routine_Name    : constant String :=
+      --                            "Node_Strings.Node_To_String ";
       Node_ID         : constant Positive := Element (Node_Curs).Node_ID;
       Node_ID_S       : constant String := Integer'Image (Node_ID);
       Top_Node        : constant Tree.Tree_Cursor :=
@@ -42,76 +55,7 @@ package body Node_Strings is
       Show_Labels     : constant Boolean
         := Exporter.Label = To_Unbounded_String ("all") or
         (Exporter.Label = To_Unbounded_String ("root") and Node_ID = 1);
-      Feature_Names   : constant ML_Types.Feature_Names_List :=
-                          Exporter.Feature_Names;
-      Feature         : Unbounded_String;
-      Percent         : Float;
       Node_String     : Unbounded_String := To_Unbounded_String ("");
-      Criteria_String : Unbounded_String := To_Unbounded_String ("");
-
-      procedure Write_Decision_Criteria is
-      begin
-         if Exporter.Feature_Names.Is_Empty then
-            --  L294
-            Feature := To_Unbounded_String ("X[") &
-              Feature_Names.Element (Node_ID) & "]";
-            Put_Line (Routine_Name & "Feature_Names is empty");
-         else
-            if not Exporter.theTree.Features.Is_Empty then
-               Feature := Exporter.theTree.Features.Element (Node_ID);
-            end if;
-         end if;
-
-         --  L299
-         Node_String := Node_String & Feature & " <= " &
-           Classifier_Utilities.Float_Precision
-           (Node_Data.Threshold, Exporter.Precision) & "\n";
-      end Write_Decision_Criteria;
-
-      --  ----------------------------------------------------------------------
-
-      procedure Write_Impurity is
-      begin
-         --  308  Write impurity
-         --           if Show_Labels then
-         --              Node_String := Node_String & "impurity = ";
-         --           end if;
-
-         if Show_Labels then
-            case Criteria is
-               when Gini_Criteria =>
-                  Criteria_String := To_Unbounded_String ("Gini");
-               when Entropy_Criteria =>
-                  Criteria_String := To_Unbounded_String ("Entropy");
-            end case;
-            Node_String := Node_String & Criteria_String & " = ";
-         end if;
-
-         Node_String := Node_String & Classifier_Utilities.Float_Precision
-           (Node_Data.Impurity, Exporter.Precision) & "\n";
-      end Write_Impurity;
-
-      --  ----------------------------------------------------------------------
-
-      procedure Write_Node_Samples_Count is
-      begin
-         --  L320
-         if Show_Labels then
-            Node_String := Node_String & "samples = ";
-         end if;
-
-         if Exporter.Proportion then
-            Percent := 100.0 * Float (Node_Data.Num_Node_Samples) /
-              Float (Element (Top_Node).Num_Node_Samples);
-            Node_String := Node_String &
-              Classifier_Utilities.Float_Precision (Percent, 1) & "%" & "\n";
-         else
-            Node_String := Node_String &
-              Integer'Image (Node_Data.Num_Node_Samples) & "\n";
-         end if;
-      end Write_Node_Samples_Count;
-
-      --  ----------------------------------------------------------------------
 
    begin
       --  L283
@@ -126,14 +70,16 @@ package body Node_Strings is
       --  L289
       if not Element (Node_Curs).Leaf_Node and then
         not Element (First_Child (Node_Curs)).Leaf_Node then
-         Write_Decision_Criteria;
+         Write_Decision_Criteria (Exporter, Node_ID, Node_Data, Node_String);
       end if;
 
       if Exporter.Impurity then
-         Write_Impurity;
+         Write_Impurity (Exporter, Show_Labels, Node_Data, Criteria,
+                         Node_String);
       end if;
 
-      Write_Node_Samples_Count;
+      Write_Node_Samples_Count (Exporter, Node_Curs, Top_Node, Show_Labels,
+                                Node_String);
       Write_Node_Class_Value (Exporter, Node_Curs, Node_ID, Show_Labels);
 
       --  L357
@@ -149,6 +95,65 @@ package body Node_Strings is
    end Node_To_String;
 
    --  -------------------------------------------------------------------------
+
+   procedure Write_Decision_Criteria
+     (Exporter    : Graphviz_Exporter.DOT_Tree_Exporter;
+      Node_ID     : Positive;
+      Node_Data   : Tree.Tree_Node;
+      Node_String : in out Unbounded_String) is
+      Routine_Name  : constant String :=
+                        "Node_Strings.Write_Decision_Criteria ";
+      Feature       : Unbounded_String;
+      Feature_Names : constant ML_Types.Feature_Names_List :=
+                        Exporter.Feature_Names;
+   begin
+      if Exporter.Feature_Names.Is_Empty then
+         --  L294
+         Feature := To_Unbounded_String ("X[") &
+           Feature_Names.Element (Node_ID) & "]";
+         Put_Line (Routine_Name & "Feature_Names is empty");
+      else
+         if not Exporter.theTree.Features.Is_Empty then
+            Feature := Exporter.theTree.Features.Element (Node_ID);
+         end if;
+      end if;
+
+      --  L299
+      Node_String := Node_String & Feature & " <= " &
+        Classifier_Utilities.Float_Precision
+        (Node_Data.Threshold, Exporter.Precision) & "\n";
+   end Write_Decision_Criteria;
+
+   --  ----------------------------------------------------------------------
+
+   procedure Write_Impurity (Exporter    : Graphviz_Exporter.DOT_Tree_Exporter;
+                             Show_Labels : Boolean;
+                             Node_Data   : Tree.Tree_Node;
+                             Criteria    : Criterion.Classifier_Criteria_Type;
+                             Node_String : in out Unbounded_String) is
+      use Criterion;
+      Criteria_String : Unbounded_String := To_Unbounded_String ("");
+   begin
+      --  308  Write impurity
+      --           if Show_Labels then
+      --              Node_String := Node_String & "impurity = ";
+      --           end if;
+
+      if Show_Labels then
+         case Criteria is
+            when Gini_Criteria =>
+               Criteria_String := To_Unbounded_String ("Gini");
+            when Entropy_Criteria =>
+               Criteria_String := To_Unbounded_String ("Entropy");
+         end case;
+         Node_String := Node_String & Criteria_String & " = ";
+      end if;
+
+      Node_String := Node_String & Classifier_Utilities.Float_Precision
+        (Node_Data.Impurity, Exporter.Precision) & "\n";
+   end Write_Impurity;
+
+   --  ----------------------------------------------------------------------
 
    procedure Write_Node_Class_Value
      (Exporter  : Graphviz_Exporter.DOT_Tree_Exporter;
@@ -237,8 +242,8 @@ package body Node_Strings is
      (Exporter    : Graphviz_Exporter.DOT_Tree_Exporter;
       Node_ID     : Positive; Show_Labels : Boolean;
       Node_String : in out Unbounded_String) is
-      Routine_Name : constant String :=
-                       "Node_Strings.Write_Node_Majority_Class ";
+      Routine_Name    : constant String :=
+                          "Node_Strings.Write_Node_Majority_Class ";
       --  Value: num_outputs x num_classes
       Value           : constant Weights.Weight_Lists_2D :=
                           Exporter.theTree.Values.Element (Node_ID);
@@ -269,5 +274,32 @@ package body Node_Strings is
    end Write_Node_Majority_Class;
 
    --  -------------------------------------------------------------------------
+
+   procedure Write_Node_Samples_Count
+     (Exporter            : Graphviz_Exporter.DOT_Tree_Exporter;
+      Node_Curs, Top_Node : Tree.Tree_Cursor;
+      Show_Labels         : Boolean;
+      Node_String         : in out Unbounded_String) is
+      use Tree.Nodes_Package;
+      Node_Data : constant Tree.Tree_Node := Element (Node_Curs);
+      Percent   : Float;
+   begin
+      --  L320
+      if Show_Labels then
+         Node_String := Node_String & "samples = ";
+      end if;
+
+      if Exporter.Proportion then
+         Percent := 100.0 * Float (Node_Data.Num_Node_Samples) /
+           Float (Element (Top_Node).Num_Node_Samples);
+         Node_String := Node_String &
+           Classifier_Utilities.Float_Precision (Percent, 1) & "%" & "\n";
+      else
+         Node_String := Node_String &
+           Integer'Image (Node_Data.Num_Node_Samples) & "\n";
+      end if;
+   end Write_Node_Samples_Count;
+
+   --  ----------------------------------------------------------------------
 
 end Node_Strings;
