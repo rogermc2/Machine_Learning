ad47e66a78df041dd4c24501d5259f54653d259c
rogermc2 <rogermc@iinet.net.au>
2021-12-04 21:19:40 +1100

    Debugging

--- examples/iris/src/classification.adb
+++ examples/iris/src/classification.adb
@@ -71,14 +71,14 @@ package body Classification is
       Put_Line ("----------------------------------------------");
       New_Line;
 
-      C_Init (theClassifier, Criterion.Gini_Criteria);
-      Classification_Fit (theClassifier, X, Y, No_Weights);
-      Put_Line (Routine_Name & ", Node_Count" & Count_Type'Image
-                (theClassifier.Attributes.Decision_Tree.Nodes.Node_Count - 1));
-      Print_Tree ("The Tree", theClassifier);
-      Put_Line ("----------------------------------------------");
-      New_Line;
-
+--        C_Init (theClassifier, Criterion.Gini_Criteria);
+--        Classification_Fit (theClassifier, X, Y, No_Weights);
+--        Put_Line (Routine_Name & ", Node_Count" & Count_Type'Image
+--                  (theClassifier.Attributes.Decision_Tree.Nodes.Node_Count - 1));
+--        Print_Tree ("The Tree", theClassifier);
+--        Put_Line ("----------------------------------------------");
+--        New_Line;
+--
       Graphviz_Exporter.C_Init
           (Exporter, theClassifier.Attributes.Decision_Tree);
 
