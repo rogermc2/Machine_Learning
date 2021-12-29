587020cfe1c7736dd5cd71b51c79062ea09115e8
rogermc2 <rogermc@iinet.net.au>
2021-12-29 13:09:59 +1100

    Update

--- decision_tree_classifier/test/classification_tests/src/test_classifier.adb
+++ decision_tree_classifier/test/classification_tests/src/test_classifier.adb
@@ -2,7 +2,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Classifier_Tests; use Classifier_Tests;
---  with Decision_Path_Tests; use Decision_Path_Tests;
+with Decision_Path_Tests; use Decision_Path_Tests;
 with Split_Tests; use Split_Tests;
 with Diabetes_Tests; use Diabetes_Tests;
 
@@ -21,12 +21,12 @@ begin
    Put_Line ("Test Iris started");
    Test_Iris;
    New_Line;
---     Put_Line ("Test Decision Path Hardcoded started");
---     Test_Decision_Path_Hardcoded;
---     New_Line;
---     Put_Line ("Test Decision Path started");
---     Test_Decision_Path;
---     New_Line;
+   Put_Line ("Test Decision Path Hardcoded started");
+   Test_Decision_Path_Hardcoded;
+   New_Line;
+   Put_Line ("Test Decision Path started");
+   Test_Decision_Path;
+   New_Line;
    Put_Line ("Test Min Samples Split started");
    Test_Min_Samples_Split;
    New_Line;
