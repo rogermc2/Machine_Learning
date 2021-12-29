1c71f8eb2ff0a42ae1689dfeeab271c3bef533a5
rogermc2 <rogermc@iinet.net.au>
2021-12-29 13:41:34 +1100

    Update

--- ML3a/src/lesson_3.adb
+++ ML3a/src/lesson_3.adb
@@ -1,47 +1,47 @@
 
-with Ada.Strings.Fixed;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Utilities;
 
 with ML_Types;
 
-with Classifier_Utilities;
+with Base_Decision_Tree;
+with Classifier_Types;
+with Criterion;
+with Decision_Tree_Classification;
 with Tree;
-with Decision_Tree_Classifer;
-with Estimator;
+with Weights;
 
 procedure Lesson_3 is
-   use Ada.Strings;
    use Utilities;
    use ML_Types;
-   use Decision_Tree_Classifer;
+   use Decision_Tree_Classification;
 
    Data_File    : File_Type;
 begin
    Put_Line ("Lesson 3");
    Open (Data_File, In_File, "src/diabetes.csv");
    declare
-      Line_1       : constant String := Get_Line (Data_File);
-      Num_Features : constant Integer := Fixed.Count (Line_1, ",");
-      Row_Data     : Rows_Vector;
-      Data         : Data_Record;
-      Weights      : constant Classifier_Utilities.Float_Array (1 .. Num_Features) :=
-                       (others => 0.0);
+      Row_Data      : Rows_Vector;
+      Data          : Data_Record;
+      Empty_Weights : Weights.Weight_List :=
+                          Classifier_Types.Float_Package.Empty_Vector;
    begin
       Put_Line ("Feature Names:");
       Utilities.Load_CSV_Data (Data_File, Row_Data);
       Data := Split_Row_Data (Row_Data);
       declare
-         aClassifier : Classifier
+         aClassifier : Base_Decision_Tree.Classifier
            (Tree.Integer_Type, Tree.Integer_Type, Tree.Integer_Type);
-         Estimate    : Estimator.Estimator_Data
-           (Positive (Row_Data.Length), Num_Features + 1);
       begin
          --  Fit function adjusts weights according to data values so that
          --  better accuracy can be achieved
-         Estimate := Decision_Tree_Classifer.Fit
-           (aClassifier, Data.Feature_Values, Data.Label_Values, Weights);
+         C_Init (aClassifier, "2", Criterion.Gini_Criteria);
+--           Classification_Fit (aClassifier, Iris_Features, Iris_Target,
+--                            No_Weights);
+--           Estimate := Base_Decision_Tree.Fit
+            Classification_Fit (aClassifier, Data.Feature_Values,
+                                Data.Label_Values, Empty_Weights);
       end;
    end;
 
