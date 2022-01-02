d33203738f5b4957e77f066d6df9c5eb5f5b85b0
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-01-02 23:38:17 +1100

    fixed Find_Best_Split Feature_Data_Type

--- decision_tree_classifier/support/classifier_utilities.adb
+++ decision_tree_classifier/support/classifier_utilities.adb
@@ -482,11 +482,12 @@ package body Classifier_Utilities is
    function Split_Raw_Data (Raw_Data    : ML_Types.Raw_Data_Vector;
                             Num_Outputs : Positive := 1)
                             return Multi_Output_Data_Record is
+      use Ada.Containers;
       use Ada.Strings;
       use Ada.Strings.Unbounded;
       use Value_Data_Package;
---        Routine_Name   : constant String :=
---                           "Classifier_Utilities.Split_Raw_Data ";
+      --        Routine_Name   : constant String :=
+      --                           "Classifier_Utilities.Split_Raw_Data ";
       aRow           : ML_Types.Unbounded_List := Raw_Data.First_Element;
       Num_Items      : constant Positive := Positive (aRow.Length);
       Num_Features   : constant Positive := Num_Items - Num_Outputs;
@@ -500,45 +501,46 @@ package body Classifier_Utilities is
    begin
       Parse_Header (aRow, Num_Features, Data);
       aRow := Raw_Data.Element (Positive'Succ (Raw_Data.First_Index));
-      for f_index in 1 .. Num_Features loop
-         declare
-            Row_S     : constant String := To_String (aRow (f_index));
-            S_Last    : constant Integer := Row_S'Last;
-            Last_Char : constant Character := Row_S (S_Last);
-         begin
-            if Character'Pos (Last_Char) < 32 then
-               aRow (f_index) := To_Unbounded_String (Row_S (1 .. S_Last - 1));
-            end if;
-            Feature_Types (Positive (f_index)) :=
-              Utilities.Get_Data_Type (aRow (Positive (f_index)));
-         end;
-      end loop;
-
-      for l_index in 1 .. Num_Outputs loop
-         declare
-            Row_S     : constant String := To_String (aRow (Num_Features + l_index));
-            S_Last    : constant Integer := Row_S'Last;
-            Last_Char : constant Character := Row_S (S_Last);
-         begin
-            if Character'Pos (Last_Char) < 32 then
-               aRow (Num_Features + l_index) := To_Unbounded_String (Row_S (1 .. S_Last - 1));
-            end if;
-            Label_Types (Positive (l_index)) :=
-              Utilities.Get_Data_Type (aRow (Positive (Num_Features + l_index)));
-         end;
-      end loop;
-
-      for row_index in Positive'Succ (Raw_Data.First_Index) ..
-        Raw_Data.Last_Index loop
-         aRow := Raw_Data.Element (row_index);  --  Unbound list
-
-         Feature_Values.Clear;
+      if aRow.Length > 1 then
          for f_index in 1 .. Num_Features loop
             declare
-               Feat_String : constant String := To_String (aRow (f_index));
-               Value       : Value_Record (Feature_Types (Positive (f_index)));
+               Row_S     : constant String := To_String (aRow (f_index));
+               S_Last    : constant Integer := Row_S'Last;
+               Last_Char : constant Character := Row_S (S_Last);
             begin
-               case Feature_Types (Positive (f_index)) is
+               if Character'Pos (Last_Char) < 32 then
+                  aRow (f_index) := To_Unbounded_String (Row_S (1 .. S_Last - 1));
+               end if;
+               Feature_Types (Positive (f_index)) :=
+                 Utilities.Get_Data_Type (aRow (Positive (f_index)));
+            end;
+         end loop;
+
+         for l_index in 1 .. Num_Outputs loop
+            declare
+               Row_S     : constant String := To_String (aRow (Num_Features + l_index));
+               S_Last    : constant Integer := Row_S'Last;
+               Last_Char : constant Character := Row_S (S_Last);
+            begin
+               if Character'Pos (Last_Char) < 32 then
+                  aRow (Num_Features + l_index) := To_Unbounded_String (Row_S (1 .. S_Last - 1));
+               end if;
+               Label_Types (Positive (l_index)) :=
+                 Utilities.Get_Data_Type (aRow (Positive (Num_Features + l_index)));
+            end;
+         end loop;
+
+         for row_index in Positive'Succ (Raw_Data.First_Index) ..
+           Raw_Data.Last_Index loop
+            aRow := Raw_Data.Element (row_index);  --  Unbound list
+
+            Feature_Values.Clear;
+            for f_index in 1 .. Num_Features loop
+               declare
+                  Feat_String : constant String := To_String (aRow (f_index));
+                  Value       : Value_Record (Feature_Types (Positive (f_index)));
+               begin
+                  case Feature_Types (Positive (f_index)) is
                   when Boolean_Type =>
                      Value.Boolean_Value := Boolean'Value (Feat_String);
                   when Integer_Type =>
@@ -547,32 +549,32 @@ package body Classifier_Utilities is
                      Value.Float_Value := Float'Value (Feat_String);
                   when UB_String_Type =>
                      Value.UB_String_Value := aRow (f_index);
-               end case;
-               Feature_Values.Append (Value);
-            end;  --  declare block
-         end loop;
-         Features_List.Append (Feature_Values);
+                  end case;
+                  Feature_Values.Append (Value);
+               end;  --  declare block
+            end loop;
+            Features_List.Append (Feature_Values);
 
-         for o_index in 1 .. Num_Outputs loop
-            Label_Values.Clear;
-            declare
-               Row_S     : constant String :=
-                             To_String (aRow (Num_Features + o_index));
-               S_Last    : constant Integer := Row_S'Last;
-               Last_Char : constant Character := Row_S (S_Last);
-            begin
-               if Character'Pos (Last_Char) < 32 then
-                  aRow (Num_Features + o_index) :=
-                    To_Unbounded_String (Row_S (1 .. S_Last - 1));
-               end if;
-            end;
+            for o_index in 1 .. Num_Outputs loop
+               Label_Values.Clear;
+               declare
+                  Row_S     : constant String :=
+                                To_String (aRow (Num_Features + o_index));
+                  S_Last    : constant Integer := Row_S'Last;
+                  Last_Char : constant Character := Row_S (S_Last);
+               begin
+                  if Character'Pos (Last_Char) < 32 then
+                     aRow (Num_Features + o_index) :=
+                       To_Unbounded_String (Row_S (1 .. S_Last - 1));
+                  end if;
+               end;
 
-            declare
-               Label       : constant String :=
-                               To_String (aRow (Num_Features + o_index));
-               Label_Value : Value_Record (Label_Types (o_index));
-            begin
-               case Label_Types (Positive (o_index)) is
+               declare
+                  Label       : constant String :=
+                                  To_String (aRow (Num_Features + o_index));
+                  Label_Value : Value_Record (Label_Types (o_index));
+               begin
+                  case Label_Types (Positive (o_index)) is
                   when Boolean_Type =>
                      Label_Value.Boolean_Value := Boolean'Value (Label);
                   when Integer_Type =>
@@ -582,15 +584,16 @@ package body Classifier_Utilities is
                   when UB_String_Type =>
                      Label_Value.UB_String_Value :=
                        aRow (Num_Features + o_index);
-               end case;
-               Label_Values.Append (Label_Value);
-            end;  --  declare block;
-            Labels_List.Append (Label_Values);
+                  end case;
+                  Label_Values.Append (Label_Value);
+               end;  --  declare block;
+               Labels_List.Append (Label_Values);
+            end loop;
          end loop;
-      end loop;
 
-      Data.Feature_Values := Features_List;
-      Data.Label_Values := Labels_List;
+         Data.Feature_Values := Features_List;
+         Data.Label_Values := Labels_List;
+      end if;
 
       return Data;
 
