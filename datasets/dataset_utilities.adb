100e6a6b29c6cfe2c70dc9cf2e8171224b7f5e21
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-01-07 12:17:37 +1100

    New package dataset_utilities

---
+++ datasets/dataset_utilities.adb
@@ -0,0 +1,21 @@
+
+with Ada.Characters.Handling;
+
+package body Dataset_Utilities is
+
+   function To_Lowercase (Text : String) return String is
+      use Ada.Characters.Handling;
+      LC : String := Text;
+   begin
+      for char in Text'First .. Text'Last loop
+         if Is_Upper (Text (char)) and then
+           Text (char) in 'A' .. 'Z' then
+            LC (char) := To_Lower (Text (char));
+         end if;
+      end loop;
+
+      return LC;
+
+   end To_Lowercase;
+
+end Dataset_Utilities;
---
+++ datasets/dataset_utilities.ads
@@ -0,0 +1,6 @@
+
+package Dataset_Utilities is
+
+   function To_Lowercase (Text : String) return String;
+
+end Dataset_Utilities;
