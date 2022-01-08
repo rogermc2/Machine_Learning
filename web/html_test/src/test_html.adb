72336296dae38cc81a3535edba405e366c1a74bb
rogermc2 <rogermc@iinet.net.au>
2022-01-08 21:43:55 +1100

    Update

--- web/html_test/src/test_html.adb
+++ web/html_test/src/test_html.adb
@@ -18,7 +18,8 @@ procedure Test_HTML is
     URL3          : constant String :=
       "http://www.openml.org/api/v1/json/data/554";
 begin
-    Res := AWS.Client.Get (URL3);
+    Res := AWS.Client.Get (URL1
+    );
     Page_Raw_Data := AWS.Response.Message_Body (Res);
     Html_Processor.Process_Request (CL_Config, Page_Raw_Data);
 
