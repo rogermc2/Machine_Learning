4e191af49dc731c4ddae10b8a2b7c919aafd266a
rogermc2 <rogermc@iinet.net.au>
2022-01-09 13:06:30 +1100

    Test_JSON_File initial coding update

--- json/json_file_test/src/test_json_file.adb
+++ json/json_file_test/src/test_json_file.adb
@@ -16,21 +16,24 @@ with Html_Processor;
 with Configure_AWS;
 with Html_Processor;
 
-procedure Test_JSON_File is
-   use  Ada.Command_Line;
-   use Configure_AWS;
-   Res           : AWS.Response.Data;
-   Page_Raw_Data : Unbounded_String;
-   CL_Config     : Config_Maps.Map;
-begin
 
-   if (Argument_Count > 0) then
-      for I in 2 .. Argument_Count loop
-         Add_Param (CL_Config, Argument (I));
-      end loop;
-      Res           := AWS.Client.Get (URL => Argument (1));
-      Page_Raw_Data := AWS.Response.Message_Body (Res);
-      Html_Processor.Process_Request (CL_Config, Page_Raw_Data);
-   end if;
+procedure Test_JSON_File is
+    use  Ada.Command_Line;
+    use Configure_AWS;
+--      URL1          : String := "http://www.openml.org/api/v1/json/data/554";
+    Res           : AWS.Response.Data;
+    Page_Raw_Data : Unbounded_String;
+    CL_Config     : Config_Maps.Map;
+begin
+    Put_Line ("URL: ");
+    declare
+        URL : String := Get_Line;
+    begin
+        Res := AWS.Client.Get (URL);
+    end;
+    New_Line;
+
+    Page_Raw_Data := AWS.Response.Message_Body (Res);
+    Html_Processor.Process_Request (CL_Config, Page_Raw_Data);
 
 end Test_JSON_File;
