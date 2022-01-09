6545cf6d17f831aec33307064a8e7d7168b59052
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-01-09 23:46:21 +1100

    oml_file_tests.Test_Data_Info working

--- great_courses/oml_file_test/src/oml_file_tests.adb
+++ great_courses/oml_file_test/src/oml_file_tests.adb
@@ -1,8 +1,5 @@
 
---  with Ada.Assertions; use Ada.Assertions;
---  with Ada.Characters.Handling;
---  with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
---  with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
 
 with GNATCOLL.JSON; use GNATCOLL.JSON;
 
@@ -18,12 +15,21 @@ package body OML_File_Tests is
     --     Data_File      : constant String := "data/v1/download/";
 
    procedure Test_Data_Info is
+      Routine_Name : constant String := "Test_Data_Info ";
       Dataset_Name : constant String := "mnist_784";
       Version      : constant String := "1";
       Data_Info    : JSON_Value;
+      JSON_Data_Id : JSON_Value;
+      Data_Id      : Integer := 0;
    begin
       Data_Info := Get_Data_Info_By_Name (Dataset_Name, Version,
                                           From_File => True);
+      JSON_Data_Id := Get (Data_Info, "data_id");
+      Put_Line (Routine_Name & "JSON_Data_Id kind: " &
+                  JSON_Value_Type'Image (JSON_Data_Id.Kind));
+      Data_Id := Integer'Value (Get (JSON_Data_Id));
+      Put_Line (Routine_Name & "Data_Id" & Integer'Image (Data_Id));
+
    end ;
 
 end OML_File_Tests;
