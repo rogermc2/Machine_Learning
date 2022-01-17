cbf52c1a156b77640e090cd9c6053ff064247898
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-01-17 17:17:43 +1100

    Added regex.Get_Groups

--- datasets/regex.adb
+++ datasets/regex.adb
@@ -1,4 +1,8 @@
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded.Text_IO; use Ada.Strings.Unbounded.Text_IO;
+with Ada.Text_IO; use Ada.Text_IO;
+
 package body Regex is
 
    procedure Find_Match
@@ -18,6 +22,31 @@ package body Regex is
         end if;
 
    end Find_Match;
+
+   --  -------------------------------------------------------------------------
+
+   function Get_Groups (Regex : String) return String is
+      use Gnat.Regpat;
+      Pattern  : constant String := "c";
+      Matcher  : constant Pattern_Matcher := Compile (Pattern);
+      UB_Regex : constant Unbounded_String := To_Unbounded_String (Regex);
+      First    : Positive;
+      Last     : Positive;
+      Found    : Boolean := True;
+      Groups   : Unbounded_String := To_Unbounded_String ("");
+   begin
+      while Found loop
+         Find_Match (Matcher, Regex, First, Last, Found);
+         if Found then
+            Put_Line ("Group found");
+            Groups := Groups & Slice (UB_Regex, First, Last);
+         end if;
+         Put_Line (Groups);
+      end loop;
+
+      return To_String (Groups);
+
+   end Get_Groups;
 
    --  -------------------------------------------------------------------------
 
--- great_courses/regex/src/regex_tester.adb
+++ great_courses/regex/src/regex_tester.adb
@@ -18,7 +18,7 @@ procedure Regex_Tester is
    --  () combines [a-zA-Z]+ into a group
    Word_Pattern  : constant String := "\\([0-9]{1,3}|u[0-9a-f]{4}|.)";
    Matcher       : constant Pattern_Matcher := Compile (Word_Pattern);
-   Str           : String := "\123 \test \u23af \. rt";
+   Str           : constant String := "\123 \test \u23af \. rt";
    Current_First : Positive := Str'First;
    First         : Positive;
    Last          : Positive;
@@ -26,7 +26,8 @@ procedure Regex_Tester is
    Curs          : Cursor;
    Found         : Boolean := True;
 begin
-   --  first, find all the words in Str
+   --  Find all the words in Str
+   Put_Line ("Find_Match results:");
    while Found loop
       Regex.Find_Match (Matcher, Str (Current_First .. Str'Last),
                         First, Last, Found);
@@ -39,21 +40,12 @@ begin
       Current_First := Last + 1;
    end loop;
 
---     second, replace "PATTERN" in Str by "pattern"
-       Regex.Find_Match (Compile ("(PATTERN)"), Str, First, Last, Found);
---         pragma Unreferenced (Found);
-
-   if Found then
-       Str :=
-         Str (Str'First .. First - 1) & "pattern" & Str (Last + 1 .. Str'Last);
-       Put_Line (Str);
-
-       New_Line;
-       Curs := Words.First;
-       while Has_Element (Curs) loop
-           Put_Line ("<<" & Element (Curs) & ">>");
-           Next (Curs);
-      end loop;
-   end if;
+   New_Line;
+   Put_Line ("String_List:");
+   Curs := Words.First;
+   while Has_Element (Curs) loop
+      Put_Line ("<<" & Element (Curs) & ">>");
+      Next (Curs);
+   end loop;
 
 end Regex_Tester;
