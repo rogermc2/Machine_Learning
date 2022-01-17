e367df4f75bdad84557f7297a64e1bc0b7de4342
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-01-17 23:11:55 +1100

    All_Matches function added

--- datasets/regex.adb
+++ datasets/regex.adb
@@ -1,32 +1,59 @@
 
+with Ada.Containers.Vectors;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Unbounded.Text_IO; use Ada.Strings.Unbounded.Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body Regex is
+   use GNAT.Regpat;
+
+   package Matches_Package is new Ada.Containers.Vectors
+     (Natural, Match_Location);
+   subtype Matches_List is Matches_Package.Vector;
+
+   function All_Matches
+     (Compiled_Expression : Gnat.Regpat.Pattern_Matcher; Text : String;
+      Found : out Boolean) return Matches_List is
+      Num_Parens : constant Natural := Paren_Count (Compiled_Expression);
+      Matches    : Match_Array (0 .. Num_Parens);
+      Result     : Matches_List;
+   begin
+      --  Match selects the first substring of Text that matches
+      --  the Compiled_Expression
+      Match (Compiled_Expression, Text, Matches);
+      Found := Matches (0) /= No_Match;
+
+      if Found then
+         for index in 0 .. Num_Parens loop
+            Result.Append (Matches (index));
+         end loop;
+      end if;
+
+      return Result;
+
+   end All_Matches;
+
+   --  -------------------------------------------------------------------------
 
    procedure Find_Match
-      (Compiled_Expression : Gnat.Regpat.Pattern_Matcher; Text : String;
-       First, Last : out Positive; Found : out Boolean) is
-      use Gnat.Regpat;
-      Result : Match_Array (0 .. Paren_Count (Compiled_Expression));
-    begin
-        --  Match selects the first substring of Text that matches
-        --  the Compiled_Expression
-        Match (Compiled_Expression, Text, Result);
-        Found := Result (0) /= No_Match;
-
-        if Found then
-            First := Result (0).First;
-            Last := Result (0).Last;
-        end if;
+     (Compiled_Expression : Gnat.Regpat.Pattern_Matcher; Text : String;
+      First, Last         : out Positive; Found : out Boolean;
+      Group_Index : Natural := 0) is
+      Matches : constant Matches_List
+        := All_Matches (Compiled_Expression, Text, Found);
+      Result  : Match_Location;
+   begin
+      if Found then
+         Result := Matches.Element (Group_Index);
+         First := Result.First;
+         Last := Result.Last;
+      end if;
 
    end Find_Match;
 
    --  -------------------------------------------------------------------------
 
    function Get_Groups (Regex : String) return String is
-      use Gnat.Regpat;
       Pattern  : constant String := "c";
       Matcher  : constant Pattern_Matcher := Compile (Pattern);
       UB_Regex : constant Unbounded_String := To_Unbounded_String (Regex);
--- datasets/regex.ads
+++ datasets/regex.ads
@@ -5,7 +5,8 @@ package Regex is
 
    procedure Find_Match
      (Compiled_Expression : Gnat.Regpat.Pattern_Matcher; Text : String;
-      First, Last         : out Positive; Found : out Boolean);
+      First, Last         : out Positive; Found : out Boolean;
+      Group_Index         : Natural := 0);
    function Get_Groups (Regex : String) return String;
 
 end Regex;
