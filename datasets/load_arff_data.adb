31c53b02852dd27288d14b7caa13b9b30a9130e3
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-02-12 13:48:03 +1100

    Coding

--- datasets/load_arff_data.adb
+++ datasets/load_arff_data.adb
@@ -1,40 +1,102 @@
 
-with Ada.Streams.Stream_IO;
+with Ada.Assertions; use Ada.Assertions;
+with Ada.IO_Exceptions;
+with Ada.Strings.Fixed;
 with Ada.Text_IO; use Ada.Text_IO;
-
-with ML_Types;
+with Ada.Text_IO.Unbounded_IO;
 
 package body Load_ARFF_Data is
 
-    subtype ARFF_Header is ML_Types.String_List;
+   procedure Load_ARFF_Header (File_ID : File_Type;
+                               Header  : out ARFF_Header_Record);
 
-    procedure Load_ARFF_Header (File_ID : Ada.Streams.Stream_IO.File_Type;
-                                Header  : out ARFF_Header);
+   --  ------------------------------------------------------------------------
 
-    --  ------------------------------------------------------------------------
+   procedure Load_ARFF (File_Name : String; Data : out ARFF_Record) is
+      Routine_Name : constant String := "Load_ARFF_Data.Load_ARFF ";
+      File_ID      : File_Type;
+      Header       : ARFF_Header_Record;
+   begin
+      Open (File_ID, In_File, File_Name);
+      Load_ARFF_Header (File_ID, Header);
+      Data.Header := Header;
+      Close (File_ID);
+      pragma Unreferenced (File_ID);
 
-    procedure Load_ARFF (File_Name : String; Data : out ARFF_Record) is
-        use Ada.Streams.Stream_IO;
-        File_ID      : Ada.Streams.Stream_IO.File_Type;
-        Data_Stream  : Ada.Streams.Stream_IO.Stream_Access;
-        Header       : ARFF_Header;
-    begin
-        Open (File_ID, In_File, File_Name);
-        Load_ARFF_Header (File_ID, Header);
+   exception
+      when Ada.IO_Exceptions.Name_Error  =>
+         Put_Line (Routine_Name & "can't find the file " & File_Name & "!");
+         raise;
+      when others =>
+         Put_Line ("An exception occurred in " & Routine_Name);
+         raise;
 
-        Close (File_ID);
+   end Load_ARFF;
 
-    end Load_ARFF;
+   --  ------------------------------------------------------------------------
 
-    --  ------------------------------------------------------------------------
+   procedure Load_ARFF_Header (File_ID : File_Type;
+                               Header  : out ARFF_Header_Record) is
+      use Unbounded_IO;
+      use Ada.Strings;
+      Routine_Name : constant String := "Load_ARFF_Data.Load_ARFF_Header ";
+      aLine        : Unbounded_String;
+      Is_Info      : Boolean := True;
+      Pos_1        : Positive;
+      Pos_2        : Positive;
+      Data_Kind    : Unbounded_String;
+      Attribute    : Attribute_Record;
+   begin
+      while Is_Info loop
+         aLine := Get_Line (File_ID);
+         Is_Info :=  Slice (aLine, 1, 1) = "%";
+         if Is_Info then
+            Header.Info.Append (Slice (aLine, 2, Length (aLine)));
+         end if;
+      end loop;
 
-    procedure Load_ARFF_Header (File_ID : Ada.Streams.Stream_IO.File_Type;
-                                Header  : out ARFF_Header) is
+      while Slice (aLine, 1, 1) /= "@" loop
+         Assert (Slice (aLine, 1, 9) = "@RELATION", Routine_Name &
+                   "invalid ARFF format, " & To_String (aLine) & " but " &
+                   " line beginning @RELATION expected");
+         Header.Relation :=
+           To_Unbounded_String (Slice (aLine, 11, Length (aLine)));
+         aLine := Get_Line (File_ID);
+      end loop;
 
-    begin
-        null;
-    end Load_ARFF_Header;
+      while Slice (aLine, 1, 1) /= "@" loop
+         aLine := Get_Line (File_ID);
+      end loop;
 
-    --  ------------------------------------------------------------------------
+      while Slice (aLine, 1, 1) = "@" loop
+         Assert (Slice (aLine, 1, 9) = "@ATTRIBUTE", Routine_Name &
+                   "invalid ARFF format, " & To_String (aLine) & " but " &
+                   " line beginning @ATTRIBUTE expected");
+         Pos_1 := 11;
+         Pos_2 := Fixed.Index (Slice (aLine, Pos_1, Length (aLine)), " ");
+         Attribute.Name := Trim (To_Unbounded_String
+                                 (Slice (aLine, Pos_1, Pos_2)), Both);
+         Pos_1 := Pos_2 + 1;
+         Data_Kind := Trim (To_Unbounded_String
+                            (Slice (aLine, Pos_1, Length (aLine))), Both);
+         if Data_Kind = To_Unbounded_String ("INTEGER") or
+           Data_Kind = To_Unbounded_String ("REAL")
+         then
+            Attribute.Data_Kind := ARFF_Numeric;
+         elsif Data_Kind = To_Unbounded_String ("DATE") then
+            Attribute.Data_Kind := ARFF_Date;
+         elsif Data_Kind = To_Unbounded_String ("STRING") then
+            Attribute.Data_Kind := ARFF_String;
+         elsif Slice (Data_Kind, 1, 1) = "{" then
+            null;
+         end if;
+
+         Header.Attributes.Append (Attribute);
+         aLine := Get_Line (File_ID);
+      end loop;
+
+   end Load_ARFF_Header;
+
+   --  ------------------------------------------------------------------------
 
 end Load_ARFF_Data;
--- datasets/load_arff_data.ads
+++ datasets/load_arff_data.ads
@@ -1,16 +1,38 @@
 
+with Ada.Containers.Doubly_Linked_Lists;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+with ML_Types;
 
 package Load_ARFF_Data is
 
-    type ARFF_Record is private;
+   type ARFF_Record is private;
+   type ARFF_Data_Type is (ARFF_Numeric, ARFF_Nominal, ARFF_String, ARFF_Date);
 
     procedure Load_ARFF (File_Name : String; Data : out ARFF_Record);
 
 private
 
+   subtype ARFF_Header is ML_Types.Indef_String_List;
+
+    type Attribute_Record is record
+        Name      : Unbounded_String;
+        Data_Kind : ARFF_Data_Type;
+   end record;
+
+   package Attribute_Data_Package is new
+     Ada.Containers.Doubly_Linked_Lists (Attribute_Record);
+   subtype Attribute_List is Attribute_Data_Package.List;
+
+    type ARFF_Header_Record is record
+        Info       : ARFF_Header;
+        Relation   : Unbounded_String;
+        Attributes : Attribute_List;
+   end record;
+
     type ARFF_Record is record
-        Header : Unbounded_String;
+        Header : ARFF_Header_Record;
+        Data   : ML_Types.Indef_String_List;
     end record;
 
 end Load_ARFF_Data;
