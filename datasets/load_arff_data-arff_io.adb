499e79669fd857e2170d00ed217a4cec3614ac44
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-02-16 11:49:21 +1100

    arff_io.Read_ARFF_Ada implemented

--- datasets/load_arff_data-arff_io.adb
+++ datasets/load_arff_data-arff_io.adb
@@ -6,6 +6,24 @@ package body Load_ARFF_Data.ARFF_IO is
 
    --  -------------------------------------------------------------------------
 
+   procedure Read_ARFF_Ada (File_Name : String; Data : out ARFF_Record) is
+      use Ada.Streams.Stream_IO;
+      Routine_Name : constant String := "Load_ARFF_Data.Read_ARFF_Ada ";
+      File_ID      : Ada.Streams.Stream_IO.File_Type;
+      Data_Stream  : Stream_Access;
+   begin
+      Open (File_ID, In_File, File_Name);
+      Data_Stream := Stream (File_ID);
+      ARFF_Record'Read (Data_Stream, Data);
+      Close (File_ID);
+      pragma Unreferenced (File_ID);
+
+      Put_Line (Routine_Name & File_Name & " loaded");
+
+   end Read_ARFF_Ada;
+
+   --  -------------------------------------------------------------------------
+
    procedure Save_ARFF (File_Name : String; Data : ARFF_Record) is
       use Ada.Streams.Stream_IO;
       Routine_Name : constant String := "Load_ARFF_Data.Save_ARFF ";
--- great_courses/arff_test/src/test_arff.adb
+++ great_courses/arff_test/src/test_arff.adb
@@ -1,4 +1,5 @@
 
+with Ada.Directories;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Load_ARFF_Data;
@@ -6,22 +7,30 @@ with Load_ARFF_Data.ARFF_IO;
 with Load_ARFF_Data.ARFF_Printing;
 
 procedure Test_ARFF is
+   use Ada.Directories;
    use Load_ARFF_Data;
    use Load_ARFF_Data.ARFF_IO;
    use Load_ARFF_Data.ARFF_Printing;
    Routine_Name : constant String := "Test_ARFF ";
    File_Name    : constant String := "../iris.arff";
+   Ada_File     : constant String := "iris.ada";
    Data         : ARFF_Record;
 begin
    Put_Line (Routine_Name);
-   Load_ARFF (File_Name, Data);
-   Put_Line (Routine_Name & "Data loaded");
+   if Exists (Ada_File) then
+      Read_ARFF_Ada (Ada_File, Data);
+      Put_Line (Routine_Name & "Data file read");
+   else
+      Load_ARFF (File_Name, Data);
+      Put_Line (Routine_Name & "Data loaded");
+      Save_ARFF (Ada_File, Data);
+   end if;
+
    New_Line;
    Print_Description (Data);
    Put_Line ("Relation: " & Get_Relation (Data));
    Print_Attributes (Data);
-   Print_Data (Data);
---     Print_Data (Data, 40, 100);
-   Save_ARFF ("iris.ada", Data);
+--     Print_Data (Data);
+   Print_Data (Data, 40, 100);
 
 end Test_ARFF;
