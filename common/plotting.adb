71aaebb10513f7e31fc49f7c6d011b7d412d3ca3
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-02-26 01:39:24 +1100

    Update

--- common/plotting.adb
+++ common/plotting.adb
@@ -5,13 +5,8 @@ with PLplot_Auxiliary; use PLplot_Auxiliary;
 with PLplot_Standard; use PLplot_Standard;
 
 package body Plotting is
-   
-   width  : constant Integer := 311;  -- columns
-   height : constant Integer := 240;  -- rows
-   num_col : constant Integer := 255; -- number of colors
 
-   --  Set gray colormap.
-   procedure gray_cmap (num_col : Integer) is
+   procedure Set_Grey_Colourmap (num_col : Integer) is
       r, g, b, pos : Real_Vector(0 .. 1);
    begin
       r (0) := 0.0;
@@ -28,21 +23,23 @@ package body Plotting is
       Set_Number_Of_Colors_In_Color_Map_1 (num_col);
       Set_Color_Map_1_Piecewise (RGB, pos, r, g, b, Alt_Hue_Path_None);
       
-   end gray_cmap;
+   end Set_Grey_Colourmap;
 
    procedure Plot (Bitmap : ML_Types.Integer_List_2D) is
-      image  : Real_Matrix(0 .. 310, 0 .. 239);
+      Num_Rows    : constant Positive := Positive (Bitmap.Length);
+      Num_Cols    : constant Positive := Positive (Bitmap.Element (1).Length);
+      Num_Colours : constant Integer := 255;
+      Image       : Real_Matrix(0 .. Num_Rows - 1, 0 .. Num_Cols - 1);
    begin
       Initialize_PLplot;
-      --  Set gray colormap.
-      gray_cmap (num_col);
+      Set_Grey_Colourmap (Num_Colours);
 
-      Set_Environment (1.0, Long_Float (width), 1.0, Long_Float (height),
+      Set_Environment (1.0, Long_Float (Num_Cols), 1.0, Long_Float (Num_Rows),
                        Justified, Box);
       --     Write_Labels ("", " ", "");
       Draw_Image_Color_Map_1_Automatic
-        (image, 1.0, Long_Float (width), 1.0, Long_Float (height), 0.0, 0.0, 1.0,
-         Long_Float (width), 1.0, Long_Float (height));
+        (Image, 1.0, Long_Float (Num_Cols), 1.0, Long_Float (Num_Rows),
+         0.0, 0.0, 1.0, Long_Float (Num_Cols), 1.0, Long_Float (Num_Rows));
 
       End_PLplot;
    
