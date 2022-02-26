8b25b2c2aea489d7037a2a7f241aba5cb159c5f7
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-02-26 11:07:16 +1100

    Update

--- common/plotting.adb
+++ common/plotting.adb
@@ -6,24 +6,18 @@ with PLplot_Standard; use PLplot_Standard;
 
 package body Plotting is
 
-   procedure Set_Grey_Colourmap (num_col : Integer) is
-      r, g, b, pos : Real_Vector(0 .. 1);
+   procedure Set_Grey_Colourmap (Num_Colours : Integer) is
+      R   : constant Real_Vector (0 .. 1) := (0.0, 1.0);
+      G   : constant Real_Vector (0 .. 1) := (0.0, 1.0);
+      B   : constant Real_Vector (0 .. 1) := (0.0, 1.0);
+      Pos : constant Real_Vector (0 .. 1) := (0.0, 1.0);
    begin
-      r (0) := 0.0;
-      g (0) := 0.0;
-      b (0) := 0.0;
-
-      r (1) := 1.0;
-      g (1) := 1.0;
-      b (1) := 1.0;
-
-      pos (0) := 0.0;
-      pos (1) := 1.0;
-
-      Set_Number_Of_Colors_In_Color_Map_1 (num_col);
-      Set_Color_Map_1_Piecewise (RGB, pos, r, g, b, Alt_Hue_Path_None);
+      Set_Number_Of_Colors_In_Color_Map_1 (Num_Colours);
+      Set_Color_Map_1_Piecewise (RGB, Pos, R, G, B, Alt_Hue_Path_None);
       
    end Set_Grey_Colourmap;
+   
+   --  -------------------------------------------------------------------------
 
    procedure Plot (Bitmap : ML_Types.Integer_List_2D) is
       Num_Rows    : constant Positive := Positive (Bitmap.Length);
