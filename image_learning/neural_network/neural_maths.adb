e06922183a4fe8d31a31d27cee589aada05c0a15
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-03-17 12:44:27 +1100

    Psi_Cephes coded

--- image_learning/neural_network/neural_maths.adb
+++ image_learning/neural_network/neural_maths.adb
@@ -8,6 +8,8 @@ with Weights;
 
 package body Neural_Maths is
 
+   type Float_Array is array (Integer range <>) of Float;
+
 --     Small_Abs    : constant Integer := 16;
 --     Small_Imag   : constant Integer := 6;
    Tol          : constant Float := 2.220446092504131 * 10.0 ** (-16);
@@ -19,7 +21,7 @@ package body Neural_Maths is
    Neg_Root     : constant Float := -0.504083008264455409;
    Neg_Root_Val : constant Float := 7.2897639029768949 * 10.0 ** (-17);
 
-   function Psi_AS103 (X : Float) return Float;  --  Digamma AS103
+--     function Psi_AS103 (X : Float) return Float;  --  Digamma AS103
    function Psi_Cephes (X : Float) return Float;
    function Zeta_Series (Z, Root, Root_Val : Float) return Float;
 
@@ -31,7 +33,7 @@ package body Neural_Maths is
       if abs (Z - Neg_Root) < 0.3 then
          Result := Zeta_Series (Z, Neg_Root, Neg_Root_Val);
       else
-         Result := Psi_AS103 (Z);
+         Result := Psi_Cephes (Z);
       end if;
 
       return Result;
@@ -77,39 +79,60 @@ package body Neural_Maths is
 
    end Mean;
 
+   --  ------------------------------------------------------------------------
+   --  Based on github.com/scipy/scipy/blob/main/scipy/special/cephes/polevl.h
+   --  Pol_Eval evaluates polynomial of degree N:
+   --  y  =  C0  + C1 x + C2 x^2  +...+ CN x^N
+   --  Coefficients are stored in reverse order:
+   --  coef [0] = CN  , ..., coef[N] = C0
+   function Pol_Eval (X : Float; Coeff : Float_Array; N : Positive) return Float is
+      Result : Float := 0.0;
+   begin
+      for index in Coeff'First .. Coeff'Last loop
+         Result := Coeff (index) * Result;
+      end loop;
+
+      for index in reverse 1 .. N loop
+         Result := Result * X + Coeff (index) * Result;
+      end loop;
+
+      return Result;
+
+   end Pol_Eval;
+
    --  -------------------------------------------------------------------------
    --  Based on https://people.sc.fsu.edu/~jburkardt/f77_src/asa103/asa103.f
    --  Applied Statistics, Volume 25, Number 3, 1976, pages 315-317
-   function Psi_AS103 (X : Float) return Float is
-      use Maths.Float_Math_Functions;
-      Routine_Name : constant String := "Neural_Maths.Psi_103 ";
-      Euler_Mascheroni : constant Float := 0.5772156649015328606;
-      R                : Float;
-      X2               : Float := X;
-      Digamma          : Float := 0.0;
-   begin
-      Assert (X > 0.0, Routine_Name & "X," & Float'Image (X) &
-                " should be greater than zero.");
-
-      if X <= 0.00001 then
-         Digamma := -Euler_Mascheroni - 1.0 / X2;
-      else
-         --  Reduce to Digamma (X + N).
-         while X2 < 8.5 loop
-            Digamma := Digamma - 1.0 / X2;
-            X2 := X2 + 1.0;
-         end loop;
-
-         --  Use Stirling's (actually de Moivre's) expansion.
-         R := 1.0 / X2;
-         Digamma := Digamma + Log (X2) - 0.5 * R;
-         R := R ** 2;
-         Digamma := Digamma - R / 12.0  - R / 120.0 - R / 252.0;
-      end if;
-
-      return Digamma;
-
-   end Psi_AS103;
+--     function Psi_AS103 (X : Float) return Float is
+--        use Maths.Float_Math_Functions;
+--        Routine_Name : constant String := "Neural_Maths.Psi_103 ";
+--        Euler_Mascheroni : constant Float := 0.5772156649015328606;
+--        R                : Float;
+--        X2               : Float := X;
+--        Digamma          : Float := 0.0;
+--     begin
+--        Assert (X > 0.0, Routine_Name & "X," & Float'Image (X) &
+--                  " should be greater than zero.");
+--
+--        if X <= 0.00001 then
+--           Digamma := -Euler_Mascheroni - 1.0 / X2;
+--        else
+--           --  Reduce to Digamma (X + N).
+--           while X2 < 8.5 loop
+--              Digamma := Digamma - 1.0 / X2;
+--              X2 := X2 + 1.0;
+--           end loop;
+--
+--           --  Use Stirling's (actually de Moivre's) expansion.
+--           R := 1.0 / X2;
+--           Digamma := Digamma + Log (X2) - 0.5 * R;
+--           R := R ** 2;
+--           Digamma := Digamma - R / 12.0  - R / 120.0 - R / 252.0;
+--        end if;
+--
+--        return Digamma;
+--
+--     end Psi_AS103;
 
    --  ------------------------------------------------------------------------
    --  Based on github.com/poliastro/cephes/blob/master/src/psi.c
@@ -120,26 +143,35 @@ package body Neural_Maths is
       use Ada.Numerics;
       use Maths.Float_Math_Functions;
       Routine_Name : constant String := "Neural_Maths.Psi_Cephes ";
-      Euler    : constant Float := 0.57721566490153286061;
+      Euler        : constant Float := 0.57721566490153286061;
+      A            : constant Float_Array (0 .. 6) :=
+                       ( 0.00833333333333333333333,
+                         -0.00210927960927960927961,
+                         0.000757575757575757575758,
+                         -0.000416666666666666666667,
+                         0.000396825396825396825397,
+                         -0.000833333333333333333333,
+                         0.00833333333333333333333);
+      Negative : constant Boolean := X <= 0.0;
       X2       : Float := X;
-      Q        : Float := X;
-      P        : Float := Float'Floor (Q);
+      P        : Float := Float'Floor (X);
       NZ       : Float := 0.0;
       N        : Positive;
-      W        : Positive;
-      Negative : Boolean := X <= 0.0;
-      Y        : Float := 0.0;
+      W        : Float;
+      S        : Float;
+      Z        : Float;
+      Result   : Float := 0.0;
    begin
       if Negative then
-            Assert (P /= Q, Routine_Name & "X should be > Floor (X).");
+            Assert (P /= X, Routine_Name & "X should be > Floor (X).");
             --  Remove the zeros of Tan (PI x) by subtracting the nearest
             --  integer from x
-            NZ := Q - P;
+            NZ := X - P;
             if NZ = 0.5 then
                 NZ := 0.0;
             elsif NZ > 0.5 then
                 P := P + 1.0;
-                NZ := Q - P;
+                NZ := X - P;
             elsif NZ < 0.5 then
                 NZ := Pi / Tan (Pi / NZ);
             end if;
@@ -150,14 +182,27 @@ package body Neural_Maths is
       if X2 <= 10.0 and then X2 = Float'Floor (X2) then
             N := Positive (X2);
             for index in 1 .. N loop
-                Y := Y + 1.0 / Float (index);
+                Result := Result + 1.0 / Float (index);
             end loop;
-            Y := Y - Euler;
+            Result := Result - Euler;
       else
-            null;
+         S := X2;
+         W := 0.0;
+         while S < 10.0 loop
+            W := W + 1.0 / S;
+            S := S + 1.0;
+         end loop;
+
+         if S < 10.0 ** 17 then
+            Z := 1.0 / (S ** 2);
+            Result := Z * Pol_Eval (Z, A, 6);
+         else
+            Result := 0.0;
+         end if;
+
       end if;
 
-      return Y;
+      return Result;
 
    end Psi_Cephes;
 
