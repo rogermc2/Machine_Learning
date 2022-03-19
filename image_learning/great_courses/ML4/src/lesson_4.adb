40e9324d4a5e31e8ab90c7a34ee51a03165156d7
rogermc2 <rogermc@iinet.net.au>
2022-03-19 21:09:49 +1100

    Updated for image_learning

--- image_learning/great_courses/ML4/src/lesson_4.adb
+++ image_learning/great_courses/ML4/src/lesson_4.adb
@@ -6,28 +6,22 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with IL_Types; use IL_Types;
 
-with Base_Decision_Tree;
-with Criterion;
+with Base_Neural;
 with Data_Splitter;
-with Decision_Tree_Classification;
---  with Graphviz_Exporter;
 --  with Load_ARFF_Data;
+with Multilayer_Perceptron;
 with Openml_Ada;
-with Printing;
-with Tree;
+--  with Printing;
 with Utilities;
-with Weights;
 
 with Support_4;
 
 procedure Lesson_4 is
    use Ada.Containers;
    use Support_4;
-   use Decision_Tree_Classification;
+   use Multilayer_Perceptron;
    Routine_Name    : constant String := "Lesson_4 ";
    Dataset_Name    : constant String := "mnist_784";
-   Return_X_Y      : constant Boolean := True;
-   Min_Split       : constant String := "2";
    Test_Size       : constant Positive := 1000;
    Train_Size      : constant Positive := 5000;
    Bunch           : Openml_Ada.Bunch_Data;
@@ -40,13 +34,10 @@ procedure Lesson_4 is
    Train_Y         : Integer_List;
    Test_X          : Float_List_2D;
    Test_Y          : Integer_List;
-   aClassifier     : Base_Decision_Tree.Classifier
-     (Tree.Integer_Type, Tree.Integer_Type, Tree.Integer_Type);
-   No_Weights      : Weights.Weight_List := Float_Package.Empty_Vector;
-   Prediction_List : Integer_List;
-   Prediction      : Integer;
-   Correct         : Natural := 0;
---     Exporter        : Graphviz_Exporter.DOT_Tree_Exporter;
+   aClassifier     : MLP_Classifier;
+--     Prediction_List : Integer_List;
+--     Prediction      : Integer;
+--     Correct         : Natural := 0;
 
 begin
    Put_Line (Routine_Name);
@@ -80,10 +71,9 @@ begin
                   Bunch);
    end if;
 
-   if not Get_Classifier (Dataset_Name, aClassifier) then
-      if not Return_X_Y then
-         Printing.Print_Strings ("Features", Bunch.Feature_Names);
-      end if;
+--     if not Get_Classifier (Dataset_Name, aClassifier) then
+--           Printing.Print_Strings ("Features", Bunch.Feature_Names);
+
       --        Printing.Print_Float_List ("Train features row 16", Train_X.Element (16));
       --        Printing.Print_Float_List ("Test features row 16", Test_X.Element (16));
 
@@ -94,32 +84,32 @@ begin
       --        Plotting.Display_Image (Train_X.Element (4));
       --        Plotting.Display_Image (Test_X.Element (4));
 
-      C_Init (aClassifier, Min_Split, Criterion.Gini_Criteria,
-              Max_Leaf_Nodes => 170);
+      aClassifier := C_Init (Max_Iter => 10000,
+                             Activation => Base_Neural.Identity_Activation);
 
       --  Fit function adjusts weights according to data values so that better
       --  accuracy can be achieved
       Put_Line ("Classification_Fit");
-      Classification_Fit (aClassifier, Train_X, Train_Y, No_Weights);
-      Support_4.Save_Classifier (Dataset_Name, aClassifier);
-   end if;
+      Fit (aClassifier, Train_X, Train_Y);
+--        Support_4.Save_Classifier (Dataset_Name, aClassifier);
+--     end if;
 
    Put_Line ("----------------------------------------------");
    New_Line;
 
    Put_Line ("Train data length: " & Count_Type'Image (Train_X.Length));
    Put_Line ("Test data length: " & Count_Type'Image (Test_X.Length));
-   Prediction_List := Base_Decision_Tree.Predict (aClassifier, Train_X);
-   for index in Train_X.First_Index .. Train_X.Last_Index loop
-      --        Put_Line (Routine_Name & "Train_X index" & Integer'Image (index));
-      Prediction := Prediction_List.Element (index);
-      if Prediction = Train_Y.Element (index) then
-         Correct := Correct + 1;
-      end if;
-   end loop;
-
-   Put_Line ("Prediction: " &
-               Float'Image (100.0 * Float (Correct) / Float (Train_X.Length)));
+--     Prediction_List := Predict (aClassifier, Train_X);
+--     for index in Train_X.First_Index .. Train_X.Last_Index loop
+--        --        Put_Line (Routine_Name & "Train_X index" & Integer'Image (index));
+--        Prediction := Prediction_List.Element (index);
+--        if Prediction = Train_Y.Element (index) then
+--           Correct := Correct + 1;
+--        end if;
+--     end loop;
+--
+--     Put_Line ("Prediction: " &
+--                 Float'Image (100.0 * Float (Correct) / Float (Train_X.Length)));
    New_Line;
 
 --     Graphviz_Exporter.C_Init
