bad8daa4488868677e0b9693d7c9525a53da1be6
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-04-08 22:20:24 +1000

    Update

--- neural_a_m_learning/mixture/base_mix.adb
+++ neural_a_m_learning/mixture/base_mix.adb
@@ -7,8 +7,8 @@ package body Base_Mix is
    --  -------------------------------------------------------------------------
     --  L498 _estimate_log_prob_resp computes the log-probabilities per
    --   component for each sample.
-   function Estimate_Log_Prob (X : Float_List_2D) return Float_List_2D is
-      Result : constant Float_List_2D := X;  --  dummy
+   function Estimate_Log_Prob (X : Float_Matrix) return Float_Matrix is
+      Result : constant Float_Matrix := X;  --  dummy
    begin
 
       return Result;
@@ -21,24 +21,28 @@ package body Base_Mix is
    --  Log_Prob_Norm for num_samples
    --  Log_Responsibil for num_samples x num_components
    procedure Estimate_Log_Prob_Resp
-      (X : Float_List_2D; Log_Prob_Norm : out Float_List;
-       Log_Responsibil : out Float_List_2D ) is
-      use Float_List_Package;
-      Weighted_Log_Prob : constant Float_List_2D :=
+      (X : Float_Matrix; Log_Prob_Norm : out Float_Array;
+       Log_Responsibil : out Float_Matrix) is
+      Weighted_Log_Prob : constant Float_Matrix :=
                               Estimate_Weighted_Log_Prob (X);
-      Log_Prob_Norm_2D  : Float_List_2D;
    begin
       Log_Prob_Norm := Neural_Maths.Log_Sum_Exponent (Weighted_Log_Prob);
-      Log_Prob_Norm_2D.Append (Log_Prob_Norm);
-      Log_Responsibil := Weighted_Log_Prob - Log_Prob_Norm_2D;
+      declare
+         Log_Prob_Norm_M  : Float_Matrix (1 .. 1, 1 .. Log_Prob_Norm'Length);
+      begin
+         for index in Log_Prob_Norm'First .. Log_Prob_Norm'Last loop
+            Log_Prob_Norm_M (1, index) := Log_Prob_Norm (index);
+         end loop;
+         Log_Responsibil := Weighted_Log_Prob - Log_Prob_Norm_M;
+      end;
 
    end Estimate_Log_Prob_Resp;
 
    --  -------------------------------------------------------------------------
 
     --  L474  BaseMixture.Estimate_Log_Prob
-   function Estimate_Weighted_Log_Prob (X : Float_List_2D)
-                                        return Float_List_2D is
+   function Estimate_Weighted_Log_Prob (X : Float_Matrix)
+                                        return Float_Matrix is
    begin
 
       return Estimate_Log_Prob (X); -- + Estimate_Log_Weights;
@@ -48,7 +52,7 @@ package body Base_Mix is
    --  -------------------------------------------------------------------------
    --  L356 Score returns the Log-likelihood of X (Float) under the Gaussian
    --  mixture model.
-   function Score (X : Float_List_2D) return Float is
+   function Score (X : Float_Matrix) return Float is
    begin
       return Neural_Maths.Mean (Score_Samples (X));
 
@@ -56,7 +60,7 @@ package body Base_Mix is
 
    --  -------------------------------------------------------------------------
    --  L337 returns Float_List num_samples
-   function Score_Samples (X : Float_List_2D) return Float_List is
+   function Score_Samples (X : Float_Matrix) return Float_Array is
         use Neural_Maths;
    begin
 
