ce5f832ccd3a45c7c748935f923bf00da55aa38d
rogermc2 <rogermc@iinet.net.au>
2022-04-12 11:12:52 +1000

    Fixed nl_arrays_and_matrices.dot

--- neural_a_m_learning/common/nl_arrays_and_matrices.adb
+++ neural_a_m_learning/common/nl_arrays_and_matrices.adb
@@ -1,301 +1,303 @@
 
-with Ada.Assertions; use Ada.Assertions;
+--  with Ada.Assertions; use Ada.Assertions;
 
 package body NL_Arrays_And_Matrices is
 
-   function "*" (L : Float; R : Float_Matrix) return Float_Matrix is
-      Result : Float_Matrix (R'First .. R'Last, R'First (2) .. R'Last (2));
-   begin
-      for row in R'First .. R'Last loop
-         for col in R'First (2) .. R'Last (2) loop
-            Result (row, col) := L * R (row, col);
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "*";
-
-   --  ------------------------------------------------------------------------
-
-   function "*" (L, R : Float_Matrix) return Float_Matrix is
-      Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
-   begin
-      for row in L'First .. L'Last loop
-         for col in L'First (2) .. L'Last (2) loop
-            Result (row, col) := L (row, col) * R (row, col);
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "*";
-
-   --  ------------------------------------------------------------------------
-
-   function "**" (L : Float_Array; P : Integer) return Float_Array is
-      Result : Float_Array := L;
-   begin
-      for row in Result'First .. Result'Last loop
-         Result (row) := Result (row) ** P;
-      end loop;
-
-      return Result;
-
-   end "**";
-
-   --  ------------------------------------------------------------------------
-
-   function "**" (L : Float_Matrix; P : Integer) return Float_Matrix is
-      Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
-   begin
-      for row in L'First .. L'Last loop
-         for col in L'First (2) .. L'Last (2) loop
-            Result (row, col) := L (row, col) ** P;
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "**";
-
-   --  ------------------------------------------------------------------------
-
-   function "/" (L : Float_Matrix; R : Float) return Float_Matrix is
-      Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
-   begin
-      for row in L'First .. L'Last loop
-         for col in L'First (2) .. L'Last (2) loop
-            Result (row, col) := L (row, col) / R;
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "/";
-
-   --  ------------------------------------------------------------------------
-
-   function "/" (L, R : Float_Matrix) return Float_Matrix is
-      Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
-   begin
-      for row in L'First .. L'Last loop
-         for col in L'First (2) .. L'Last (2) loop
-            Result (row, col) := L (row, col) / R (row, col);
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "/";
-
-   --  ------------------------------------------------------------------------
-
-   function "+" (L, R : Float_Array) return Float_Array is
-      Result : Float_Array (L'First .. L'Last);
-   begin
-      for row in L'First .. L'Last loop
-         Result (row) := L (row) + R (row);
-      end loop;
-
-      return Result;
-
-   end "+";
-
-   --  ------------------------------------------------------------------------
-
-   function "+" (L, R : Float_Matrix) return Float_Matrix is
-      Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
-   begin
-      for row in L'First .. L'Last loop
-         for col in L'First (2) .. L'Last (2) loop
-            Result (row, col) := L (row, col) + R (row, col);
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "+";
-
-   --  ------------------------------------------------------------------------
-
-   function "-" (L, R : Float_Matrix) return Float_Matrix is
-      Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
-   begin
-      for row in L'First .. L'Last loop
-         for col in L'First (2) .. L'Last (2) loop
-            Result (row, col) := L (row, col) - R (row, col);
-         end loop;
-      end loop;
-
-      return Result;
-
-   end "-";
-
-   --  ------------------------------------------------------------------------
-
-   function Dot (L, R : Float_Matrix) return Float_Matrix is
-      Routine_Name : constant String := "NL_Arrays_And_Matrices.Dot ";
-      Num_Rows : constant Positive := L'Length;
-      Num_Cols : constant Positive := L'Length (2);
-      RT       : constant Float_Matrix := Transpose (R);
-      L_Row    : Float_Array (1 .. Num_Cols);
-      LR       : Float;
-      Product  : Float_Matrix  (1 .. Num_Rows, 1 .. Num_Cols);
-   begin
-      Assert (R'Length (2) = Num_Rows, Routine_Name &
-      "Num columns" & Integer'Image (R'Length (2)) & " of right matrix " &
-      "don't equal num rows" & Integer'Image (Num_Rows) & " of left matrix");
-      for row in Product'Range loop
-         for col in Product'Range (2)loop
-            L_Row (col) := L (row, col);
-            LR := 0.0;
-            for lr_index in L_Row'Range loop
-               LR := LR + L_Row (lr_index) * RT (lr_index, row);
+    function "*" (L : Float; R : Float_Matrix) return Float_Matrix is
+        Result : Float_Matrix (R'First .. R'Last, R'First (2) .. R'Last (2));
+    begin
+        for row in R'First .. R'Last loop
+            for col in R'First (2) .. R'Last (2) loop
+                Result (row, col) := L * R (row, col);
             end loop;
-            Product (row, col) := LR;
-         end loop;
-      end loop;
+        end loop;
 
-      return Product;
+        return Result;
 
-   end Dot;
+    end "*";
 
-   --  ----------------------------------------------------------------------------
+    --  ------------------------------------------------------------------------
 
-   function To_Float_Array (List : NL_Types.Float_List) return Float_Array is
-      Result : Float_Array (1 .. Positive (List.Length));
-   begin
-      for row in List.First_Index .. List.Last_Index loop
-         Result (row) := List (row);
-      end loop;
+    function "*" (L, R : Float_Matrix) return Float_Matrix is
+        Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
+    begin
+        for row in L'First .. L'Last loop
+            for col in L'First (2) .. L'Last (2) loop
+                Result (row, col) := L (row, col) * R (row, col);
+            end loop;
+        end loop;
 
-      return Result;
+        return Result;
 
-   end To_Float_Array;
+    end "*";
 
-   --  ------------------------------------------------------------------------
+    --  ------------------------------------------------------------------------
 
-   function To_Float_Matrix (IM : Integer_Matrix) return Float_Matrix is
-      Result : Float_Matrix (1 .. IM'Length, 1 .. IM'Length (2));
-   begin
-      for row in IM'First .. IM'Last loop
-         for col in IM'First (2) .. IM'Last (2) loop
-            Result (row, col) := Float (IM (row, col));
-         end loop;
-      end loop;
+    function "**" (L : Float_Array; P : Integer) return Float_Array is
+        Result : Float_Array := L;
+    begin
+        for row in Result'First .. Result'Last loop
+            Result (row) := Result (row) ** P;
+        end loop;
 
-      return Result;
+        return Result;
 
-   end To_Float_Matrix;
+    end "**";
 
-   --  ------------------------------------------------------------------------
+    --  ------------------------------------------------------------------------
 
-   function To_Float_Matrix (List : NL_Types.Float_List_2D)
+    function "**" (L : Float_Matrix; P : Integer) return Float_Matrix is
+        Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
+    begin
+        for row in L'First .. L'Last loop
+            for col in L'First (2) .. L'Last (2) loop
+                Result (row, col) := L (row, col) ** P;
+            end loop;
+        end loop;
+
+        return Result;
+
+    end "**";
+
+    --  ------------------------------------------------------------------------
+
+    function "/" (L : Float_Matrix; R : Float) return Float_Matrix is
+        Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
+    begin
+        for row in L'First .. L'Last loop
+            for col in L'First (2) .. L'Last (2) loop
+                Result (row, col) := L (row, col) / R;
+            end loop;
+        end loop;
+
+        return Result;
+
+    end "/";
+
+    --  ------------------------------------------------------------------------
+
+    function "/" (L, R : Float_Matrix) return Float_Matrix is
+        Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
+    begin
+        for row in L'First .. L'Last loop
+            for col in L'First (2) .. L'Last (2) loop
+                Result (row, col) := L (row, col) / R (row, col);
+            end loop;
+        end loop;
+
+        return Result;
+
+    end "/";
+
+    --  ------------------------------------------------------------------------
+
+    function "+" (L, R : Float_Array) return Float_Array is
+        Result : Float_Array (L'First .. L'Last);
+    begin
+        for row in L'First .. L'Last loop
+            Result (row) := L (row) + R (row);
+        end loop;
+
+        return Result;
+
+    end "+";
+
+    --  ------------------------------------------------------------------------
+
+    function "+" (L, R : Float_Matrix) return Float_Matrix is
+        Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
+    begin
+        for row in L'First .. L'Last loop
+            for col in L'First (2) .. L'Last (2) loop
+                Result (row, col) := L (row, col) + R (row, col);
+            end loop;
+        end loop;
+
+        return Result;
+
+    end "+";
+
+    --  ------------------------------------------------------------------------
+
+    function "-" (L, R : Float_Matrix) return Float_Matrix is
+        Result : Float_Matrix (L'First .. L'Last, L'First (2) .. L'Last (2));
+    begin
+        for row in L'First .. L'Last loop
+            for col in L'First (2) .. L'Last (2) loop
+                Result (row, col) := L (row, col) - R (row, col);
+            end loop;
+        end loop;
+
+        return Result;
+
+    end "-";
+
+    --  ------------------------------------------------------------------------
+
+    function Dot (L, R : Float_Matrix) return Float_Matrix is
+--          Routine_Name : constant String := "NL_Arrays_And_Matrices.Dot ";
+        Num_Rows : constant Positive := L'Length;
+        Num_Cols : constant Positive := L'Length (2);
+        LT       : constant Float_Matrix := Transpose (L);
+        R_Row    : Float_Array (1 .. Num_Cols);
+        LR       : Float;
+        Product  : Float_Matrix  (1 .. Num_Rows, 1 .. Num_Cols);
+    begin
+--          Assert (R'Length = Num_Cols, Routine_Name &
+--                    "Num rows" & Integer'Image (R'Length) & " of right matrix " &
+--                    "doesn't equal num colums" & Integer'Image (Num_Cols) & " of left matrix");
+        for row in Product'Range loop
+            for col in Product'Range (2)loop
+                for col_r in R'Range (2)loop
+                    R_Row (col_r) := R (row, col_r);
+                end loop;
+                LR := 0.0;
+                for lr_index in R_Row'Range loop
+                    LR := LR + R_Row (lr_index) * LT (lr_index, row);
+                end loop;
+                Product (row, col) := LR;
+            end loop;
+        end loop;
+
+        return Product;
+
+    end Dot;
+
+    --  ----------------------------------------------------------------------------
+
+    function To_Float_Array (List : NL_Types.Float_List) return Float_Array is
+        Result : Float_Array (1 .. Positive (List.Length));
+    begin
+        for row in List.First_Index .. List.Last_Index loop
+            Result (row) := List (row);
+        end loop;
+
+        return Result;
+
+    end To_Float_Array;
+
+    --  ------------------------------------------------------------------------
+
+    function To_Float_Matrix (IM : Integer_Matrix) return Float_Matrix is
+        Result : Float_Matrix (1 .. IM'Length, 1 .. IM'Length (2));
+    begin
+        for row in IM'First .. IM'Last loop
+            for col in IM'First (2) .. IM'Last (2) loop
+                Result (row, col) := Float (IM (row, col));
+            end loop;
+        end loop;
+
+        return Result;
+
+    end To_Float_Matrix;
+
+    --  ------------------------------------------------------------------------
+
+    function To_Float_Matrix (List : NL_Types.Float_List_2D)
                              return Float_Matrix is
-   begin
-      if not List.Is_Empty then
-         declare
-            List_Row : NL_Types.Float_List;
-            Result   : Float_Matrix (List.First_Index .. List.Last_Index,
-                                     List (1).First_Index ..
-                                       List (1).Last_Index);
-         begin
-            for row in Result'Range loop
-               List_Row := List (row);
-               for col in Result'Range (2) loop
-                  Result (row, col) := List_Row (col);
-               end loop;
-            end loop;
-            return Result;
-         end;
-      else
-         declare
-            Result : Float_Matrix (1 .. 0, 1 .. 0);
-         begin
-            return Result;
-         end;
-      end if;
+    begin
+        if not List.Is_Empty then
+            declare
+                List_Row : NL_Types.Float_List;
+                Result   : Float_Matrix (List.First_Index .. List.Last_Index,
+                                         List (1).First_Index ..
+                                           List (1).Last_Index);
+            begin
+                for row in Result'Range loop
+                    List_Row := List (row);
+                    for col in Result'Range (2) loop
+                        Result (row, col) := List_Row (col);
+                    end loop;
+                end loop;
+                return Result;
+            end;
+        else
+            declare
+                Result : Float_Matrix (1 .. 0, 1 .. 0);
+            begin
+                return Result;
+            end;
+        end if;
 
-   end To_Float_Matrix;
+    end To_Float_Matrix;
 
-   --  ------------------------------------------------------------------------
+    --  ------------------------------------------------------------------------
 
-   function To_Integer_Array (List : NL_Types.Integer_List)
+    function To_Integer_Array (List : NL_Types.Integer_List)
                               return Integer_Array is
-   begin
-      if not List.Is_Empty then
-         declare
-            Result : Integer_Array (List.First_Index .. List.Last_Index);
-         begin
-            for row in Result'Range loop
-               Result (row) := List (row);
-            end loop;
-            return Result;
-         end;
-      else
-         declare
-            Result : Integer_Array (1 .. 0);
-         begin
-            return Result;
-         end;
-      end if;
+    begin
+        if not List.Is_Empty then
+            declare
+                Result : Integer_Array (List.First_Index .. List.Last_Index);
+            begin
+                for row in Result'Range loop
+                    Result (row) := List (row);
+                end loop;
+                return Result;
+            end;
+        else
+            declare
+                Result : Integer_Array (1 .. 0);
+            begin
+                return Result;
+            end;
+        end if;
 
-   end To_Integer_Array;
+    end To_Integer_Array;
 
-   --  ------------------------------------------------------------------------
+    --  ------------------------------------------------------------------------
 
-   function To_Natural_Array (List : NL_Types.Natural_List)
+    function To_Natural_Array (List : NL_Types.Natural_List)
                               return Natural_Array is
-   begin
-      if not List.Is_Empty then
-         declare
-            Result : Natural_Array (List.First_Index .. List.Last_Index);
-         begin
-            for row in Result'Range loop
-               Result (row) := List (row);
+    begin
+        if not List.Is_Empty then
+            declare
+                Result : Natural_Array (List.First_Index .. List.Last_Index);
+            begin
+                for row in Result'Range loop
+                    Result (row) := List (row);
+                end loop;
+                return Result;
+            end;
+        else
+            declare
+                Result : Natural_Array (1 .. 0);
+            begin
+                return Result;
+            end;
+        end if;
+    end To_Natural_Array;
+
+    --  ------------------------------------------------------------------------
+
+    function Transpose (Values : Float_Matrix) return  Float_Matrix is
+        Result : Float_Matrix (1 .. Values'Length (2), 1 .. Values'Length);
+    begin
+        for row in Values'First .. Values'Last loop
+            for col in Values'First (2) .. Values'Last (2) loop
+                Result (col, row) := Values (row, col);
             end loop;
-            return Result;
-         end;
-      else
-         declare
-            Result : Natural_Array (1 .. 0);
-         begin
-            return Result;
-         end;
-      end if;
-   end To_Natural_Array;
+        end loop;
 
-   --  ------------------------------------------------------------------------
+        return Result;
 
-   function Transpose (Values : Float_Matrix) return  Float_Matrix is
-      Result : Float_Matrix (1 .. Values'Length (2), 1 .. Values'Length);
-   begin
-      for row in Values'First .. Values'Last loop
-         for col in Values'First (2) .. Values'Last (2) loop
-            Result (col, row) := Values (row, col);
-         end loop;
-      end loop;
+    end Transpose;
 
-      return Result;
+    --  -------------------------------------------------------------------------
 
-   end Transpose;
+    function Transpose (Values : Integer_Matrix) return  Integer_Matrix is
+        Result   : Integer_Matrix (1 .. Values'Length (2), 1 .. Values'Length);
+    begin
+        for row in Values'Range loop
+            for col in Values'Range (2) loop
+                Result (col, row) := Values (row, col);
+            end loop;
+        end loop;
 
-   --  -------------------------------------------------------------------------
+        return Result;
 
-   function Transpose (Values : Integer_Matrix) return  Integer_Matrix is
-      Result   : Integer_Matrix (1 .. Values'Length (2), 1 .. Values'Length);
-   begin
-      for row in Values'Range loop
-         for col in Values'Range (2) loop
-            Result (col, row) := Values (row, col);
-         end loop;
-      end loop;
+    end Transpose;
 
-      return Result;
-
-   end Transpose;
-
-   --  -------------------------------------------------------------------------
+    --  -------------------------------------------------------------------------
 
 end NL_Arrays_And_Matrices;
