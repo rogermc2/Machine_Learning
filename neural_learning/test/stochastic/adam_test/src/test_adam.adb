e91e6b767c6c572912bdcbb27ef67871b0adbde8
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-05-21 22:12:41 +1000

    Update

--- neural_learning/test/stochastic/adam_test/src/test_adam.adb
+++ neural_learning/test/stochastic/adam_test/src/test_adam.adb
@@ -5,8 +5,7 @@
 --  with Ada.Containers;
 with Ada.Text_IO; use Ada.Text_IO;
 
-with Multilayer_Perceptron;
---  with NL_Arrays_And_Matrices; use NL_Arrays_And_Matrices;
+with NL_Arrays_And_Matrices; use NL_Arrays_And_Matrices;
 with NL_Types;
 --  with Printing;
 with Stochastic_Optimizers;
@@ -15,25 +14,34 @@ with Test_Common;
 --  Based on  test_adam_optimizer()
 procedure Test_Adam is
    --      use Ada.Containers;
-   use Multilayer_Perceptron;
    use Stochastic_Optimizers;
+   use Test_Common;
    Routine_Name  : constant String := "Test_Adam ";
-   Params        : NL_Types.Integer_Array_List;
+   Params        : Parameters_List;
    Adam          : Stochastic_Optimizers.Adam_Optimizer;
-   aClassifier   : MLP_Classifier;
    First_Moment  : Moments_List;
    Second_Moment : Moments_List;
    T             : Natural := 10;
 begin
    Put_Line (Routine_Name);
    Test_Common.Init;
-   Params := Test_Common.Shapes;
+   for index in Shapes.First_Index .. Shapes.Last_Index loop
+      declare
+         Bounds : constant NL_Types.Integer_Array := Shapes.Element (index);
+         Coeff  : constant Real_Float_Matrix (1 .. Bounds (1), 1 .. Bounds (2))
+           := (others =>  (others => 0.0));
+         Ints   : constant Real_Float_Vector (1 .. Bounds (1)) :=
+                    (others => 0.0);
+         PR     : Parameters_Record (Bounds (1), Bounds (2));
+      begin
+         PR.Coeff_Grads := Coeff;
+         PR.Intercept_Grads := Ints;
+         Params.Append (PR);
+      end;
+   end loop;
 
-   aClassifier := C_Init
-     (Solver => Stochastic_Optimizers.Adam_Solver, Learning_Rate => 0.001,
+   C_Init (Self => Adam,Params => Params,Initial_Learning_Rate => 0.001,
       Beta_1 => 0.9, Beta_2 => 0.995, Epsilon => 10.0 ** (-8));
-    Init_Optimizer (aClassifier);
-    Adam.First_Moments := First_Moment;
     Adam.Second_Moments := Second_Moment;
     Adam.Time_Step := T - 1;
 
