0c35b0ac4e85fff89f2a583376bdb21aaa1f8290
rogermc2 <rogermc@iinet.net.au>
2022-06-09 16:16:12 +1000

    Coding differentiable_functions.C_Init

--- scipy/optimise/differentiable_functions.adb
+++ scipy/optimise/differentiable_functions.adb
@@ -4,25 +4,52 @@ with Ada.Assertions; use Ada.Assertions;
 
 package body Differentiable_Functions is
 
-    type Finite_Options is record
-        Method            : Boolean;
-        Rel_Step          : Boolean;
-        Bounds            : Boolean;
-        As_Linear_Opertor : Boolean;
-    end record;
-
-
-    procedure C_Init (Self : in out Scalar_Function;
-                      fun : access function (X : Float) return Float;
-                      X0   : Fortran_DP_Array; Grad, Hess : FD_Methods;
-                      Finite_Diff_Rel_Step, Finite_Diff_Bounds : Float) is
+    procedure C_Init
+      (Self : in out Scalar_Function;
+       Fun : access function (X : Fortran_DP_Array) return Float;
+       X0   : Fortran_DP_Array; Grad, Hess : FD_Methods;
+       Finite_Diff_Rel_Step, Finite_Diff_Bounds : Float) is
         Routine_Name : constant String := "Differentiable_Functions.C_Init ";
+        Finite_Diff_Options : Finite_Options;
+
+        function Fun_Wrapped (X : Fortran_DP_Array) return Float is
+        begin
+            Self.N_Fev := Self.N_Fev + 1;
+            return Fun (X);
+
+        end Fun_Wrapped;
+
+        procedure Update_Fun is
+        begin
+            Self.Fun := Fun_Wrapped (Self.X);
+        end Update_Fun;
+
+        procedure Update_Grad is
+        begin
+            Update_Fun;
+
+        end Update_Grad;
+
     begin
---          Assert (not ((Grad in FD_Methods) and (Hess in FD_Methods)), Routine_Name &
---                    "Whenever the gradient is estimated via finite-differences" &
---                    " the Hessian must be estimated using one of the " &
---                    "quasi-Newton strategies.") ;
-        null;
+        Assert (not (Grad = FD_None or Hess = FD_None), Routine_Name &
+                  "Whenever the gradient is estimated via finite-differences" &
+                  " the Hessian must be estimated using one of the " &
+                  "quasi-Newton strategies.") ;
+        Finite_Diff_Options.Rel_Step := Finite_Diff_Rel_Step;
+        if Grad /= FD_None then
+            Finite_Diff_Options.Method := Grad;
+            Finite_Diff_Options.Bounds := Finite_Diff_Bounds;
+        elsif Hess /= FD_None then
+            Finite_Diff_Options.Method := Hess;
+            Finite_Diff_Options.As_Linear_Opertor := True;
+        end if;
+
+        Update_Fun;
+
+        if Grad /= FD_None then
+            Self.Update_Grad_Impl := Update_Grad'Access;
+        end if;
+
     end C_Init;
 
 end Differentiable_Functions;
