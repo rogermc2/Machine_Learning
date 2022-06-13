3c8649e552f7ccf26009e725338b4776472a4fb4
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-06-13 22:36:59 +1000

    Coding differentiable_functions

--- scipy/optimise/differentiable_functions.adb
+++ scipy/optimise/differentiable_functions.adb
@@ -6,22 +6,24 @@ package body Differentiable_Functions is
 
    procedure C_Init
      (Self                  : in out Scalar_Function;
-      Fun                   : access function (X : Fortran_DP_Array)
-      return Float;
+      Fun                   : DP_Fun_Access;
       X0                    : Fortran_DP_Array; Grad, Hess      : FD_Methods;
       Finite_Diff_Rel_Step,
-      Finite_Diff_Bounds    : Float) is
+      Finite_Diff_Bounds    : Float;
+      Epsilon               : Float := 10.0 ** (-8)) is
       Routine_Name        : constant String := "Differentiable_Functions.C_Init ";
       Finite_Diff_Options : Finite_Options;
 
+      --  L132
       function Fun_Wrapped (X : Fortran_DP_Array) return Float is
-         FX : constant Float := Self.Fun (X);
+         FX : constant Float := Fun (X);
       begin
          Self.N_Fev := Self.N_Fev + 1;
          if FX < Self.Lowest_F then
             Self.Lowest_X := X;
             Self.Lowest_F := FX;
          end if;
+
          return FX;
 
       end Fun_Wrapped;
@@ -31,28 +33,60 @@ package body Differentiable_Functions is
          Self.F := Fun_Wrapped (Self.X);
       end Update_Fun;
 
+      --  L170
       procedure Update_Grad is
       begin
          Update_Fun;
          Self.N_Gev := Self.N_Gev + 1;
+--           Self.Grad := Num_Diff.Approx_Derivative
+--             (Fun'Access, Self.X, Abs_Step => Epsilon, F0 => Self.F);
 
       end Update_Grad;
 
+      procedure Update_Hess is
+      begin
+         null;
+
+      end Update_Hess;
+
+      procedure Update_X (X : Fortran_DP_Array) is
+      begin
+         Self.X := X;
+         Self.F_Updated := False;
+         Self.G_Updated := False;
+         Self.H_Updated := False;
+
+      end Update_X;
+
    begin
+      --  L100
       Assert (not (Grad = FD_None or Hess = FD_None), Routine_Name &
                 "Whenever the gradient is estimated via finite-differences" &
                 " the Hessian must be estimated using one of the " &
                 "quasi-Newton strategies.") ;
+      Self.X := X0;
+
+      --  L120
       Finite_Diff_Options.Rel_Step := Finite_Diff_Rel_Step;
+      Finite_Diff_Options.Abs_Step := Epsilon;
       if Grad /= FD_None then
          Finite_Diff_Options.Method := Grad;
          Finite_Diff_Options.Bounds := Finite_Diff_Bounds;
       elsif Hess /= FD_None then
          Finite_Diff_Options.Method := Hess;
-         Finite_Diff_Options.As_Linear_Opertor := True;
+         Finite_Diff_Options.As_Linear_Operator := True;
       end if;
 
+      --  L158
       Update_Fun;
+      --  L177
+      Update_Grad;
+
+      if Hess /= FD_None then
+         --  L212
+         Update_Hess;
+         Self.H_Updated := True;
+      end if;
 
    end C_Init;
 
--- scipy/optimise/differentiable_functions.ads
+++ scipy/optimise/differentiable_functions.ads
@@ -6,13 +6,14 @@ with Num_Diff; use Num_Diff;
 package Differentiable_Functions is
 
    type Finite_Options is private;
+   type DP_Fun_Access is access function (X : Fortran_DP_Array) return Float;
 
    --  The Scalar_Function class defines a scalar function F: R^n->R and
    --  methods for computing or approximating its first and second derivatives.
    type Scalar_Function (X_Size : Positive) is record
-      Fun             : access function (X: Fortran_DP_Array) return Float;
-      --          Update_Fun_Impl  : access function (X: Fortran_DP_Array) return Float;
-      --          Update_Grad_Impl : access procedure;
+      Fun             : DP_Fun_Access;
+--        Update_Fun      : DP_Fun_Access;
+--        Update_Grad     : access procedure;
       X               : Fortran_DP_Array (1 .. X_Size);
       N_Fev           : Natural := 0;
       N_Gev           : Natural := 0;
@@ -28,18 +29,21 @@ package Differentiable_Functions is
    end record;
 
    procedure C_Init
-     (Self                                     : in out Scalar_Function;
-      Fun                                      : access function (X : Fortran_DP_Array) return Float;
-      X0                                       : Fortran_DP_Array; Grad, Hess      : FD_Methods;
-      Finite_Diff_Rel_Step, Finite_Diff_Bounds : Float);
+     (Self                   : in out Scalar_Function;
+      Fun                    : DP_Fun_Access;
+      X0                     : Fortran_DP_Array; Grad, Hess : FD_Methods;
+      Finite_Diff_Rel_Step,
+      Finite_Diff_Bounds     : Float;
+      Epsilon                : Float := 10.0 ** (-8));
 
 private
 
    type Finite_Options is record
-      Method            : FD_Methods;
-      Rel_Step          : Float := 0.0;
-      Bounds            : Float := 0.0;
-      As_Linear_Opertor : Boolean;
+      Method             : FD_Methods;
+      Rel_Step           : Float := 0.0;
+      Abs_Step           : Float := 0.0;
+      Bounds             : Float := 0.0;
+      As_Linear_Operator : Boolean := False;
    end record;
 
 end Differentiable_Functions;
