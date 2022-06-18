a4c43b33e156a779da0fc05529ce38ccfa09b2b8
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-06-18 20:59:29 +1000

    test_set_ulb runs and passes

--- neural_learning/test/optimise/setulb_test/src/test_set_ulb.adb
+++ neural_learning/test/optimise/setulb_test/src/test_set_ulb.adb
@@ -5,7 +5,6 @@ with Interfaces.Fortran; use Interfaces.Fortran;
 with Ada.Assertions; use Ada.Assertions;
 with Ada.Text_IO; use Ada.Text_IO;
 
---  with LBFGSB1; use LBFGSB1;
 with Lbfgsb_F_Interface; use Lbfgsb_F_Interface;
 --  with Printing;
 
@@ -110,21 +109,33 @@ procedure Test_Set_ULB is
    WA_Length          : constant Positive := 2 * M * N + 5 * N + 11 * M** 2 + 8 * M;
    WA                 : Fortran_DP_Array (1 .. WA_Length) := (others => 0.0);
    IWA                : Fortran_Integer_Array (1 .. 3 * N);
-   Task_Name          : Character_60 := To_Fortran ("START");
-   C_Save             : Character_60 := To_Fortran ("");
+   Task_Name          : Character_60;
+   C_Save             : Character_60;
    L_Save             : Fortran_LSave_Array := (others => 0);
    I_Save             : Fortran_Integer_Array (1 .. 4) := (others => 0);
    D_Save             : Fortran_DSave_Array := (others => 0.0);
-   Loss               : Float;
-   pragma Unreferenced (Loss);
-
+   Pass               : Boolean := True;
 begin
-   Put_Line (Routine_Name);
+   Put (Routine_Name);
+   Task_Name (1 .. 5) := To_Fortran ("START");
    for iter in 1 .. 7 loop
       Obj_Fun (X, F, G);
       Setulb (Fortran_Integer (M), X, Low_Bound, Upper_Bound, Nbd, F, G, Factor,
               Pg_Tol, WA, IWA, Task_Name, I_Print, C_Save, L_Save,
               I_Save, D_Save, Max_Ls);
+      for index in X'Range loop
+         if X (index) > Upper_Bound (index) or
+           X (index) < Low_Bound (index) then
+            Pass := False;
+            Put_Line ("X (" & Integer'Image (index) & ") outside bounds");
+         end if;
+      end loop;
    end loop;
+
+   if Pass then
+      Put_Line ("passed");
+   else
+      Put_Line ("failed");
+   end if;
 
 end Test_Set_UlB;
