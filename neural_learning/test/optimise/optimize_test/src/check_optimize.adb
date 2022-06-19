8ebe5453116b5d1e008189a798daf051d14544bd
rogermc2 <rogermc@iinet.net.au>
2022-06-19 11:55:22 +1000

    Coding Hess

--- neural_learning/test/optimise/optimize_test/src/check_optimize.adb
+++ neural_learning/test/optimise/optimize_test/src/check_optimize.adb
@@ -51,4 +51,32 @@ package body Check_Optimize is
 
    --  -------------------------------------------------------------------------
 
+   function Hess (Self : in out Check_Data; X : Real_Float_Vector)
+                  return Real_Float_Vector is
+      use Real_Float_Arrays;
+      use Maths.Float_Math_Functions;
+--        Routine_Name : constant String := "Check_Optimize.Hess ";
+      Log_PDot     : constant Real_Float_Vector := Self.F * X;
+      PDot         : constant Real_Float_Vector := Exp (Log_PDot);
+      PDot_Sum     : Float := 0.0;
+      P            : Real_Float_Vector (PDot'Range);
+      Diag_P       : Real_Float_Matrix (PDot'Range, PDot'Range) :=
+                         (others => (others => 0.0));
+      T_F          : Real_Float_Matrix := Transpose (Self.F);
+   begin
+      Self.Grad_Calls := Self.Grad_Calls + 1;
+      for row in PDot'Range loop
+         PDot_Sum := PDot_Sum + PDot (row);
+      end loop;
+      P := Exp (Log_PDot - Log (PDot_Sum));
+      for index in P'Range loop
+         Diag_P (index, index) := P (index);
+      end loop;
+
+      return T_F * (Diag_P * (T_F - P));
+
+   end Hess;
+
+   --  -------------------------------------------------------------------------
+
 end Check_Optimize;
