fda27642e9c070c88917f1efbca927ec0a61fb56
rogermc2 <rogermc@iinet.net.au>
2022-07-18 12:11:51 +1000

    Debugging Train_Test_Split

--- neural_learning/model_selection/data_splitter.adb
+++ neural_learning/model_selection/data_splitter.adb
@@ -43,7 +43,7 @@ package body Data_Splitter is
    procedure Iterate_Indices
      (Self                        : Base_Shuffle_Data;
       Train_Indices, Test_Indices : out Integer_Array) is
-      Routine_Name  : constant String := "Data_Splitter.Iterate_Indices ";
+--        Routine_Name  : constant String := "Data_Splitter.Iterate_Indices ";
       Num_Train     : constant Natural := Self.Train_Size;
       Num_Test      : constant Natural := Self.Test_Size;
       Num_Samples   : constant Natural := Num_Train + Num_Test;
@@ -60,9 +60,6 @@ package body Data_Splitter is
             Test_Indices (test_index) := Perms (test_index);
          end loop;
 
-         Put_Line (Routine_Name & "Num_Samples:" & Integer'Image (Num_Samples));
-         Put_Line (Routine_Name & "Num_Train:" & Integer'Image (Num_Train));
-         Put_Line (Routine_Name & "Num_Test:" & Integer'Image (Num_Test));
          for train_index in 1 .. Num_Train loop
             Train_Indices (train_index) := Perms (Num_Test + train_index);
          end loop;
@@ -123,28 +120,33 @@ package body Data_Splitter is
       Init_Base_Shuffle_Split (Shuffle_Data, 1, Train_Size, Test_Size,
                                Default_Test_Size);
 
-      Put_Line (Routine_Name);
       Base_Shuffle_Split (Shuffle_Data, Train_Indices, Test_Indices);
 
-      for index in Test_Indices'First .. Test_Indices'Last loop
-         for col in Test_X'First (2) .. Test_X'Last (2) loop
+      for index in Test_Indices'Range loop
+         Put_Line (Routine_Name & "index" & Integer'Image (index));
+         for col in Test_X'Range (2) loop
+            Put_Line (Routine_Name & "col" & Integer'Image (col));
+            Put_Line (Routine_Name & "Test_Indices (index)" &
+                        Integer'Image (Test_Indices (index)));
+            Put_Line (Routine_Name & "col" & Integer'Image (col));
+            Put_Line (Routine_Name & "X length:" & Integer'Image (X'Length));
             Test_X (index - Test_Indices'First + 1, col) :=
               X (Test_Indices (index), col);
          end loop;
 
-         for col in Test_Y'First (2) .. Test_Y'Last (2) loop
+         for col in Test_Y'Range (2) loop
             Test_Y (index - Test_Indices'First + 1, col) :=
               Y (Test_Indices (index), col);
          end loop;
       end loop;
 
-      for index in Train_Indices'First .. Train_Indices'Last loop
-         for col in Train_X'First (2) .. Train_X'Last (2) loop
+      for index in Train_Indices'Range loop
+         for col in Train_X'Range (2) loop
             Train_X (index - Test_Indices'First + 1, col) :=
               X (Test_Indices (index), col);
          end loop;
 
-         for col in Train_Y'First (2) .. Train_Y'Last (2) loop
+         for col in Train_Y'Range (2) loop
             Train_Y (index - Test_Indices'First + 1, col) :=
               Y (Test_Indices (index), col);
          end loop;
