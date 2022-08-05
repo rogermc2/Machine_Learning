ed87bc6434288dc010a18634945e88fce273b5c4
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-08-05 16:35:56 +1000

    Debuggign

--- neural_learning/test/mlp/partial_fit_test/src/test_partial_fit.adb
+++ neural_learning/test/mlp/partial_fit_test/src/test_partial_fit.adb
@@ -11,20 +11,23 @@ with Printing;
 with Stochastic_Optimizers;
 
 procedure Test_Partial_Fit is
-    use Real_Float_Arrays;
-    use Multilayer_Perceptron;
-    use Stochastic_Optimizers;
+   use Real_Float_Arrays;
+   use Multilayer_Perceptron;
+   use Stochastic_Optimizers;
 
-    Routine_Name : constant String := "Test_Partial_Fit ";
-    Data         : constant Load_Dataset.Digits_Data_Record :=
-                    Load_Dataset.Load_Digits ("../../digits.csv", 3);
-    X            : constant Real_Float_Matrix := To_Real_Float_Matrix (Data.Features);
-    Y            : constant Integer_Matrix := To_Integer_Matrix (Data.Target);
---      Classes      : constant NL_Types.Class_Names_List := Data.Class_Names;
+   Routine_Name  : constant String := "Test_Partial_Fit ";
+   Data          : constant Load_Dataset.Digits_Data_Record :=
+                     Load_Dataset.Load_Digits ("../../digits.csv", 3);
+   X             : constant Real_Float_Matrix :=
+                     To_Real_Float_Matrix (Data.Features);
+   Y             : constant Integer_Matrix := To_Integer_Matrix (Data.Target);
+   --      Classes      : constant NL_Types.Class_Names_List := Data.Class_Names;
    aClassifier   : MLP_Classifier;
 begin
    Put_Line (Routine_Name);
-    aClassifier := C_Init
+   Printing.Print_Float_Lists_2D (Routine_Name & "Features", Data.Features);
+   Printing.Print_Integer_Matrix (Routine_Name & "Y", Y, 1, 3);
+   aClassifier := C_Init
      (Solver => Stochastic_Optimizers.Sgd_Solver, Max_Iter => 100,
       Random_State => 1, Tol => 0.0, Alpha => 10.0 ** (-5),
       Learning_Rate_Init => 0.2);
@@ -39,6 +42,6 @@ begin
       Printing.Print_Binary_Matrix ("Pred1", Pred1);
    end;
 
-    --  Partial_Fit updates the model with a single iteration over the data.
+   --  Partial_Fit updates the model with a single iteration over the data.
 
 end Test_Partial_Fit;
