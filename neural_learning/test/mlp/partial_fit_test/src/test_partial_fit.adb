b98a5a18501b5afe498610bdbf539b610399440c
rogermc2 <rogermc@iinet.net.au>
2022-08-06 11:04:12 +1000

    dded Load_Y function

--- neural_learning/test/mlp/partial_fit_test/src/test_partial_fit.adb
+++ neural_learning/test/mlp/partial_fit_test/src/test_partial_fit.adb
@@ -13,62 +13,53 @@ with Stochastic_Optimizers;
 --  with Utilities;
 
 procedure Test_Partial_Fit is
-   use Real_Float_Arrays;
-   use Multilayer_Perceptron;
-   use Stochastic_Optimizers;
+    use Real_Float_Arrays;
+    use Multilayer_Perceptron;
+    use Stochastic_Optimizers;
 
---     function Get_CSV_Data (File_Name : String) return NL_Types.Raw_Data_Vector is
---        Data_File    : File_Type;
---        Raw_CSV_Data : NL_Types.Raw_Data_Vector;
---     begin
---        Open (Data_File, In_File, File_Name);
---        Raw_CSV_Data := Utilities.Load_Raw_CSV_Data (Data_File);
---        Close (Data_File);
---
---        for row in Raw_CSV_Data.First_Index .. Raw_CSV_Data.First_Index loop
---           Printing.Print_Unbounded_List ("", Raw_CSV_Data.Element (row));
---        end loop;
---
---        return Raw_CSV_Data;
---
---     end Get_CSV_Data;
+    Routine_Name  : constant String := "Test_Partial_Fit ";
 
-   Routine_Name  : constant String := "Test_Partial_Fit ";
---     Raw_CSV_Data  : NL_Types.Raw_Data_Vector :=
---                       Get_CSV_Data ("../../digits.csv");
-   Data          : constant Load_Dataset.Digits_Data_Record :=
-                     Load_Dataset.Load_Digits
-                       ("../../digits.csv", Num_Classes => 3);
-   X             : constant Real_Float_Matrix :=
-                     To_Real_Float_Matrix (Data.Features);
-   Y1            : constant Integer_Array := Data.Target;
-   Y             : Integer_Matrix (Y1'Range, 1 .. 1);
-   --      Classes      : constant Integer_Array := Data.Classes;
-   aClassifier   : MLP_Classifier;
+    function Load_Y (Y1 : Integer_Array) return Integer_Matrix is
+        YM : Integer_Matrix (Y1'Range, 1 .. 1);
+    begin   Printing.Print_Integer_Array
+          (Routine_Name & "Load_Y Y1", Y1, 1, 10);
+        for row in Y1'Range loop
+            YM (row, 1) := Y1 (row);
+        end loop;
+
+        return YM;
+
+    end Load_Y;
+
+    Data          : constant Load_Dataset.Digits_Data_Record :=
+                      Load_Dataset.Load_Digits
+                        ("../../digits.csv", Num_Classes => 3);
+    X             : constant Real_Float_Matrix :=
+                      To_Real_Float_Matrix (Data.Features);
+    Y             : constant Integer_Matrix := Load_Y (Data.Target);
+    --      Classes      : constant Integer_Array := Data.Classes;
+    aClassifier   : MLP_Classifier;
 begin
-   Put_Line (Routine_Name);
---     Printing.Print_Float_Matrix (Routine_Name & "X", X, 1, 3);
-   Printing.Print_Integer_Array (Routine_Name & "Y1", Y1, 1, 10);
-   for row in Y1'Range loop
-      Y (row, 1) := Y1 (row);
-   end loop;
-   Printing.Print_Integer_Matrix (Routine_Name & "Y", Y, 1, 10);
+    Put_Line (Routine_Name);
+    --     Printing.Print_Float_Matrix (Routine_Name & "X", X, 1, 3);
 
-   aClassifier := C_Init
-     (Solver => Stochastic_Optimizers.Sgd_Solver, Max_Iter => 100,
-      Random_State => 1, Tol => 0.0, Alpha => 10.0 ** (-5),
-      Learning_Rate_Init => 0.2);
+    Printing.Print_Integer_Matrix (Routine_Name & "Y", Y, 1, 10);
 
-   Init_Optimizer (aClassifier);
-   Put_Line (Routine_Name & "Optimizer initialized");
-   Fit (aClassifier, X, Y);
-   Put_Line (Routine_Name & "aClassifier fitted");
-   declare
-      Pred1 : constant Binary_Matrix := Predict (aClassifier, X);
-   begin
-      Printing.Print_Binary_Matrix ("Pred1", Pred1);
-   end;
+    aClassifier := C_Init
+      (Solver => Stochastic_Optimizers.Sgd_Solver, Max_Iter => 100,
+       Random_State => 1, Tol => 0.0, Alpha => 10.0 ** (-5),
+       Learning_Rate_Init => 0.2);
 
-   --  Partial_Fit updates the model with a single iteration over the data.
+    Init_Optimizer (aClassifier);
+    Put_Line (Routine_Name & "Optimizer initialized");
+    Fit (aClassifier, X, Y);
+    Put_Line (Routine_Name & "aClassifier fitted");
+    declare
+        Pred1 : constant Binary_Matrix := Predict (aClassifier, X);
+    begin
+        Printing.Print_Binary_Matrix ("Pred1", Pred1);
+    end;
+
+    --  Partial_Fit updates the model with a single iteration over the data.
 
 end Test_Partial_Fit;
