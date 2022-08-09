def3e49ac6e4b2d904197956907cc358461cd20b
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-08-09 23:10:05 +1000

    Prediction added

--- neural_learning/test/mlp/partial_fit_test/src/test_partial_fit.adb
+++ neural_learning/test/mlp/partial_fit_test/src/test_partial_fit.adb
@@ -4,62 +4,62 @@
 --  with Ada.Assertions; use Ada.Assertions;
 with Ada.Text_IO; use Ada.Text_IO;
 
---  with Base;
+with Base;
 with Load_Dataset;
 with Multilayer_Perceptron;
 with NL_Arrays_And_Matrices; use NL_Arrays_And_Matrices;
---  with Printing;
+with Printing;
 with Stochastic_Optimizers;
 --  with Utilities;
 
 procedure Test_Partial_Fit is
-    use Real_Float_Arrays;
-    use Multilayer_Perceptron;
-    use Stochastic_Optimizers;
+   use Real_Float_Arrays;
+   use Multilayer_Perceptron;
+   use Stochastic_Optimizers;
 
-    Routine_Name  : constant String := "Test_Partial_Fit ";
+   Routine_Name  : constant String := "Test_Partial_Fit ";
 
-    function Load_Y (Y1 : Integer_Array) return Integer_Matrix is
-        YM : Integer_Matrix (Y1'Range, 1 .. 1);
-    begin
-        for row in Y1'Range loop
-            YM (row, 1) := Y1 (row);
-        end loop;
+   function Load_Y (Y1 : Integer_Array) return Integer_Matrix is
+      YM : Integer_Matrix (Y1'Range, 1 .. 1);
+   begin
+      for row in Y1'Range loop
+         YM (row, 1) := Y1 (row);
+      end loop;
 
-        return YM;
+      return YM;
 
-    end Load_Y;
+   end Load_Y;
 
-    Data          : constant Load_Dataset.Digits_Data_Record :=
-                      Load_Dataset.Load_Digits
-                        ("../../digits.csv", Num_Classes => 3);
-    X             : constant Real_Float_Matrix :=
-                      To_Real_Float_Matrix (Data.Features);
-    Y             : constant Integer_Matrix := Load_Y (Data.Target);
-    --      Classes      : constant Integer_Array := Data.Classes;
-    aClassifier   : MLP_Classifier;
---     Score               : Float;
+   Data                : constant Load_Dataset.Digits_Data_Record :=
+                           Load_Dataset.Load_Digits
+                             ("../../digits.csv", Num_Classes => 3);
+   X                   : constant Real_Float_Matrix :=
+                           To_Real_Float_Matrix (Data.Features);
+   Y                   : constant Integer_Matrix := Load_Y (Data.Target);
+   --      Classes      : constant Integer_Array := Data.Classes;
+   aClassifier         : MLP_Classifier;
+   Score               : Float;
 begin
-    Put_Line (Routine_Name);
-    --  Printing.Print_Float_Matrix (Routine_Name & "X", X, 1, 3);
-    --  Printing.Print_Integer_Matrix (Routine_Name & "Y", Y, 1, 10);
+   Put_Line (Routine_Name);
+   --  Printing.Print_Float_Matrix (Routine_Name & "X", X, 1, 3);
+   --  Printing.Print_Integer_Matrix (Routine_Name & "Y", Y, 1, 10);
 
-    aClassifier := C_Init
-      (Solver => Stochastic_Optimizers.Sgd_Solver, Max_Iter => 100,
-       Random_State => 1, Tol => 0.0, Alpha => 10.0 ** (-5),
-       Learning_Rate_Init => 0.2, Verbose => True);
+   aClassifier := C_Init
+     (Solver => Stochastic_Optimizers.Sgd_Solver, Max_Iter => 100,
+      Random_State => 1, Tol => 0.0, Alpha => 10.0 ** (-5),
+      Learning_Rate_Init => 0.2, Verbose => True);
 
-    Init_Optimizer (aClassifier);
-    Put_Line (Routine_Name & "Optimizer initialized");
-    Fit (aClassifier, X, Y);
-    Put_Line (Routine_Name & "aClassifier fitted");
---      declare
---          Pred1 : constant Binary_Matrix := Predict (aClassifier, X);
---      begin
---          Printing.Print_Binary_Matrix ("Pred1", Pred1, 1, 4);
---      end;
---
---      Score := Base.Score (aClassifier, X, Y);
---      Put_Line (Routine_Name & " Score:" & Float'Image (Score));
+   Init_Optimizer (aClassifier);
+   Put_Line (Routine_Name & "Optimizer initialized");
+   Fit (aClassifier, X, Y);
+   Put_Line (Routine_Name & "aClassifier fitted");
+   declare
+      Pred1 : constant Binary_Matrix := Predict (aClassifier, X);
+   begin
+      Printing.Print_Binary_Matrix ("Pred1", Pred1, 1, 4);
+   end;
+
+   Score := Base.Score (aClassifier, X, Y);
+   Put_Line (Routine_Name & " Score:" & Float'Image (Score));
 
 end Test_Partial_Fit;
