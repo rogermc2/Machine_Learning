2a1141a0ea03316ed6985bdcdbb2a54054433630
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-08-31 22:31:31 +1000

    Coding unbounded string tests

--- neural_learning/test/preprocessing/test_label/multilabel_binarizer_test/src/test_multilabel_binarizer.adb
+++ neural_learning/test/preprocessing/test_label/multilabel_binarizer_test/src/test_multilabel_binarizer.adb
@@ -2,6 +2,7 @@
 --  test_multilabel_binarizer()
 
 with Ada.Assertions; use  Ada.Assertions;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Label;
@@ -12,20 +13,30 @@ with NL_Arrays_And_Matrices; use NL_Arrays_And_Matrices;
 procedure Test_Multilabel_Binarizer is
    use NL_Types.Integer_Package;
    use Integer_Array_Package;
-   Routine_Name     : constant String := "Test_Multilabel_Binarizer ";
-   Indicator_Mat    : constant Binary_Matrix (1 .. 3, 1 .. 3) :=
-                        ((0, 1, 1), (1, 0, 0), (1, 1, 0));
-   Indicator_Mat2    : constant Binary_Matrix (1 .. 3, 1 .. 4) :=
-                        ((0, 0, 1, 1), (1, 1, 0, 0), (0, 1, 1, 0));
-   lambda_Mat       : constant Integer_Matrix (1 .. 3, 1 .. 2) :=
-                           ((2, 3), (0, 1), (1, 2));
-   lambda1_1        : constant Integer_Array (1 .. 2) := (2, 3);
-   lambda1_2        : constant Integer_Array (1 .. 1) := (1 => 1);
-   lambda1_3        : constant Integer_Array (1 .. 2) := (1, 2);
-   lambda1          : Integer_Array_List;
-   Expected_Classes : NL_Types.Integer_List;
+   Routine_Name         : constant String := "Test_Multilabel_Binarizer ";
+   Spam                 : constant Unbounded_String := To_Unbounded_String ("spam");
+   Eggs                 : constant Unbounded_String := To_Unbounded_String ("eggs");
+   Zero                 : constant Unbounded_String := To_Unbounded_String ("0");
+   Inp11                : constant Unbounded_String_Array (1 .. 2) :=
+                           (Spam, Zero);
+   Inp12                : constant Unbounded_String_Array (1 .. 1) :=
+                            (1 => Eggs);
+   Inp13                : constant Unbounded_String_Array (1 .. 2) :=
+                            (Eggs, Spam);
+   Indicator_Mat        : constant Binary_Matrix (1 .. 3, 1 .. 3) :=
+                            ((0, 1, 1), (1, 0, 0), (1, 1, 0));
+   Indicator_Mat2       : constant Binary_Matrix (1 .. 3, 1 .. 4) :=
+                            ((0, 0, 1, 1), (1, 1, 0, 0), (0, 1, 1, 0));
+   lambda_Mat           : constant Integer_Matrix (1 .. 3, 1 .. 2) :=
+                            ((2, 3), (0, 1), (1, 2));
+   lambda1_1            : constant Integer_Array (1 .. 2) := (2, 3);
+   lambda1_2            : constant Integer_Array (1 .. 1) := (1 => 1);
+   lambda1_3            : constant Integer_Array (1 .. 2) := (1, 2);
+   lambda1              : Integer_Array_List;
+   String1              : Unbounded_String_Array_List;
+   Expected_Classes     : NL_Types.Integer_List;
    Expected_Mat_Classes : NL_Types.Integer_List;
-   MLB              : Label.Multi_Label_Binarizer;
+   MLB                  : Label.Multi_Label_Binarizer;
 begin
    Put_Line (Routine_Name);
    lambda1.Append (lambda1_1);
@@ -62,7 +73,7 @@ begin
    --  fit_transform case
    declare
       MLB2 : Label.Multi_Label_Binarizer;
-      Got : constant Binary_Matrix := Label.Fit_Transform (MLB2, lambda1);
+      Got  : constant Binary_Matrix := Label.Fit_Transform (MLB2, lambda1);
    begin
       Assert (MLB2.Classes = Expected_Classes,
               "fit_transform case unexpected classes");
@@ -81,6 +92,22 @@ begin
               "matrix fit_transform case Got invalid lambda_Mat data");
       Assert (Label.Inverse_Transform (MLB2, Got) = lambda_Mat,
               "matrix fit_transform case invalid inverse lambda_Mat Got");
+   end;
+
+   String1.Append (Inp11);
+   String1.Append (Inp12);
+   String1.Append (Inp13);
+
+   --  fit case strings
+   Label.Fit (MLB, String1);
+   declare
+      Got : constant Binary_Matrix := Label.Transform (String1);
+   begin
+      Test_Support.Print_Binary_Matrix ("String1 Got", Got);
+      --        Assert (MLB.Classes = Expected_Classes, "fit case unexpected classes");
+      --        Assert (Got = Indicator_Mat, "fit case Got invalid data");
+      Assert (Label.Inverse_Transform (MLB, Got) = String1,
+              "fit case invalid inverse Got");
    end;
 
    Put_Line (Routine_Name & "tests passed.");
