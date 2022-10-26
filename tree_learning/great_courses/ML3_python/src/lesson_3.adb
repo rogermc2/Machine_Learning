fc713165f74e8158aad6ffea7a95e24a91ce1821
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-10-27 02:58:40 +1100

    Update

--- tree_learning/great_courses/ML3_python/src/lesson_3.adb
+++ tree_learning/great_courses/ML3_python/src/lesson_3.adb
@@ -5,24 +5,16 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with ML_Types;
 
---  with Base_Decision_Tree;
---  with Classifier_Types;
 with Classifier_Utilities;
---  with Criterion;
---  with Decision_Tree_Classification;
 --  with Graphviz_Exporter;
 with Printing;
 with Python;
---  with Tree;
---  with Weights;
 
 procedure Lesson_3 is
    use ML_Types;
    use ML_Types.String_Package;
---     use Decision_Tree_Classification;
    use Printing;
    Routine_Name  : constant String := "Lesson_3";
---     Min_Split     : constant String := "2";
    Data          : constant Multi_Output_Data_Record :=
                      Classifier_Utilities.Load_Data ("../diabetes.csv");
    Feature_Names : constant String_List := Data.Feature_Names;
@@ -32,9 +24,6 @@ procedure Lesson_3 is
    Classes       : Python.Module;
    Names_Cursor  : String_Package.Cursor := Feature_Names.First;
    Features      : Feature_Names_List;
---     aClassifier   : Base_Decision_Tree.Classifier
---       (Tree.Integer_Type, Tree.Integer_Type, Tree.Integer_Type);
---     Labels        : Value_Data_Lists_2D;
 --     No_Weights    : Weights.Weight_List :=
 --                       Classifier_Types.Float_Package.Empty_Vector;
 --     Correct       : Natural := 0;
@@ -42,8 +31,6 @@ procedure Lesson_3 is
 begin
    Put_Line ("Lesson 3");
    Assert (Num_Samples > 0, Routine_Name & " called with empty X vector.");
-   --  Labels is 2D list num outputs x num samples
---     Labels := Data.Label_Values;
    while Has_Element (Names_Cursor) loop
       Features.Append (Element (Names_Cursor));
       Next (Names_Cursor);
@@ -57,9 +44,8 @@ begin
    Base := Python.Import_File ("base");
    Classes := Python.Import_File ("classes");
 
---     C_Init (aClassifier, Min_Split, Criterion.Gini_Criteria,
---             Max_Leaf_Nodes => 3);
-   Python.Execute_String ("clf = tree.DecisionTreeClassifier(max_leaf_nodes = 3)");
+   Python.Execute_String
+     ("clf = tree.DecisionTreeClassifier(max_leaf_nodes = 3)");
    --  Fit function adjusts weights according to data values so that
    --  better accuracy can be achieved
 --     Classification_Fit (aClassifier, X_Data, Labels, No_Weights);
