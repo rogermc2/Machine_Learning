204bd12e44f62c86b05d5799da8c5ae9ac0f3a71
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-10-29 17:00:49 +1100

    New python module load.py

---
+++ tree_learning/great_courses/ML3_python/src/load.py
@@ -0,0 +1,14 @@
+def load
+    import os
+    cwd = os.getcwd()
+    print ('cwd: ', cwd)
+    os.chdir(os.path.join (cwd, 'src'))
+    cwd = os.getcwd()
+    print ('cwd: ', os.getcwd())
+    os.listdir(os.getcwd())
+    import Tree2
+    cwd = os.path.join (cwd, 'Tree2')
+    os.chdir(cwd)
+    os.listdir(os.getcwd())
+    import base
+    import classes
