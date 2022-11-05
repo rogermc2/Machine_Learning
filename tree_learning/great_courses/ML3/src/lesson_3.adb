bc53ab55eb357e41cb4f1dd8ae1ca985a19afb91
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-11-05 22:58:17 +1100

    Update

--- tree_learning/great_courses/ML3/src/lesson_3.adb
+++ tree_learning/great_courses/ML3/src/lesson_3.adb
@@ -23,7 +23,7 @@ procedure Lesson_3 is
    Routine_Name  : constant String := "Lesson_3";
    Min_Split     : constant String := "2";
    Data          : constant Multi_Output_Data_Record :=
-                     Classifier_Utilities.Load_Data ("src/diabetes.csv");
+                     Classifier_Utilities.Load_Data ("../diabetes.csv");
    Feature_Names : constant String_List := Data.Feature_Names;
    X_Data        : constant Value_Data_Lists_2D := Data.Feature_Values;
    Num_Samples   : constant Natural := Natural (X_Data.Length);
