de162f8726fad6db2e788946655b532efd9ab00b
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-11-29 00:21:29 +1100

    Update

--- tree_learning/great_courses/c_python/ML3_aux2/src/py_package/word_classifier_aux.py
+++ tree_learning/great_courses/c_python/ML3_aux2/src/py_package/word_classifier_aux.py
@@ -1,13 +1,13 @@
 from sklearn import tree
 
-def word_classifier_aux (dat, labels, words, pros, features):
+def word_classifier_aux (dat, labels, words, pros):
 # Train the decision tree classifer using eight decision rules and calculate the number of words that are correct with this model.
 # Set up the learner and run it on the data then compute the accuracy and print it
     clf = tree.DecisionTreeClassifier(max_leaf_nodes=8)
     clf = clf.fit(dat, labels)
 
-    correct = 0
-    for i in range(len(words)):
-        if clf.predict([features([words[i], pros[i]])]) == labels[i]: correct = correct + 1
-    print("Number of correct words: ", correct)
-    return (correct)
+#    correct = 0
+#    for i in range(len(words)):
+#        if clf.predict([features([words[i], pros[i]])]) == labels[i]: correct = correct + 1
+#    print("Number of correct words: ", correct)
+#    return (correct)
--- tree_learning/great_courses/c_python/ML3_aux2/src/word_classification.adb
+++ tree_learning/great_courses/c_python/ML3_aux2/src/word_classification.adb
@@ -74,7 +74,7 @@ package body Word_Classification is
    procedure Build_Dataset
      (IE_Data, EI_Data         : ML_Types.Unbounded_List;
       Labels, Words, Pronounce : out ML_Types.Bounded_String_List;
-      Data                    : out NL_Types.Boolean_List_2D) is
+      Data                     : out NL_Types.Boolean_List_2D) is
 
    begin
       Build_Data (IE_Data, Data, Labels, Words, Pronounce);
