2ce913198ddfa1a661e8150518ed7012de51ac0c
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-04 22:05:01 +1100

    Update

--- tree_learning/great_courses/c_python/ML3_aux2/src/py_package/word_classifier_aux.py
+++ tree_learning/great_courses/c_python/ML3_aux2/src/py_package/word_classifier_aux.py
@@ -3,11 +3,14 @@ from sklearn import tree
 
 def word_classifier_aux (dat, labels):
     print("word_classifier_aux")
-    data_List = list (dat)
+    data_tuple_list = list (dat)
+    data_lists = list(map(list, dat))
     label_list = list (labels)
     
-    print("data_List length: ", len(data_List))
-    print("data_List [1]: ", data_List [1][0:20])
+    print("data_tuple_list length: ", len(data_tuple_list))
+    print("data_tuple_list [1][0:20]: ", data_tuple_list [1][0:20])
+    print("data_lists length: ", len(data_lists))
+    print("data_lists [1][0:20]: ", data_lists [1][0:20])
     
     print("label_list length: ", len(label_list))
     print("label_list: ", label_list [0:10])
@@ -16,7 +19,7 @@ def word_classifier_aux (dat, labels):
 # Set up the learner and run it on the data then compute the accuracy and print it
     clf = tree.DecisionTreeClassifier(max_leaf_nodes=8)
     print("clf set")
-#    clf = clf.fit(dat, labels)
+#    clf = clf.fit(data_lists, label_list)
 
 #    correct = 0
 #    for i in range(len(words)):
