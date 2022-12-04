1c43ae62cd823498c13622c7e9b7f0a811871a70
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-04 19:56:53 +1100

    Update

--- tree_learning/great_courses/c_python/ML3_aux2/src/py_package/word_classifier_aux.py
+++ tree_learning/great_courses/c_python/ML3_aux2/src/py_package/word_classifier_aux.py
@@ -2,22 +2,40 @@
 from sklearn import tree
 
 def word_classifier_aux (dat, labels):
-#    print(dat[0])
     print("word_classifier_aux")
-#    print(labels[0])
-#    print("dat type: ", type(dat))
     print("dat length: ", len(dat))
     print("labels length: ", len(labels))
-#    print("word_classifier_aux 2")
 # Train the decision tree classifer using eight decision rules and calculate the number of words that are correct with this model.
 # Set up the learner and run it on the data then compute the accuracy and print it
 #    clf = tree.DecisionTreeClassifier(max_leaf_nodes=8)
+#    print("clf set")
+    label_list = list (labels)
+    print("label_list length: ", len(labels))
+    
+    res = type(dat) is tuple
+    print("dat is tuple: ", res)
+    
+    data = list (dat)
+    print("data set")
+    
+    res = type(dat[0]) is tuple
+    print("dat[0] is tuple: ", res)
+    print("type(data[0]): ", type(data[0]))
+    res = type(data[0]) is tuple
+    print("data[0] is tuple: ", res)
+#    t0=dat[0]
+#    print("t0 length: ", len(t0))
+#    t00=dat[0][0]
+#    print("t00 length: ", len(t00))
+#    words = [list(tup) for tup in dat]
+#    print("clf set")
+#    print("words length: ", len(words))
 #    clf = clf.fit(dat, labels)
 
-    correct = 0
+#    correct = 0
 #    for i in range(len(words)):
 #        if clf.predict([features([words[i], pros[i]])]) == labels[i]: correct = correct + 1
 #    print("Number of correct words: ", correct)
 #    print("word_classifier_aux end")
-    return correct
+#    return 0
 
