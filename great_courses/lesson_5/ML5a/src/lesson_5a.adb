0285c8e09d82e7d7294084d58690e040f0917c77
rogermc2 <rogermc@iinet.net.au>
2022-12-11 09:35:05 +1100

    Update

--- great_courses/lesson_5/ML5a/src/lesson_5a.adb
+++ great_courses/lesson_5/ML5a/src/lesson_5a.adb
@@ -1,5 +1,5 @@
 
---  with Ada.Text_IO; use Ada.Text_IO;
+--  with Ada.Text_IO;
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 
 with Bitmap.File_IO; use Bitmap.File_IO;
@@ -9,7 +9,7 @@ procedure Lesson_5A is
    File   : Ada.Streams.Stream_IO.File_Type;
    Buffer : Any_Bitmap_Buffer;
 begin
-   Open (File, In_File, "../greenML.png");
+   Open (File, In_File, "src/greenML.png");
    Buffer := Read_BMP_File (File);
 
    Close (File);
