1ef0fc2ae836f5e9d4142edb71ea9ea7474bcf8a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-11 14:07:53 +1100

    GID package added

---
+++ great_courses/lesson_5/To_Bmp/src/bmp_support.ads
@@ -0,0 +1,10 @@
+
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+package BMP_Support is
+
+   procedure Blurb;
+   procedure Process (name : String; as_background, test_only : Boolean;
+                     background_image_name : in out Unbounded_String);
+
+end BMP_Support;
