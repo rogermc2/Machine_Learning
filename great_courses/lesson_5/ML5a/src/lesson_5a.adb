5f86aacd71edb59c930cc81bda8d27f9820a0598
rogermc2 <rogermc@iinet.net.au>
2022-12-12 20:11:09 +1100

    Update

--- great_courses/lesson_5/ML5a/src/lesson_5a.adb
+++ great_courses/lesson_5/ML5a/src/lesson_5a.adb
@@ -12,7 +12,7 @@ procedure Lesson_5A is
    Image_File_Name : constant String := "../greenML.png";
    --     File   : Ada.Streams.Stream_IO.File_Type;
    --     Buffer : Any_Bitmap_Buffer;
-   MLP             : Python.Module;
+   MLP : Python.Module;
 begin
    Simple_PNG_To_BMP.Process (Image_File_Name);
    --     Open (File, In_File, "src/greenML.png");
