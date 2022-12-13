80933c7e32fe847ffba48508f765f7e1291743c0
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-13 16:39:24 +1100

    Update

---
+++ great_courses/lesson_5/ML5a/src/py_package/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.9
+cwd=os.getcwd()
+>>> cwd=cwd + '/src/py_package'
+>>> os.chdir(cwd)
--- great_courses/lesson_5/ML5a/src/py_package/lesson_5a.py
+++ great_courses/lesson_5/ML5a/src/py_package/lesson_5a.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3.9
 import numpy as np
+#import tensorflow
 from keras.preprocessing import image
 
 def display (file_name):
+    print("display")
     img = image.load_img(file_name)
     display(img)
+    print("display done")
