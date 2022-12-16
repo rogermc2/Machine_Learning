85961be647183750433f959bfd8abf74f3c0fcc1
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-16 12:24:33 +1100

    Update

--- great_courses/lesson_5/ML5a/src/lesson_5a.adb
+++ great_courses/lesson_5/ML5a/src/lesson_5a.adb
@@ -1,6 +1,8 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 
+with Python; use Python;
+with Python_API;
 with Simple_PNG_To_BMP; use Simple_PNG_To_BMP;
 
 procedure Lesson_5A is
@@ -8,10 +10,20 @@ procedure Lesson_5A is
    Image_File_Name : constant String := "../greenML.png";
 begin
    declare
-      Image_Data : constant Simple_PNG_To_BMP.Image_Array := Process (Image_File_Name);
+      Image_Data : constant Simple_PNG_To_BMP.Image_Array :=
+                     Process (Image_File_Name);
+      Module     : Python_API.PyObject;
    begin
       Put_Line (Project_Name & "Image Dimensions: " &
-                  Integer'Image (Height (Image_Data)) & " x" & Integer'Image (Width (Image_Data)));
+                  Integer'Image (Height (Image_Data)) & " x" &
+                  Integer'Image (Width (Image_Data)));
+      Python.Initialize;
+--        Module := Python_API.PyObject (Import_File ("ML5.py"));
+--        Python.Execute_String ("import matplotlib.pyplot as plt");
+--        Python.Call (Module, Show, Image_Array);
+
+      Python.Finalize;
+
    end;
 
 end Lesson_5A;
