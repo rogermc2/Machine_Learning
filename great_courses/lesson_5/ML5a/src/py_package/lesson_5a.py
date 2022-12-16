2fc78a129328f5e7090a1c7bdf6fed4634c8703e
rogermc2 <rogermc@iinet.net.au>
2022-12-16 20:03:58 +1100

    Update

--- great_courses/lesson_5/ML5a/src/py_package/lesson_5a.py
+++ great_courses/lesson_5/ML5a/src/py_package/lesson_5a.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
 def show(bitmap):
+    print("bitmap size:", len(bitmap), "x", len(bitmap[0]))
+    print(bitmap[10][0:15])
     plt.imshow(bitmap)
     plt.show()
