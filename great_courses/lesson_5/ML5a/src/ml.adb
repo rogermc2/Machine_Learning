20c41ad35fa70600b5f96e014e47e046d5a6dac4
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-20 03:36:58 +1100

    Update

--- great_courses/lesson_5/ML5a/src/ml.adb
+++ great_courses/lesson_5/ML5a/src/ml.adb
@@ -20,13 +20,13 @@ package body ML is
                           To_Real_Float_Matrix (Data);
       F_Labels        : constant Real_Float_Vector :=
                           To_Real_Float_Vector (Labels);
-      Alpha           : Float := 0.1;
+      Step_Size       : Float := 0.1;
       --  Y1 = h = np.matmul(alldat,w) dot product
       --  Y1i = w1xi1 + w2x21 + ... wnxn1 + w_offset
       Y1              : Real_Float_Vector (Data'Range);
       Exp_Y1          : Real_Float_Vector (Data'Range);
       Y               : Real_Float_Vector (Data'Range);
-      Diff            : Real_Float_Vector (Data'Range);
+      Errors          : Real_Float_Vector (Data'Range);
       Delta_Weights   : Real_Float_Vector (Weights'Range);
       New_Weights     : Real_Float_Vector (Weights'Range);
       Current_Loss    : Float;
@@ -52,22 +52,22 @@ package body ML is
          --  delta_w is the change in the weights suggested by the gradient
          --  Delta_Weight = np.add.reduce
          --  (np.reshape((labs-y) * np.exp(-h)*y**2,(len(y),1)) * alldat)
-         Diff := (F_Labels - Y);
-         Delta_Weights := Mult (Diff, Exp_Y1, Y) ** 2 * F_Data;
+         Errors := (F_Labels - Y);
+         Delta_Weights := Mult (Errors, Exp_Y1, Y) ** 2 * F_Data;
 
          --  Get new weights by taking a step of size alpha and updating
          Current_Loss := Loss (Weights, Data, Labels);
-         Alpha := 2.0 * Alpha;
-         New_Weights := Weights + Alpha * Delta_Weights;
+         Step_Size := 2.0 * Step_Size;
+         New_Weights := Weights + Step_Size * Delta_Weights;
 
          while Loss (New_Weights, Data, Labels) >= Current_Loss and not Done loop
-            Alpha := Alpha / 2.0;
-            Done := Alpha * max (abs (Delta_Weights)) < 0.0001;
+            Step_Size := Step_Size / 2.0;
+            Done := Step_Size * Max (abs (Delta_Weights)) < 0.0001;
             if Done then
-               Put_Line ("Alpha" & Float'Image (Alpha));
+               Put_Line ("Step_Size: " & Float'Image (Step_Size));
                Print_Float_Vector ("Delta_Weights", Delta_Weights);
             else
-               New_Weights := Weights + Alpha * Delta_Weights;
+               New_Weights := Weights + Step_Size * Delta_Weights;
                if not done then
                   Weights := New_Weights;
                end if;
@@ -83,18 +83,18 @@ package body ML is
    function Loss (Weights : Real_Float_Vector; Data   : Integer_Matrix;
                   Labels : Integer_Array) return Float is
       use Real_Float_Arrays;
-      H     : constant Real_Float_Vector := To_Real_Float_Matrix (Data) * Weights;
+      H      : constant Real_Float_Vector := To_Real_Float_Matrix (Data) * Weights;
       --  transform using the sigmoid function
-      Exp_H : constant Real_Float_Vector := Exp (-H);
-      Y     : constant Real_Float_Vector := 1.0 / (1.0 + Exp_H);
-      Diff  : Real_Float_Vector (Labels'Range);
+      Exp_H  : constant Real_Float_Vector := Exp (-H);
+      Y      : constant Real_Float_Vector := 1.0 / (1.0 + Exp_H);
+      Errors : Real_Float_Vector (Labels'Range);
    begin
       --  take the difference between the labels and the output of the
       --  sigmoid squared, then sum over all instances to get the
       --  total loss.
-      Diff := (Y - To_Real_Float_Vector (Labels)) ** 2;
+      Errors := (To_Real_Float_Vector (Labels) - Y) ** 2;
 
-      return Sum (Diff);
+      return Sum (Errors);
 
    end Loss;
 
