c909464df88d5c7e34c2ccfe6ef02bf632ad261d
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-23 16:51:46 +1100

    Update

--- examples/gradient_descent/grad_descent_2d/src/py_package/grad_descent_2d.py
+++ examples/gradient_descent/grad_descent_2d/src/py_package/grad_descent_2d.py
@@ -5,6 +5,6 @@ def show_contours(xy, z):
     listmap = list (xy[0])
     contour_array = np.asarray(xy[0])
     num_rows = int (len(contour_array) / 2)
-    contour_array = bm_array.reshape(num_rows, Row_Size, 3)
-    plt.contourf(xy, z, 10, cmap="Greys")
+#    bm_array = bm_array.reshape(num_rows, Row_Size, 3)
+    plt.contourf(contour_array, z, 10, cmap="Greys")
     plt.show()
