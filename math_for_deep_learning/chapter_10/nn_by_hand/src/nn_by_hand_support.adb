223220fa8f1d942085132f897a2c0c6abbdb6f41
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-25 14:48:21 +1100

    Update

--- math_for_deep_learning/chapter_10/nn_by_hand/src/nn_by_hand_support.adb
+++ math_for_deep_learning/chapter_10/nn_by_hand/src/nn_by_hand_support.adb
@@ -1,5 +1,5 @@
 
-with Maths;
+--  with Maths;
 
 with Load_Dataset;
 with ML_Types;
@@ -7,13 +7,13 @@ with NL_Types;
 
 package body NN_By_Hand_Support is
 
-   procedure Mean (Row : in out Real_Float_Vector);
+   function Means (M : Real_Float_Matrix) return Real_Float_Vector;
 
    --  -------------------------------------------------------------------------
 
    procedure Build_Dataset (X_Train, Y_Train,
                             X_Test, Y_Test : out Real_Float_Vector) is
-      use maths.Float_Math_Functions;
+      --        use maths.Float_Math_Functions;
       Iris_Data        : constant Load_Dataset.Iris_Data_Record :=
                            Load_Dataset.Load_Iris ("src/iris.csv");
       Features         : constant NL_Types.Float_List_2D := Iris_Data.Features;
@@ -22,7 +22,6 @@ package body NN_By_Hand_Support is
       Feature_Row      : NL_Types.Float_List;
       X                : Real_Float_Matrix
         (Features.First_Index .. Features.Last_Index, 1 ..2);
-      X_Row            : Real_Float_Vector (1 .. 2);
    begin
       for row in X'Range loop
          Feature_Row := Features (row);
@@ -30,30 +29,28 @@ package body NN_By_Hand_Support is
          X (row, 2) := Feature_Row (2);
       end loop;
 
-      for row in X'Range loop
-         X_Row (1) := X (row, 1);
-         X_Row (2) := X (row, 2);
-         Mean (X_Row);
-
-         X (row, 1) := X_Row (1);
-         X (row, 2) := X_Row (2);
-      end loop;
-
    end Build_Dataset;
 
    --  -------------------------------------------------------------------------
 
-   procedure Mean (Row : in out Real_Float_Vector) is
-      Sum : Float := 0.0;
+   function Means (M : Real_Float_Matrix) return Real_Float_Vector is
+      M_Length : constant Float := Float (M'Length);
+      Sum1     : Float := 0.0;
+      Sum2     : Float := 0.0;
+      Result   : Real_Float_Vector (1 .. 2);
    begin
-      for index in Row'Range loop
-         Sum := Sum + Row (index);
+      for row in M'Range loop
+         Sum1 := Sum1 + M (row, 1);
+         Sum2 := Sum2 + M (row, 2);
       end loop;
 
-      for index in Row'Range loop
-         Row (index) := Row (index) / Sum;
+      for row in M'Range loop
+         Result (1) := Sum1 / M_Length;
+         Result (2) := Sum2 / M_Length;
       end loop;
 
-   end Mean;
+      return Result;
+
+   end Means;
 
 end NN_By_Hand_Support;
