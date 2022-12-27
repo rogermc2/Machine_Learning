3c74a5f4a1af4b4eb426ee72e2bc3cb4b9d66c4a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-27 21:03:25 +1100

    fully_connected_layer renamed neural_processes

---
+++ math_for_deep_learning/chapter_10/neural_net/src/neural_processes.adb
@@ -0,0 +1,107 @@
+
+with Maths;
+
+with Neural_Maths;
+
+package body Neural_Processes is
+
+   function Backward
+     (Layer : Activation_Layer_Data; Out_Error : Real_Float_Vector)
+      return Real_Float_Vector is
+      use Neural_Maths;
+      Result : Real_Float_Vector (Layer.Input_Data'Range);
+   begin
+      for index in Layer.Input_Data'Range loop
+         Result (index) :=
+           D_Sigmoid (Layer.Input_Data (index)) * Out_Error (index);
+      end loop;
+
+      return Result;
+
+   end Backward;
+
+   --  --------------------------------------------------------------
+
+   function Backward (Layer : in out Layer_Data; Out_Error : Real_Float_Vector)
+                      return Real_Float_Vector is
+      use Real_Float_Arrays;
+      In_Error      : constant Real_Float_Vector :=
+                        Out_Error * Transpose (Layer.Weights);
+      Weights_Error : constant Real_Float_Vector :=
+                        Transpose (Layer.Input_Data) * Out_Error;
+   begin
+      Layer.Delta_W := Layer.Delta_W + Weights_Error;
+      Layer.Bias := Layer.Bias + Out_Error;
+      Layer.Passes := Layer.Passes + 1;
+
+      return In_Error;
+
+   end Backward;
+
+   --  --------------------------------------------------------------
+
+   function Forward
+     (Layer : out Activation_Layer_Data; Input_Data : Real_Float_Matrix)
+      return Real_Float_Matrix is
+      Result : Real_Float_Vector (Input_Data'Range);
+   begin
+      Layer.Input_Data := Input_Data;
+      for index in Input_Data'Range loop
+         Result (index) := Neural_Maths.Sigmoid (Layer.Input_Data (index));
+      end loop;
+
+      return Result;
+
+   end Forward;
+
+   --  --------------------------------------------------------------
+
+   function Forward (Layer : out Layer_Data; Input_Data : Real_Float_Matrix)
+                     return Real_Float_Matrix is
+      use Real_Float_Arrays;
+   begin
+      Layer.Input_Data := Input_Data;
+      return Input_Data * Layer.Weights + Layer.Bias;
+
+   end Forward;
+
+   --  --------------------------------------------------------------
+
+   procedure Initialize (Layer : out Layer_Data) is
+   begin
+      for row in Layer.Weights'Range loop
+         for col in Layer.Weights'Range (2) loop
+            Layer.Weights (row, col) := 0.5 * Maths.Random_Float;
+         end loop;
+         Layer.Bias (row) := 0.5 * Maths.Random_Float;
+      end loop;
+
+   end Initialize;
+
+   --  --------------------------------------------------------------
+
+   procedure Step (Layer : Activation_Layer_Data) is
+   begin
+      null;
+
+   end Step;
+
+   --  --------------------------------------------------------------
+
+   procedure Step (Layer : in out Layer_Data; Eta : Float) is
+      use Real_Float_Arrays;
+   begin
+      Layer.Weights :=
+        Layer.Weights - Eta / Float (Layer.Passes) * Layer.Delta_W;
+      Layer.Bias :=
+        Layer.Bias - Eta * Layer.Delta_B / Float (Layer.Passes);
+      Layer.Delta_W :=
+        Zero_Matrix (Layer.Delta_W'Length, Layer.Delta_W'Length (2));
+      Layer.Delta_B := Zero_Array (Layer.Delta_B'Length);
+      Layer.Passes := 0;
+
+   end Step;
+
+   --  --------------------------------------------------------------
+
+end Neural_Processes;
