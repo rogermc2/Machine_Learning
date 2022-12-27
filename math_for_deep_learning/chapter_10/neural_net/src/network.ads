d036955e6ec45a820048de4d4a08695ed61b0612
Roger Mc Murtrie <rogermc@grapevine.com.au>
2022-12-27 22:19:37 +1100

    Predict function coded

--- math_for_deep_learning/chapter_10/neural_net/src/network.ads
+++ math_for_deep_learning/chapter_10/neural_net/src/network.ads
@@ -1,33 +1,24 @@
 
-with Ada.Containers.Ordered_Maps;
+with Ada.Containers.Indefinite_Vectors;
 
 with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
-with ML_Types;
+--  with ML_Types;
+
+with Neural_Processes; use Neural_Processes;
 
 package Network is
 
-   package Network_Package is new Ada.Containers.Ordered_Maps
-     (Key_Type => String_2, Element_Type => Float);
+   package Network_Layers is new Ada.Containers.Indefinite_Vectors
+     (Positive, Neural_Processes.Layer_Data);
+   subtype Network_List is Network_Layers.Vector;
 
-   type Dataset (Train_Length, Test_Length, Num_Features : Positive) is record
-      X_Train : Real_Float_Matrix (1 .. Train_Length, 1 .. Num_Features);
-      Y_Train : Integer_Array (1 .. Train_Length);
-      X_Test  : Real_Float_Matrix (1 .. Test_Length, 1 .. Num_Features);
-      Y_Test  : Integer_Array (1 .. Test_Length);
+   type Network_Data is record
+      Verbose : Boolean := False;
+      Layers  : Network_List;
    end record;
 
-   function Build_Dataset return Dataset;
-   procedure Evaluate
-     (Net            : Network_Package.Map; X : Real_Float_Matrix;
-      Y              : Integer_Array;
-      Tn, Fp, Fn, Tp : out Natural; Accuracy : out Float;
-      Pred           : out ML_Types.Integer_List);
-   procedure Gradient_Descent
-     (Net : in out Network_Package.Map; X   : Real_Float_Matrix;
-      Y   : Integer_Array; Epochs : Positive; Eta : Float);
-   function Load_Data (File_Name : String; Num_Columns : Positive)
-                       return Real_Float_Matrix;
-   function Load_Data (File_Name : String) return Real_Float_Vector;
-   function Neural_Net return Network_Package.Map;
+   procedure Add (Network : in out Network_Data; Layer : Layer_Data);
+   function Predict (Network : in out Network_Data; Input_Data : Real_Float_Matrix)
+                     return Real_Matrix_List;
 
 end Network;
