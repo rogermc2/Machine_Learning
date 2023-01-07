9f9d837c670b0d3811d449dff84c18a00f5e3f45
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-01-08 07:21:13 +1100

    Update

--- math_for_deep_learning/chapter_10/aflak_neural_net/src/aflak_neural_net.adb
+++ math_for_deep_learning/chapter_10/aflak_neural_net/src/aflak_neural_net.adb
@@ -46,6 +46,7 @@ begin
       Y_Train       : constant Binary_Matrix := Categorize (Data.Train_Y);
       X_Test        : constant Real_Float_Matrix := Data.Test_X / 255.0;
       Y_Test        : constant Binary_Matrix := Categorize (Data.Test_Y);
+      Output_Data   : Real_Float_List;
    begin
       Put_Line ("Train X length: " & Count_Type'Image (X_Train'Length) & " x" &
                   Count_Type'Image (X_Train'Length (2)));
@@ -64,25 +65,29 @@ begin
       Train (Net, X_Train, Y_Train, Epochs, Learning_Rate);
 
       --  Build the confusion matrix using the test set predictions
---        Predictions := Predict (Net, X_Test);  --  out
---        Put_Line ("Predictions size:" & Integer'Image (Integer (Predictions.Length))
---                  & " x" & Integer'Image (Integer (Predictions (1).Length)));
---        Print_Binary_Matrix ("Y_Test", Y_Test, 1, 1);
---        Print_Real_Float_List_2D ("Predictions", Predictions, 1, 1);
---
---        --  Y_Test values range is the digits 0 .. 9
---        for index in Y_Test'Range loop
---           CM_Col := Arg_Max (Predictions (index)) - 1;
---           Confusion (Integer (Y_Test (index, CM_Col)), CM_Col) :=
---             Confusion (Integer (Y_Test (index, CM_Col)), CM_Col) + 1 ;
---        end loop;
---
---        Print_Integer_Matrix ("Confusion matrix", Confusion);
---        Put_Line ("Confusion diagonal sum:" &
---                    Integer'Image (Sum_Diagonal (Confusion)));
---        Put_Line ("Confusion sum:" & Integer'Image (Sum (Confusion)));
---        Put_Line ("Accuracy: " & Float'Image (Float (Sum_Diagonal (Confusion)) /
---                    Float (Sum (Confusion))));
+      for sample in X_Test'Range loop
+         Output_Data := To_Real_Float_List (Get_Row (X_Test, sample));
+         Predict (Net, Output_Data);
+         Predictions.Append (Output_Data);
+      end loop;
+      Put_Line ("Predictions size:" & Integer'Image (Integer (Predictions.Length))
+                & " x" & Integer'Image (Integer (Predictions (1).Length)));
+      Print_Binary_Matrix ("Y_Test", Y_Test, 1, 1);
+      Print_Real_Float_List_2D ("Predictions", Predictions, 1, 1);
+
+      --  Y_Test values range is the digits 0 .. 9
+      for index in Y_Test'Range loop
+         CM_Col := Arg_Max (Predictions (index)) - 1;
+         Confusion (Integer (Y_Test (index, CM_Col)), CM_Col) :=
+           Confusion (Integer (Y_Test (index, CM_Col)), CM_Col) + 1 ;
+      end loop;
+
+      Print_Integer_Matrix ("Confusion matrix", Confusion);
+      Put_Line ("Confusion diagonal sum:" &
+                  Integer'Image (Sum_Diagonal (Confusion)));
+      Put_Line ("Confusion sum:" & Integer'Image (Sum (Confusion)));
+      Put_Line ("Accuracy: " & Float'Image (Float (Sum_Diagonal (Confusion)) /
+                  Float (Sum (Confusion))));
    end;
 
    Put_Line (Project_Name & "done.");
