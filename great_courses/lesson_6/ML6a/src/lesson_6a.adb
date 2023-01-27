54d20ca61efa805f7ec81f56558a2989bb08c26e
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-01-28 08:49:42 +1100

    Update

--- great_courses/lesson_6/ML6a/src/lesson_6a.adb
+++ great_courses/lesson_6/ML6a/src/lesson_6a.adb
@@ -2,23 +2,25 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 --  with Basic_Printing; use Basic_Printing;
-with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
+--  with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
 with Python; use Python;
 
 --  with Maths;
 
-with ML; use ML;
+--  with ML; use ML;
 with Support_6A; use Support_6A;
 
 procedure Lesson_6A is
 
-   type Integer3_Array is array (Integer range 1 .. 3) of Integer;
+--     type Integer3_Array is array (Integer range 1 .. 3) of Integer;
 
    Project_Name           : constant String := "Lesson_6A ";
-   Green_File_Name        : constant String := "../greenML.png";
-   Forest_File_Name       : constant String := "../forest.jpg";
+   Vocab_File_Name        : constant String := "../data/vocab.txt";
+   Train_File_Name        : constant String := "../data/spam-train.csv";
+   Test_File_Name         : constant String := "../data/spam-test.csv";
    Py_Module              : Module;
-   Weights                : Real_Float_Vector (1 .. 4);
+   Word_Dict              : Vocablary_Dictionary_Map;
+--     Weights                : Real_Float_Vector (1 .. 4);
 begin
 
    Python.Initialize;
