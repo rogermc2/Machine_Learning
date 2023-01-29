db6a3233d81d79636a9d5b3ffce6028962206a83
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-01-29 23:14:10 +1100

    Update

--- great_courses/lesson_6/ML6a/src/py_package/lesson_6a.py
+++ great_courses/lesson_6/ML6a/src/py_package/lesson_6a.py
@@ -12,15 +12,6 @@ def fit (clf, dat, labels):
 def predict (clf, testdat):
     return tuple (clf.predict (testdat))
 
-#    return sum([yhat[i] == testlabs[i] for i in range(len(testdat))])/len(testdat)
-
-#    correct = 0
-#    for i in range(len(dat)):
-#        if clf.predict([dat[i]]) == labels[i]:
-#            correct = correct + 1
-#    percent_correct = 100.0 * correct / len (dat)
-#    print("Percentage correct: ", percent_correct)
-
 def show_tree (clf, feat_names):
     tree.plot_tree (clf, feature_names=feat_names, filled=True,
                     rounded=True, fontsize=8)
