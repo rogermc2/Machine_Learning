fa7a333d5cf365575c6c7dba33f3080c22c08939
rogermc2 <rogermc@iinet.net.au>
2023-02-04 16:19:02 +1100

    Update

--- great_courses/lesson_7/ML7a/src/py_package/lesson_7a.py
+++ great_courses/lesson_7/ML7a/src/py_package/lesson_7a.py
@@ -8,8 +8,8 @@ def init_classifer(max_leaves):
     return tree.DecisionTreeClassifier(max_leaf_nodes=max_leaves)
 
 def plot_data(X_train, y_train):
-    plt.scatter(X_train, y_train)
-    plt.title('Target distance')
-    plt.xlabel('angle')
-    plt.ylabel('distance')
-    plt.show()
+    pyplot.scatter(X_train, y_train)
+    pyplot.title('Target distance')
+    pyplot.xlabel('angle')
+    pyplot.ylabel('distance')
+    pyplot.show()
