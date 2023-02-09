9b88fbc4f2b01f2c1c7be5bb830664ceea1cc8bc
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-02-09 15:20:56 +1100

    lesson_7qs completed

--- great_courses/lesson_7/ML7qs/src/lesson_7qs.adb
+++ great_courses/lesson_7/ML7qs/src/lesson_7qs.adb
@@ -1,7 +1,20 @@
 
-with Ada.Text_IO; use Ada.Text_IO;
+--  That is, it solves a regression problem by finding a symbolic expression
+--  that is a small piece of a program.
+--  The program it finds will use 'add' for add, 'mul' for multiply and
+--  'abs' for absolute value.
+--  A program can use as many of these operators as necessary.
 
---  with Maths;
+--  training data and being parsimonious; that is, using a small expression.
+--  The parsimony_coefficient tells the genetic programming search how much
+--  weight to put on parsimony compared to accuracy.
+--  The parsimony coefficient can be any non-negative value, where
+--  smaller numbers tend to result in much bigger programs.
+
+--  Lesson_7Qs trains a symbolic regression using genetic programming and
+--  tries to fit the model to a set of points.
+
+with Ada.Text_IO; use Ada.Text_IO;
 
 --  with Basic_Printing; use Basic_Printing;
 with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
@@ -15,7 +28,7 @@ procedure Lesson_7Qs is
 
    Project_Name : constant String := "Lesson 7Qs ";
    Population   : constant Positive := 10000;
-   Parsimony    : constant Float := 0.1;
+   Parsimony    : constant Float := 0.01;
    Num_Samples  : constant Positive := 50;
    X            : constant Real_Float_Matrix := Load_Data (Num_Samples);
    Y            : constant Real_Float_Vector := Fit (X);
