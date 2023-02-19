402a5e52ca0d229aa8157749834776de010a2649
rogermc2 <rogermc@iinet.net.au>
2023-02-19 12:54:19 +1100

    Update

--- great_courses/lesson_8/ML8aux/src/support_8aux.ads
+++ great_courses/lesson_8/ML8aux/src/support_8aux.ads
@@ -3,10 +3,6 @@ with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
 
 package Support_8Aux is
 
---     type Prediction_Kind is (True_Negative, True_Positive, False_Negative,
---                              False_Positive);
---     type Prediction_Info is array (Positive range <>) of Prediction_Kind;
-
    type Data_Record (Num_Items, Num_Features : Positive) is record
       Features : Real_Float_Matrix (1 .. Num_Items, 1 .. Num_Features);
       Labels   : Integer_Array (1 .. Num_Items);
@@ -22,10 +18,5 @@ package Support_8Aux is
       Test_X     : out Real_Float_Matrix; Test_Y : out Boolean_Array);
    function Accuracy (Predictions : Boolean_Array; Labels : Boolean_Array)
                       return Float;
---  private
---     for Prediction_Kind use (True_Negative  => 0,
---                              True_Positive  => 1,
---                              False_Negative => 2,
---                              False_Positive => 3);
 
 end Support_8Aux;
