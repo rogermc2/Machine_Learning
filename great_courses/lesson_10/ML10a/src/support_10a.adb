4482e411532c6c3889774c1f9e52bd8520aea5b1
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-03-08 22:02:45 +1100

    type Features_Record added

--- great_courses/lesson_10/ML10a/src/support_10a.adb
+++ great_courses/lesson_10/ML10a/src/support_10a.adb
@@ -1,12 +1,11 @@
 
 with Ada.Strings;
-with Ada.Strings.Fixed;
-with Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with Maths;
 
 --  with Basic_Printing; use Basic_Printing;
+with ML_Types;
 with Neural_Loader;
 
 package body Support_10A is
@@ -32,7 +31,6 @@ package body Support_10A is
 
    function Get_Data (File_Name : String) return Data_Record is
       use Ada.Strings;
-      use Ada.Strings.Unbounded;
       use ML_Types;
       use String_Package;
       Routine_Name  : constant String := "Support_8A.Get_Data ";
@@ -40,27 +38,15 @@ package body Support_10A is
       Row_Words     : String_List;
       aWord         : Unbounded_String;
       Word_Cursor   : Cursor;
-      Boolean_Value : Value_Record (Boolean_Type);
-      Float_Value   : Value_Record (Float_Type);
-      Integer_Value : Value_Record (Integer_Type);
-      Unbound_Value : Value_Record (UB_String_Type);
    begin
       Raw_Data := Neural_Loader.Load_CSV_Data (File_Name);
       Put_Line (Routine_Name & File_Name & " loaded");
 
       declare
          Data_Length : constant Positive := Positive (Raw_Data.Length) - 1;
-         Data        : Data_Record (Data_Length, 9);
+         Data        : Data_Record (Data_Length);
          Embarked    : String (1 .. 1);
       begin
-         Data.Feature_Names :=
-           (To_Unbounded_String ("Pclass"), To_Unbounded_String ("Sex"),
-            To_Unbounded_String ("Age"), To_Unbounded_String ("SibSp"),
-            To_Unbounded_String ("Parch"), To_Unbounded_String ("Fare"),
-            To_Unbounded_String ("Embarked S"),
-            To_Unbounded_String ("Embarked C"),
-            To_Unbounded_String ("Embarked Q"));
-
          for row in Raw_Data.First_Index + 1 .. Raw_Data.Last_Index loop
             Row_Words :=
               Neural_Loader.Split_String (To_String (Raw_Data (row)), ",");
@@ -77,52 +63,43 @@ package body Support_10A is
               Integer'Value (To_String (Element (Word_Cursor)));
             Next (Word_Cursor);              --  Survived
             Next (Word_Cursor);              --  Pclass
-            Integer_Value.Integer_Value :=
+            Data.Features (row - 1).P_Class :=
               Integer'Value (To_String (Element (Word_Cursor)));
-            Data.Features (row - 1, 1) := Integer_Value;
             Next (Word_Cursor);              --  First Name
             Next (Word_Cursor);              --  Surname Name
             Next (Word_Cursor);              --  Sex
-            Unbound_Value.UB_String_Value := Element (Word_Cursor);
-            Data.Features (row - 1, 2) := Unbound_Value;
+            if To_String (Element (Word_Cursor)) = "male" then
+               Data.Features (row - 1).Sex := 0;
+            else
+               Data.Features (row - 1).Sex := 1;
+            end if;
             Next (Word_Cursor);              --  Age
             aWord := Element (Word_Cursor);
             if To_String (aWord)'Length = 0 then
-               Integer_Value.Integer_Value := 50;
-            elsif Fixed.Index (To_String (aWord), ".") /= 0 then
-               Integer_Value.Integer_Value :=
-                 Integer (Float'Value (To_String (aWord)));
+               Data.Features (row - 1).Age := 50.0;
             else
-               Integer_Value.Integer_Value := Integer'Value (To_String (aWord));
+               Data.Features (row - 1).Age := Float'Value (To_String (aWord));
             end if;
-            Data.Features (row - 1, 3) := Integer_Value;
             Next (Word_Cursor);              --  SibSp
-            Integer_Value.Integer_Value :=
+            Data.Features (row - 1).Sib_Sp :=
               Integer'Value (To_String (Element (Word_Cursor)));
-            Data.Features (row - 1, 4) := Integer_Value;
             Next (Word_Cursor);              --  Parch
-            Integer_Value.Integer_Value :=
+            Data.Features (row - 1).Parch :=
               Integer'Value (To_String (Element (Word_Cursor)));
-            Data.Features (row - 1, 5) := Integer_Value;
             Next (Word_Cursor);              --  Ticket
-            Next (Word_Cursor);              --  Fare
-            Float_Value.Float_Value :=
+            Next (Word_Cursor);
+            Data.Features (row - 1).Fare :=
               Float'Value (To_String (Element (Word_Cursor)));
-            Data.Features (row - 1, 6) := Float_Value;
             Next (Word_Cursor);              --  Cabin
             Next (Word_Cursor);              --  Embarked
-            if To_String (Element (Word_Cursor))'Length /= 1 then
+            if To_String (Element (Word_Cursor))'Length = 0 then
                Embarked := " ";
             else
                Embarked := To_String (Element (Word_Cursor));
             end if;
-            Boolean_Value.Boolean_Value := Embarked = "S";
-            Data.Features (row - 1, 7) := Boolean_Value;
-            Boolean_Value.Boolean_Value := Embarked = "C";
-            Data.Features (row - 1, 8) := Boolean_Value;
-            Boolean_Value.Boolean_Value := Embarked = "Q";
-            Data.Features (row - 1, 9) := Boolean_Value;
-
+            Data.Features (row - 1).Embark_S := Embarked = "S";
+            Data.Features (row - 1).Embark_C := Embarked = "C";
+            Data.Features (row - 1).Embark_Q := Embarked = "Q";
          end loop;
 
          return Data;
@@ -149,27 +126,25 @@ package body Support_10A is
       end loop;
 
       declare
-         Train_Data : Data_Record (Train_Length, Data.Num_Features);
-         Test_Data  : Data_Record (Test_Length, Data.Num_Features);
+         Train_Data : Data_Record (Train_Length);
+         Test_Data  : Data_Record (Test_Length);
          Train_Row  : Natural := 0;
          Test_Row   : Natural := 0;
       begin
          for row in Mask'Range loop
             if Mask (row) then
                Train_Row := Train_Row + 1;
-               for col in 1 .. Data.Num_Features loop
-                  Train_Data.Features (Train_Row, col) :=
-                    Data.Features (row, col);
-               end loop;
+               Train_Data.Features (Train_Row) := Data.Features (row);
                Train_Data.Labels (Train_Row) := Data.Labels (row);
             else
                Test_Row := Test_Row + 1;
+               Test_Data.Features (Test_Row) := Data.Features (row);
+               Test_Data.Labels (Test_Row) := Data.Labels (row);
             end if;
          end loop;
 
          declare
-            Split_Data : Split_Data_Record (Train_Length, Test_Length,
-                                            Data.Num_Features);
+            Split_Data : Split_Data_Record (Train_Length, Test_Length);
          begin
             Split_Data.Train_Features := Train_Data.Features;
             Split_Data.Train_Labels := Train_Data.Labels;
--- great_courses/lesson_10/ML10a/src/support_10a.ads
+++ great_courses/lesson_10/ML10a/src/support_10a.ads
@@ -1,23 +1,40 @@
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
-with ML_Types;
 
 package Support_10A is
 
-   type Data_Record (Num_Items, Num_Features : Positive) is record
-      Features      : ML_Types.Value_Data_Array_2D (1 .. Num_Items,
-                                                    1 .. Num_Features);
-      Labels        : Integer_Array (1 .. Num_Items);
-      Feature_Names : Unbounded_String_Array (1 .. Num_Features);
+   type Features_Record is record
+      P_Class  : Positive;
+      Sex      : Natural;
+      Age      : Float;
+      Sib_Sp   : Natural;
+      Parch    : Natural;
+      Fare     : Float;
+      Embark_S : Boolean := False;
+      Embark_C : Boolean := False;
+      Embark_Q : Boolean := False;
    end record;
 
-   type Split_Data_Record
-     (Num_Train, Num_Test, Num_Features : Positive) is record
-      Train_Features : ML_Types.Value_Data_Array_2D (1 .. Num_Train,
-                                                     1 .. Num_Features);
+   type Features_Array is array (Integer range <>) of Features_Record;
+
+   type Data_Record (Num_Items : Positive) is record
+      Features      : Features_Array (1 .. Num_Items);
+      Labels        : Integer_Array (1 .. Num_Items);
+      Feature_Names : Unbounded_String_Array (1 .. 9) :=
+           (To_Unbounded_String ("Pclass"), To_Unbounded_String ("Sex"),
+            To_Unbounded_String ("Age"), To_Unbounded_String ("SibSp"),
+            To_Unbounded_String ("Parch"), To_Unbounded_String ("Fare"),
+            To_Unbounded_String ("Embarked S"),
+            To_Unbounded_String ("Embarked C"),
+            To_Unbounded_String ("Embarked Q"));
+   end record;
+
+   type Split_Data_Record (Num_Train, Num_Test : Positive) is record
+      Train_Features : Features_Array (1 .. Num_Train);
       Train_Labels   : Integer_Array (1 .. Num_Train);
-      Test_Features  : ML_Types.Value_Data_Array_2D (1 .. Num_Test,
-                                                     1 .. Num_Features);
+      Test_Features  : Features_Array (1 .. Num_Test);
       Test_Labels    : Integer_Array (1 .. Num_Test);
    end record;
 
