c7caecb95b33f54d3c4cddb48fd0ec866b04c489
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-03-10 10:52:16 +1100

    New project lesson_10qs

---
+++ great_courses/lesson_10/ML10qs/src/py_package/lesson_10qs.py
@@ -0,0 +1,25 @@
+from sklearn import tree
+
+import matplotlib
+from matplotlib import pyplot
+
+def classify (dat, lab, feat_names):
+    data = dat
+    labels = lab
+    clf = tree.DecisionTreeClassifier(max_leaf_nodes=8)
+    clf = clf.fit(data, labels)
+
+    correct = 0
+    for i in range(len(data)):
+        if clf.predict([data[i]]) == labels[i]:
+            correct = correct + 1
+    percent_correct = 100.0 * correct / len (data)
+    print("Percentage correct: ", percent_correct)
+    
+    print ("feat_names: ", feat_names)
+    tree.plot_tree (clf, feature_names=feat_names, filled=True,
+                    rounded=True, fontsize=8)
+    pyplot.show()
+    print()
+    return percent_correct
+
