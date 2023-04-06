6af09ab393028985bb5e940929ca81419a9a0e55
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-04-06 22:53:17 +1000

    Update

--- great_courses/lesson_11/ML11qs/src/support_11qs.adb
+++ great_courses/lesson_11/ML11qs/src/support_11qs.adb
@@ -3,19 +3,26 @@ with Python_CLF;
 
 package body Support_11QS is
 
-   function Try_Clusterer (Classifier : Python.Module; Num_Clusters : Positive;
-                           Clusterer  : Python_API.PyObject;
-                           Train_X    : Real_Float_Matrix) return Float is
+   function Try_Clusterer
+     (Classifier : Python.Module; Num_Clusters : Positive;
+      Clusterer  : Python_API.PyObject;
+      Train_X    : Real_Float_Matrix; Train_Y : Integer_Matrix) return Float is
       use Python_API;
-      Clr    : constant PyObject := Python.Call (Classifier, "clust", Num_Clusters);
+      Clr    : constant PyObject := Python.Call (Classifier, "clust",
+                                                 Num_Clusters);
       Result : Float;
    begin
       Python_CLF.Call (Classifier, "fit", Clr, Train_X);
       declare
-         Train_IDS : constant Integer_Array :=
-           Python_CLF.Call (Classifier, "copy_labels", Clr);
+         Train_IDs      : constant Integer_Array :=
+                            Python_CLF.Call (Classifier, "copy_labels", Clr);
+         Cluster_Labels : Integer_Array (1 .. Num_Clusters) := (others => -1);
       begin
-         null;
+         --  Request one label per cluster and make an interim dataset out of
+         --  X_train, y_guess .
+         for index in Cluster_Labels'Range loop
+            Cluster_Labels (index) := Train_Y (Train_IDs (index), 1);
+           end loop;
       end;
       return Result;
 
--- great_courses/lesson_11/ML11qs/src/support_11qs.ads
+++ great_courses/lesson_11/ML11qs/src/support_11qs.ads
@@ -5,7 +5,8 @@ with Python_API;
 
 package Support_11QS is
 
-   function Try_Clusterer (Classifier : Python.Module; Num_Clusters : Positive;
-                           Clusterer  : Python_API.PyObject;
-                           Train_X    : Real_Float_Matrix) return Float;
+   function Try_Clusterer
+     (Classifier : Python.Module; Num_Clusters : Positive;
+      Clusterer  : Python_API.PyObject;
+      Train_X    : Real_Float_Matrix; Train_Y : Integer_Matrix) return Float;
 end Support_11QS;
