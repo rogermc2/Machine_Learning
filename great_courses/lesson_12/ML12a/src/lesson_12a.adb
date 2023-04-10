1063032b192f181715470629b7ec5e9b6b70ae0b
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-04-10 22:54:25 +1000

    Update

--- great_courses/lesson_12/ML12a/src/lesson_12a.adb
+++ great_courses/lesson_12/ML12a/src/lesson_12a.adb
@@ -2,23 +2,25 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 --  with Basic_Printing; use  Basic_Printing;
-with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
+--  with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
 with Python;
 
 with Support_12A; use Support_12A;
 
 procedure Lesson_12A is
-   use Real_Float_Arrays;
+--     use Real_Float_Arrays;
    Program_Name     : constant String := "Lesson 12A ";
-   Vocab            : constant String := "../../../data/vocab2.txt";
-   CB               : constant String := "../../../data/cb.txt";
+   Vocab_Dictionary : constant Dictionary_List :=
+                        Read_Vocabulary ("../../data/vocab2.txt");
+   CB               : constant Data_Record :=
+                        Get_Data ("../../data/cb.txt", Vocab_Dictionary);
    Classifier       : Python.Module;
 begin
    Put_Line (Program_Name);
 
    Python.Initialize;
 
-   Classifier := Python.Import_File ("lesson_11a");
+   Classifier := Python.Import_File ("lesson_12a");
 
    Python.Close_Module (Classifier);
    Python.Finalize;
--- great_courses/lesson_12/ML12a/src/support_12a.ads
+++ great_courses/lesson_12/ML12a/src/support_12a.ads
@@ -1,9 +1,28 @@
 
+with Ada.Containers.Doubly_Linked_Lists;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
 with ML_Types;
 
 package Support_12A is
 
-   function Load_Data (File_Name : String) return ML_Types.Unbounded_List;
+   type Data_Record is record
+      Features : Integer_Array_List;
+      Labels   : ML_Types.Integer_List;
+   end record;
+
+   type Dictionary_Record is record
+      Key   : Unbounded_String;
+      Value : Natural := 0;
+   end record;
+
+   package Dictionary_Package is new
+     Ada.Containers.Doubly_Linked_Lists (Dictionary_Record);
+   subtype Dictionary_List is Dictionary_Package.List;
+
+   function Get_Data (File_Name : String; Dictionary : Dictionary_List)
+                      return Data_Record;
+   function Read_Vocabulary (File_Name : String) return Dictionary_List;
 
 end Support_12A;
