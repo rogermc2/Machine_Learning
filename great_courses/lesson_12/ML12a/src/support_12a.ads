131b73a226f554b8c20ed5ac46ceada3c95677f4
rogermc2 <rogermc@iinet.net.au>
2023-04-10 20:30:52 +1000

    Update

--- great_courses/lesson_12/ML12a/src/support_12a.ads
+++ great_courses/lesson_12/ML12a/src/support_12a.ads
@@ -4,12 +4,6 @@ with ML_Types;
 
 package Support_12A is
 
-   function Get_Plot_Data
-     (Data         : Real_Float_Matrix; Cluster_Labels : Integer_Array;
-      Center_IDs   : Integer_Array; Cluster_ID : Natural;
-      Col_1, Col_2 : Positive) return Real_Float_Matrix ;
-   function Select_Items (Data  : Integer_Matrix;  Center_IDs : Integer_Array;
-                          Index : Natural) return ML_Types.Integer_List;
    function Load_Data (File_Name : String) return ML_Types.Unbounded_List;
 
 end Support_12A;
