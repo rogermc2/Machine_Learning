6fbd8a9bc46890f7802044eb5901ee3fae4a3a1c
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-05-27 16:23:50 +1000

    High-Sierra Reinstalled scikit-learn

--- great_courses/lesson_15/ML15a/src/py_package/lesson_15a.py
+++ great_courses/lesson_15/ML15a/src/py_package/lesson_15a.py
@@ -3,6 +3,12 @@ from PIL import Image
 from sklearn import tree
 from matplotlib import pyplot as plt
 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+
 def load_image (file_name):
     img = Image.open(file_name)
     img_arr = np.array(img)
@@ -28,3 +34,30 @@ def show_bitmap(bitmap, Row_Size):
     bm_array = bm_array.reshape(num_rows, Row_Size, 3)
     plt.imshow(bm_array)
     plt.show()
+def build_network():
+    input_shape = (64, 64, 3)
+    #Instantiate an empty model
+    model = Sequential()
+    model.add(Conv2D(64, (3, 3), input_shape=input_shape, padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    return(model)
