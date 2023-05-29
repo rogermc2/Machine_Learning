17f3bee4fcf1aea52f90df8cd57de96dbd2e13a9
rogermc2 <rogermc@iinet.net.au>
2023-05-29 15:44:32 +1000

    New project

---
+++ great_courses/lesson_16/ML16a/src/support_16a.ads
@@ -0,0 +1,39 @@
+
+with Ada.Containers.Doubly_Linked_Lists;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+with ML_Arrays_And_Matrices; use ML_Arrays_And_Matrices;
+with ML_Types;
+with Python;
+with Python_API;
+
+package Support_16A is
+
+   type Data_Items (Rows : Positive) is record
+      Features : Integer_Array_List;
+      Labels   : Integer_Matrix (1 .. Rows, 1 .. 1);
+   end record;
+
+   type Dictionary_Record is record
+      Key   : Unbounded_String;
+      Value : Natural := 0;
+   end record;
+
+   package Dictionary_Package is new
+     Ada.Containers.Doubly_Linked_Lists (Dictionary_Record);
+   subtype Dictionary_List is Dictionary_Package.List;
+
+   function ProbA_Chooser
+     (Classifier       : Python.Module;
+      Current_Item     : Positive; Num_Items : Positive;
+      Labeled_Examples : Data_Items;
+      Train_Set        : ML_Types.Integer_List_2D;
+      Train_Labels     : ML_Types.Integer_List; Alpha : Integer;
+      Clf              : Python_API.PyObject) return Integer;
+   function Get_Data (File_Name : String) return Data_Items;
+   function Play_Game (Classifier : Python.Module;
+                       Rounds     : Positive; Labeled_Examples : Data_Items;
+                       Alpha      : Integer) return Natural;
+   function Read_Vocabulary (File_Name : String) return Dictionary_List;
+
+end Support_16A;
