d56d881349b58db10391cf6deeeee1d27bf25a21
rogermc2 <rogermc@iinet.net.au>
2023-07-23 16:45:11 +1000

    Update

--- great_courses/lesson-22/rewards/src/py_package/rewards.py
+++ great_courses/lesson-22/rewards/src/py_package/rewards.py
@@ -11,11 +11,20 @@ from dowhy import CausalModel
 #from matplotlib import pyplot
 
 def init_data(num_users, num_months, signup_months):
-    return pd.DataFrame({'user_id': np.repeat(np.arange(num_users), num_months),
+    df = pd.DataFrame({'user_id': np.repeat(np.arange(num_users), num_months),
                         # signup month == 0 means customer did not sign up
                       'signup_month': np.repeat(signup_months, num_months),
                       'month': np.tile(np.arange(1, num_months+1), num_users),
                       'spend': np.random.poisson(500, num_users*num_months)}) #np.random.beta(a=2, b=5, size=num_users * num_months)*1000 # centered at 500})
+    # A customer is in the treatment group if and only if they are signed up
+    df["treatment"] = df["signup_month"]>0
+    # Simulating an effect of month (monotonically decreasing--customers buy less later in the year)
+    df["spend"] = df["spend"] - df["month"]*10
+    after_signup = (df["signup_month"] < df["month"]) & (df["treatment"])
+    df.loc[after_signup,"spend"] = df[after_signup]["spend"] + 100
+    print (df)
+    return df
+                      
                         
 #def init_model(col, data_in, xs):
 #    data = pd.DataFrame(list(data_in))
