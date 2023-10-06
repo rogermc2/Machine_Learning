56571e3619f6f691476f681154adcca2cacb0ac0
Roger Mc Murtrie <rogermc@grapevine.com.au>
2023-10-06 16:45:53 +1100

    Update

--- deep_learning_patterns/house_prices/src/prices.adb
+++ deep_learning_patterns/house_prices/src/prices.adb
@@ -15,12 +15,12 @@ procedure Prices is
    use Real_Float_Arrays;
    Program_Name : constant String            := "House Prices ";
    Train_Length : constant Positive := 70;
-   Test_Length  : constant Positive := 2;
+   Test_Length  : constant Positive := 1;
    Data         : constant Dataset           := Build_Dataset (Train_Length,
                                                                Test_Length, 6);
    Num_Samples  : constant Positive          := Test_Length;
    Num_Features : constant Positive          := Data.Num_Features;
-   Num_Epochs   : constant Positive          := 4;
+   Num_Epochs   : constant Positive          := 1;
    Learn_Rate   : constant Float             := 1.0;
    Input_Data   : constant Real_Float_Matrix := Data.X_Test;
    Labels       : constant Real_Float_Matrix := Data.Y_Test;
